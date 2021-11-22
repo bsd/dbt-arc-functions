44a79d279f29cac4711a9ff3402772224d01cdfd
Tim Ryan <tryan@bluestate.co>
2021-11-22 14:47:03 +0000

    change campaign name and id to line name and id

--- macros/supermetrics_yahoo_dsp/staging/stg_supermetrics_yahoo_dsp_paidmedia_campaigns.sql
+++ macros/supermetrics_yahoo_dsp/staging/stg_supermetrics_yahoo_dsp_paidmedia_campaigns.sql
@@ -2,9 +2,9 @@
        source_name='supermetrics_yahoo_dsp',
        source_table_name='alldates_VDSP_AD') 
 %}
-SELECT  DISTINCT SAFE_CAST(ad.campaign_id AS STRING) AS campaign_id
+SELECT  DISTINCT SAFE_CAST(ad.line_id AS STRING) AS campaign_id
        ,SAFE_CAST(ad.ad_id AS STRING)                AS message_id
        ,SAFE_CAST('yahoo_dsp' AS STRING)             AS channel
-       ,SAFE_CAST(ad.campaign_name AS STRING)        AS campaign_name
+       ,SAFE_CAST(ad.line_name AS STRING)        AS campaign_name
 FROM {{ source(source_name,source_table_name) }} ad
 {% endmacro %}
