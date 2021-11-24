2e94b2f59402cfabf01863eb9c3a36b0874fcd70
Tim Ryan <ryantimjohn@gmail.com>
2021-11-24 13:27:00 -0500

    fix problem with regex

--- utils/create_or_update_standard_models.py
+++ utils/create_or_update_standard_models.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 # coding: utf-8
 # todo: add support for non-sql, non-macro files
+# todo: add support for keeping your dependencies (i.e. "-- depends_on: " lines from up top
 
 import os
 import re
 from os import path
-from shutil import rmtree
-from git import Repo
 import difflib
 
 dbt_string = """-- macro used to create this file can be found at:
@@ -14,31 +13,26 @@ dbt_string = """-- macro used to create this file can be found at:
 {{{{ {function} }}}}
 """
 
+
 def get_create_or_update():
     print("\nFirst thing's first, do you want to create a new set of standard models or update an existing set?")
     create = None
-    while create not in ('c','u'):
+    while create not in ('c', 'u'):
         print("Enter c to (c)reate new standard models or u to (u)pdate standard models.")
         create = input()
     return create == 'c'
 
 
-def get_access_token():
-    print("\nPlease go to this website and copy your access token:")
-    print("https://github.atom.io/auth/github_package/token")
-    print("\nPaste your access token here: ")
-    a_t = input()
-    if a_t:
-        return a_t
-    else:
-        print('You did not provide an access token.')
-        print()
-        return get_access_token()
-
-
-def clone_repo(a_t):
-    git_url = f'https://{a_t}:x-oauth-basic@github.com/bsd/arc-dbt-functions'
-    Repo.clone_from(git_url, 'tmp-arc-dbt-functions')
+def get_destination():
+    print("\nNow, what's the absolute or relative path where you'd like to create models?")
+    print("This is usually the 'models' folder of the target repo.")
+    print("This script will not let you create models in the current repo.")
+    destination_path = ''
+    while not destination_path and not (destination_path.endswith(path.join('..', 'models'))
+                                        or destination_path.endswith(path.join('dbt-arc-functions', 'models'))):
+        print("\nPlease enter the absolute relative path where you'd like to create standard models:")
+        destination_path = input()
+    return destination_path
 
 
 def get_list_of_sources(macros_path):
@@ -55,22 +49,24 @@ def get_sources_wanted(list_of_sources):
     sources_wanted = list(set([source.strip() for source in sources_wanted.split(',')]))
     return sources_wanted
 
-def overwrite_choice(source_file_path,output,destination_file_path):
-    with open(destination_file_path,'r') as d:
+
+def overwrite_choice(source_file_path, output, destination_file_path):
+    with open(destination_file_path, 'r') as d:
         d_text = d.readlines()
         differences = list(difflib.unified_diff(
-                d_text, output, fromfile=destination_file_path,
-                tofile=source_file_path, lineterm=''))
+            d_text, output, fromfile=destination_file_path,
+            tofile=source_file_path, lineterm=''))
         if not differences:
             print(f"No differences between {destination_file_path} and {source_file_path}, skipping.")
             return False
-        print(f'\nThere are differences between your model at:\n{destination_file_path} and the standard model at:\n{source_file_path}')
+        print(
+            f'\nThere are differences between your model at:\n{destination_file_path} and the standard model at:\n{source_file_path}')
         print("Here is your current model:\n")
-        print(*d_text,sep='')
+        print(*d_text, sep='')
         print("Here is the standard model in arc-dbt-functions:\n")
-        print(*output,sep='')
+        print(*output, sep='')
         print("Here are the differences between the two files: \n")
-        print(*differences,sep='')
+        print(*differences, sep='')
     overwrite = None
     while overwrite not in ('o', 'k'):
         print("Enter o to (o)verwrite your model with the standard model or k to (k)eep your model.")
@@ -78,10 +74,10 @@ def overwrite_choice(source_file_path,output,destination_file_path):
     return overwrite == 'o'
 
 
-def write_new_file_choice(output,destination_file_path):
+def write_new_file_choice(output, destination_file_path):
     print(f"\nYou do not have a model at {destination_file_path}\n")
     print("Here is the standard model in arc-dbt-functions:\n")
-    print(*output,sep='')
+    print(*output, sep='')
     print(f"Would you like to create a model with this content at {destination_file_path}?\n")
     yes = None
     while yes not in ('y', 'n'):
@@ -90,10 +86,10 @@ def write_new_file_choice(output,destination_file_path):
     return yes == 'y'
 
 
-def process_sources(sources_wanted, list_of_sources, dbt_string,macros_path,create):
-    model_types = ['source', 'staging', 'marts']
-    rx = re.compile(r"\{% macro (.*?) %\}", re.DOTALL)
-    git_prepend = 'https://github.com/bsd/arc-dbt-functions/blob/main/macros'
+def process_sources(sources_wanted, list_of_sources, dbt_string, macros_path, create, destination):
+    model_types = ['sources', 'staging', 'marts']
+    rx = re.compile(r"\{%\s+macro\s+(.*?)\s+%\}", re.DOTALL)
+    git_prepend = "https://github.com/bsd/dbt-arc-functions/blob/main/macros"
 
     for source in sources_wanted:
         if source not in list_of_sources:
@@ -104,14 +100,14 @@ def process_sources(sources_wanted, list_of_sources, dbt_string,macros_path,crea
                 print(f"Weird, {model_type} is not one of our standard model types. Going to skip.")
                 continue
             source_path = path.join(macros_path, source, model_type)
-            destination_path = path.join('models', model_type, source)
+            destination_path = path.join(destination, model_type, source)
             if path.exists(destination_path) and create:
                 print("\nThis directory: {} already exists and I don't want to overwrite anything in create mode."
-                    .format(destination_path))
+                      .format(destination_path))
                 continue
             elif not path.exists(destination_path) and not create:
                 print("\nThis directory: {} doesn't exist and I don't want to make directories in update mode."
-                    .format(destination_path))
+                      .format(destination_path))
                 continue
             elif create:
                 os.makedirs(destination_path)
@@ -119,18 +115,18 @@ def process_sources(sources_wanted, list_of_sources, dbt_string,macros_path,crea
                 for file in files:
                     source_file_path = path.join(source_path, file)
                     with open(source_file_path, 'r') as f:
-                        destination_file_path = path.join(destination_path,file)
+                        destination_file_path = path.join(destination_path, file)
                         content = f.read()
                         function = rx.search(content).group(1) if rx.search(content) else None
                         github_path = '/'.join([git_prepend, source, model_type, file])
                         output = dbt_string.format(github_path=github_path,
                                                    function=function)
                         if path.exists(destination_file_path) and not create:
-                            output_formatted = [line+'\n' for line in output.split('\n')][:-1]
-                            if not overwrite_choice(source_file_path,output_formatted,destination_file_path):
+                            output_formatted = [line + '\n' for line in output.split('\n')][:-1]
+                            if not overwrite_choice(source_file_path, output_formatted, destination_file_path):
                                 continue
                         if not path.exists(destination_file_path) and not create:
-                            if not write_new_file_choice(output,destination_file_path):
+                            if not write_new_file_choice(output, destination_file_path):
                                 continue
                         if function:
                             with open(destination_file_path, 'w') as tf:
@@ -139,16 +135,14 @@ def process_sources(sources_wanted, list_of_sources, dbt_string,macros_path,crea
 
 
 if __name__ == '__main__':
-    macros_path = path.join('tmp-arc-dbt-functions', 'macros')
+    macros_path = path.join('..', 'macros')
     create = get_create_or_update()
     try:
-        a_t = get_access_token()
-        clone_repo(a_t)
+        destination = get_destination()
         list_of_sources = get_list_of_sources(macros_path)
         sources_wanted = get_sources_wanted(list_of_sources)
-        process_sources(sources_wanted,list_of_sources,dbt_string,macros_path,create)
+        process_sources(sources_wanted, list_of_sources, dbt_string, macros_path, create, destination)
     except Exception as e:
         print(e)
         pass
-    rmtree('tmp-arc-dbt-functions')
     print('\nProgram terminated successfully!\n')
