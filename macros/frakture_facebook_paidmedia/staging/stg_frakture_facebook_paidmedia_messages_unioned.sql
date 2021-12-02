1bfcef53d3783bfc786fa348e577e2b2e86bafc3
Tim Ryan <ryantimjohn@gmail.com>
2021-12-02 13:25:49 -0500

    update facebook sources

--- macros/frakture_facebook_paidmedia/staging/stg_frakture_facebook_paidmedia_messages_unioned.sql
+++ macros/frakture_facebook_paidmedia/staging/stg_frakture_facebook_paidmedia_messages_unioned.sql
@@ -1,7 +1,7 @@
 {% macro create_stg_frakture_facebook_paidmedia_messages_unioned() %}
 {% set relations = dbt_arc_functions.relations_that_match_regex('^facebook_bizman_[A-Za-z0-9]{3}_message$',
   is_source=True,
-  source_name='frakture_facebook',
+  source_name='frakture_facebook_paidmedia',
   schema_to_search='src_frakture') 
 %}
 SELECT DISTINCT * FROM ({{ dbt_utils.union_relations(relations) }})
