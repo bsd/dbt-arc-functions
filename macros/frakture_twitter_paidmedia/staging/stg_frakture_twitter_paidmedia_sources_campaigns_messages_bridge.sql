0546cd27cea9bd87186c96b74062d36c6716c924
Tim Ryan <ryantimjohn@gmail.com>
2021-12-06 15:03:10 -0500

    remove source bridge and subscribes

--- macros/frakture_twitter_paidmedia/staging/stg_frakture_twitter_paidmedia_sources_campaigns_messages_bridge.sql
+++
@@ -1,7 +0,0 @@
-{% macro create_stg_frakture_twitter_paidmedia_sources_campaigns_messages_bridge(
-    reference_name='stg_frakture_twitter_paidmedia_messages_unioned') %}
-SELECT DISTINCT SAFE_CAST(message_id AS STRING) AS message_id,
-       SAFE_CAST(source_code AS STRING) AS source_code,       
-       SAFE_CAST(campaign_id AS STRING) AS campaign_id
-FROM  {{ ref(reference_name) }}
-{% endmacro %}
