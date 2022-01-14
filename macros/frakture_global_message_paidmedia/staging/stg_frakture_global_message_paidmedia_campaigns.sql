bfceeac04c89ce20aa2f5296965d7105ce185160
Tim Ryan <ryantimjohn@gmail.com>
2022-01-13 22:39:10 -0500

    initial commit

---
+++ macros/frakture_global_message_paidmedia/staging/stg_frakture_global_message_paidmedia_campaigns.sql
@@ -0,0 +1,8 @@
+{% macro create_stg_frakture_global_message_paidmedia_campaigns(
+    reference_name='stg_frakture_global_message_paidmedia_ad_summary_by_date') %}
+SELECT DISTINCT SAFE_CAST(campaign_id AS STRING) AS campaign_id
+  , SAFE_CAST(message_id AS STRING) AS message_id
+  , SAFE_CAST(channel AS STRING) AS channel
+  , SAFE_CAST(campaign_name AS STRING) AS campaign_name
+FROM {{ ref(reference_name) }}
+{% endmacro %}
---
+++ macros/frakture_global_message_paidmedia/staging/stg_frakture_global_message_paidmedia_messages.sql
@@ -0,0 +1,4 @@
+{% macro create_stg_frakture_global_message_paidmedia_ad_summary_by_date() %}
+SELECT DISTINCT * FROM 
+ WHERE message_id IS NOT NULL
+{% endmacro %}
