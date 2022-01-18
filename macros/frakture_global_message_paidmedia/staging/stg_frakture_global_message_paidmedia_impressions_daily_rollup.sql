9c789f2badc0b07f8a7ef040a9082197fbb9d443
Tim Ryan <ryantimjohn@gmail.com>
2022-01-18 13:42:34 -0500

    remove unique impressions

--- macros/frakture_global_message_paidmedia/staging/stg_frakture_global_message_paidmedia_impressions_daily_rollup.sql
+++ macros/frakture_global_message_paidmedia/staging/stg_frakture_global_message_paidmedia_impressions_daily_rollup.sql
@@ -2,6 +2,6 @@
     reference_name='stg_frakture_global_message_paidmedia_ad_summary_by_date') %}
 SELECT DISTINCT SAFE_CAST(ad_summary_by_date.message_id AS STRING) AS message_id,  SAFE_CAST(ad_summary_by_date.date AS TIMESTAMP) AS date_timestamp,
   SAFE_CAST(ad_summary_by_date.impressions AS INT) AS total_impressions,
-  SAFE_CAST(ad_summary_by_date.reach AS INT) AS unique_impressions
+  SAFE_CAST(NULL AS INT) AS unique_impressions
 FROM {{ ref(reference_name) }} ad_summary_by_date
 {% endmacro %}
