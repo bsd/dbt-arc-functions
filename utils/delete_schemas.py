bf0fa49e6f8ec1e0f5e6bee6d18f9da37513fdf2
Tim Ryan <ryantimjohn@gmail.com>
2022-01-25 09:57:18 -0500

    update commands to pass dbt_username correctly

--- utils/delete_schemas.py
+++ utils/delete_schemas.py
@@ -12,22 +12,30 @@ def get_client(credentials_path):
 function_helptext = """\nSometimes it helps to delete existing personal dbt schemas before doing a 
 dbt build to delete outdated tables and views"""
 
-def main(credentials_path='',username=''):
+prepend_helptext = """\ndbt user schemas usually start with 'dbt_'.
+Can I add dbt_ to your username to check for schemas that start with dbt_{username}?
+Enter y for (y)es or n for (n)o:\n
+"""
+
+def main(credentials_path='', dbt_username=''):
     if not credentials_path:
         credentials_path = input("\nPlease enter the path to your BigQuery credentials JSON:\n")
-    if not username:
-        username = input("\nPlease enter your dbt username OR your bluestate email:\n")
-        if '@bluestate.co' in username:
-            username = username.split('@')[0]
-            username = 'dbt_'+username
+    if not dbt_username:
+        dbt_username = input("\nPlease enter your dbt username OR your bluestate email:\n")
+        if '@bluestate.co' in dbt_username:
+            dbt_username = dbt_username.split('@')[0]
+    if not dbt_username.startswith('dbt_'):
+        prepend_choice = input(prepend_helptext.format(username=dbt_username))
+        if prepend_choice == 'y':
+            dbt_username = 'dbt_' + dbt_username
     print(function_helptext)
     client = get_client(credentials_path)
     datasets = list(client.list_datasets())  # Make an API request.
     project = client.project
     if datasets:
-        print(f"\nSchemas in project {project} with prepend {username}:")
+        print(f"\nSchemas in project {project} with prepend {dbt_username}:")
         for dataset in datasets:
-            if dataset.dataset_id.startswith(username):
+            if dataset.dataset_id.startswith(dbt_username):
                 print(f"\t{dataset.dataset_id} exists in your database")
                 delete_choice = input('Enter yes to delete the schema (no other input but yes will delete the schema):\n')
                 if delete_choice == 'yes':
--- utils/main.py
+++ utils/main.py
@@ -12,7 +12,7 @@ def main():
      project_id,
      yaml,
      dbt_credentials_path,
-     username] = first_run_dbt_project_and_profiles_fixer.main()
+     dbt_username] = first_run_dbt_project_and_profiles_fixer.main()
     dbt_base_path = path.dirname(dbt_project_path)
     dbt_models_path = path.join(dbt_base_path, "models")
     print("\nRunning create_or_update_standard_models.py\n")
@@ -21,7 +21,7 @@ def main():
     print("\nRunning compile_sources.py\n")
     compile_sources.main(dbt_project_path, dbt_credentials_path, project_id, yaml, dbt_models_sources_path)
     print("\nRunning delete_schemas.py\n")
-    delete_schemas.main(dbt_credentials_path,username)
+    delete_schemas.main(dbt_credentials_path,dbt_username)
     print("\nRunning add_dependencies.py\n")
     add_dependencies.main(dbt_base_path)
     print("\nWow, it all actually ran! Congratulations, you should be able to run this repo now.\n")
