b6483d85ddd9379cebd96ef1441ac09271e6cd59
Tim Ryan <ryantimjohn@gmail.com>
2022-02-15 09:48:58 -0500

    make pull from source rather than reference

--- macros/supermetrics_yahoo_dsp_paidmedia/staging/stg_supermetrics_yahoo_dsp_paidmedia_subscribes_daily_rollup.sql
+++ macros/supermetrics_yahoo_dsp_paidmedia/staging/stg_supermetrics_yahoo_dsp_paidmedia_subscribes_daily_rollup.sql
@@ -1,7 +1,8 @@
 {% macro create_stg_supermetrics_yahoo_dsp_paidmedia_subscribes_daily_rollup(
-    reference_name='stg_supermetrics_yahoo_dsp_paidmedia_ad_summary_by_date_unioned') %}
+       source_name='supermetrics_yahoo_dsp_paidmedia',
+       source_table_name='alldates_VDSP_AD') %}
 SELECT DISTINCT  SAFE_CAST(ad_summary_by_date.message_id AS STRING) AS message_id,
   SAFE_CAST(ad_summary_by_date.date AS TIMESTAMP) AS date_timestamp,
   SAFE_CAST(NULL AS INTEGER) AS subscribes
-FROM  {{ ref(reference_name) }} ad_summary_by_date
+FROM   {{ source(source_name,source_table_name) }}  ad_summary_by_date
 {% endmacro %}
