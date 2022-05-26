9ac36474e463226ef16824339213d2c385163ef7
Frydafly <fryda.guedes@gmail.com>
2022-05-26 12:36:25 -0500

    fix a comma

--- macros/frakture_global_message_email/staging/stg_frakture_global_message_email_unsubscribes_rollup.sql
+++ macros/frakture_global_message_email/staging/stg_frakture_global_message_email_unsubscribes_rollup.sql
@@ -1,6 +1,6 @@
 {% macro create_stg_frakture_global_message_email_unsubscribes_rollup(
     reference_name='stg_frakture_global_message_email_summary_by_date') %}
 SELECT DISTINCT SAFE_CAST(message_id AS STRING) AS message_id
- SAFE_CAST(unsubscribes AS INT) AS unsubscribes,
+ SAFE_CAST(unsubscribes AS INT) AS unsubscribes
 FROM {{ ref(reference_name) }} 
 {% endmacro %}
