51167ef9dc1b641924304b35c385c27cfd4665f6
Frydafly <fryda.guedes@gmail.com>
2022-05-26 12:42:17 -0500

    delete everyaction email

--- macros/frakture_everyaction_email/staging/stg_frakture_everyaction_email_clicks_rollup.sql
+++
@@ -1,6 +0,0 @@
-{% macro create_stg_frakture_everyaction_email_clicks_rollup(
-    reference_name='stg_frakture_everyaction_email_summary_unioned') %}
-SELECT SAFE_CAST(message_id AS STRING) AS message_id,
-  SAFE_CAST(email_summary.email_clicked AS INT) AS clicks
-FROM  {{ ref(reference_name) }} email_summary
-{% endmacro %}
--- macros/frakture_everyaction_email/staging/stg_frakture_everyaction_email_recipients_rollup.sql
+++
@@ -1,6 +0,0 @@
-{% macro create_stg_frakture_everyaction_email_recipients_rollup(
-    reference_name='stg_frakture_everyaction_email_summary_unioned') %}
-SELECT SAFE_CAST(message_id AS STRING) AS message_id,
-  SAFE_CAST(email_summary.email_sent AS INT) AS recipients
-FROM  {{ ref(reference_name) }} email_summary
-{% endmacro %}
