6d8571e18ba87ba1cb9c05294327a52ae974e54b
Frydafly <fryda.guedes@gmail.com>
2022-05-26 12:05:42 -0500

    delete other email structure

--- macros/email_everyaction/staging/stg_email_recipients_rollup_unioned.sql
+++
@@ -1,4 +0,0 @@
-{% macro create_stg_email_recipients_rollup_unioned() %}
-{% set relations = dbt_arc_functions.relations_that_match_regex('^stg_.*_email_recipients_rollup$') %}
-{{ dbt_utils.union_relations(relations) }}
-{% endmacro %}
