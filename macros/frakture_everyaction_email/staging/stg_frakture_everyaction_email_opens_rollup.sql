e250d1dc53f2912fb6b23c5423fd74c7d44eead6
Timothy Ryan <tryan@bluestate.co>
2022-05-27 14:51:08 -0400

    Revert "delete everyaction email"
    This reverts commit 51167ef9dc1b641924304b35c385c27cfd4665f6.

---
+++ macros/frakture_everyaction_email/staging/stg_frakture_everyaction_email_opens_rollup.sql
@@ -0,0 +1,6 @@
+{% macro create_stg_frakture_everyaction_email_opens_rollup(
+    reference_name='stg_frakture_everyaction_email_summary_unioned') %}
+SELECT SAFE_CAST(message_id AS STRING) AS message_id,
+  SAFE_CAST(email_summary.email_opened AS INT) AS opens
+FROM  {{ ref(reference_name) }} email_summary
+{% endmacro %}
