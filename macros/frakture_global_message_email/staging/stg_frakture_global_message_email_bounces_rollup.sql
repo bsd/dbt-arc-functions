cfeaa69686e2397367628426469a0dbfc483a633
Frydafly <fryda.guedes@gmail.com>
2022-05-27 10:28:25 -0500

    commas

--- macros/frakture_global_message_email/staging/stg_frakture_global_message_email_bounces_rollup.sql
+++ macros/frakture_global_message_email/staging/stg_frakture_global_message_email_bounces_rollup.sql
@@ -1,6 +1,6 @@
 {% macro create_stg_frakture_global_message_email_bounces_rollup(
     reference_name='stg_frakture_global_message_email_summary_by_date') %}
-SELECT DISTINCT SAFE_CAST(message_id AS STRING) AS message_id
+SELECT DISTINCT SAFE_CAST(message_id AS STRING) AS message_id,
  SAFE_CAST(hard_bounces + soft_bounces AS INT) AS total_bounces,
   SAFE_CAST(0 AS INT) AS block_bounces,
   SAFE_CAST(0 AS INT) AS tech_bounces,
--- macros/frakture_global_message_email/staging/stg_frakture_global_message_email_recipients_rollup.sql
+++ macros/frakture_global_message_email/staging/stg_frakture_global_message_email_recipients_rollup.sql
@@ -1,6 +1,6 @@
 {% macro create_stg_frakture_global_message_email_recipients_rollup(
     reference_name='stg_frakture_global_message_email_summary_by_date') %}
-SELECT DISTINCT SAFE_CAST(message_id AS STRING) AS message_id
+SELECT DISTINCT SAFE_CAST(message_id AS STRING) AS message_id,
   SAFE_CAST(sent AS INT) AS recipients
 FROM {{ ref(reference_name) }} 
 {% endmacro %}
