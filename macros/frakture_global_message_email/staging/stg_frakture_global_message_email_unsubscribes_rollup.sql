41c8624ab52d2e60a604666dad728e1c48b9ae29
fguedes@bluestate.co <fguedes@bluestate.co>
2022-07-19 19:58:39 +0000

    another one

--- macros/frakture_global_message_email/staging/stg_frakture_global_message_email_unsubscribes_rollup.sql
+++ macros/frakture_global_message_email/staging/stg_frakture_global_message_email_unsubscribes_rollup.sql
@@ -1,7 +1,7 @@
 {% macro create_stg_frakture_global_message_email_unsubscribes_rollup(
     reference_name='stg_frakture_global_message_email_summary_by_date') %}
 SELECT DISTINCT SAFE_CAST(message_id AS STRING) AS message_id,
-SAFE_CAST(ad_summary.date AS TIMESTAMP) AS date_timestamp,
+SAFE_CAST(publish_date AS TIMESTAMP) AS date_timestamp,
  SAFE_CAST(unsubscribes AS INT) AS unsubscribes
 FROM {{ ref(reference_name) }} 
 {% endmacro %}
