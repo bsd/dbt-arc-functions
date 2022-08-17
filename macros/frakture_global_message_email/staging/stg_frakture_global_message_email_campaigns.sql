5bd3eba9772aaa00ffa54f8b3ad6ea950338360e
Frydafly <fryda.guedes@gmail.com>
2022-08-17 13:53:02 -0500

    account prefix is correct source code field

--- macros/frakture_global_message_email/staging/stg_frakture_global_message_email_campaigns.sql
+++ macros/frakture_global_message_email/staging/stg_frakture_global_message_email_campaigns.sql
@@ -2,7 +2,7 @@
     reference_name='stg_frakture_global_message_email_summary') %}
 SELECT DISTINCT SAFE_CAST(message_id AS STRING) AS message_id
   , SAFE_CAST(bot_nickname as STRING) as crm_entity
-  , SAFE_CAST(affiliation as STRING) as source_code_entity
+  , SAFE_CAST(account_prefix as STRING) as source_code_entity
   ,SAFE_CAST(audience as STRING) as audience
 FROM {{ ref(reference_name) }}
 {% endmacro %}
