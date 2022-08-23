216adfaf622d6ed8524fd0aac65a0dca674ee2d8
Frydafly <fryda.guedes@gmail.com>
2022-08-23 14:34:04 -0500

    case when to coalesce

--- macros/frakture_global_message_email/staging/stg_frakture_global_message_email_campaign_dates_rollup.sql
+++ macros/frakture_global_message_email/staging/stg_frakture_global_message_email_campaign_dates_rollup.sql
@@ -1,10 +1,7 @@
 {% macro create_stg_frakture_global_message_email_campaign_dates_rollup(
     reference_name='stg_frakture_global_message_email_campaign_dates') %}
 SELECT 
-  case when crm_campaign is not null 
-                then crm_campaign 
-                else source_code_campaign END 
-                AS campaign_name,
+  COALESCE(crm_campaign,source_code_campaign) AS campaign_name,
   MIN(campaign_timestamp) as campaign_start_timestamp,
   MAX(campaign_timestamp) as campaign_latest_timestamp
 FROM {{ ref(reference_name) }}
--- macros/frakture_global_message_email/staging/stg_frakture_global_message_email_campaigns_rollup.sql
+++ macros/frakture_global_message_email/staging/stg_frakture_global_message_email_campaigns_rollup.sql
@@ -1,13 +1,9 @@
 {% macro create_stg_frakture_global_message_email_campaigns_rollup(
     reference_name='stg_frakture_global_message_email_campaigns') %}
 SELECT DISTINCT message_id,
-crm_entity,
-source_code_entity,
-audience,
-case when crm_campaign is not null 
-      then crm_campaign 
-      else source_code_campaign END 
-      AS campaign_name
+    crm_entity,
+    source_code_entity,
+    audience,
+    COALESCE(crm_campaign,source_code_campaign) AS campaign_name
 FROM {{ ref(reference_name) }}
-
 {% endmacro %}
