54dc2936cad6bdda1dc2c70076f3c944ff843555
Frydafly <fryda.guedes@gmail.com>
2022-08-23 13:05:25 -0500

    Update stg_frakture_global_message_email_campaigns.sql

--- macros/frakture_global_message_email/staging/stg_frakture_global_message_email_campaigns.sql
+++ macros/frakture_global_message_email/staging/stg_frakture_global_message_email_campaigns.sql
@@ -1,11 +1,11 @@
 {% macro create_stg_frakture_global_message_email_campaigns(
     reference_name='stg_frakture_global_message_email_summary') %}
-with base as (SELECT DISTINCT SAFE_CAST(message_id AS STRING) AS message_id,
+SELECT DISTINCT SAFE_CAST(message_id AS STRING) AS message_id,
   SAFE_CAST(bot_nickname as STRING) as crm_entity,
   SAFE_CAST(account_prefix as STRING) as source_code_entity,
   SAFE_CAST(audience as STRING) as audience,
   SAFE_CAST(campaign_name AS STRING) AS crm_campaign,
   SAFE_CAST(coalesce(campaign,campaign_label) AS STRING) AS source_code_campaign
-FROM {{ ref(reference_name) }})
+FROM {{ ref(reference_name) }}
 
 {% endmacro %}
