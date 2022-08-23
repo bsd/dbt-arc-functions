fba556c5b7445a67ce70c846f7fed86efd539be4
Frydafly <fryda.guedes@gmail.com>
2022-08-23 12:05:00 -0500

    Update stg_frakture_global_message_email_campaigns.sql
    missing comma

--- macros/frakture_global_message_email/staging/stg_frakture_global_message_email_campaigns.sql
+++ macros/frakture_global_message_email/staging/stg_frakture_global_message_email_campaigns.sql
@@ -1,6 +1,6 @@
 {% macro create_stg_frakture_global_message_email_campaigns(
     reference_name='stg_frakture_global_message_email_summary') %}
-SELECT DISTINCT SAFE_CAST(message_id AS STRING) AS message_id
+SELECT DISTINCT SAFE_CAST(message_id AS STRING) AS message_id,
   SAFE_CAST(bot_nickname as STRING) as crm_entity,
   SAFE_CAST(account_prefix as STRING) as source_code_entity,
   SAFE_CAST(audience as STRING) as audience,
