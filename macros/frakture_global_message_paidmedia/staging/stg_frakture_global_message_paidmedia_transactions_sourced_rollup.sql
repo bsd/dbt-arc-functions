f19ff4193530657248b1cbaaed81f1c4bc4487f8
Frydafly <fryda.guedes@gmail.com>
2022-08-30 14:52:02 -0500

    new monthly revenue!!

--- macros/frakture_global_message_paidmedia/staging/stg_frakture_global_message_paidmedia_transactions_sourced_rollup.sql
+++ macros/frakture_global_message_paidmedia/staging/stg_frakture_global_message_paidmedia_transactions_sourced_rollup.sql
@@ -7,8 +7,8 @@ SELECT SAFE_CAST(ad_summary.message_id AS STRING) AS message_id,
     SAFE_CAST(ad_summary.origin_person_count AS int) AS total_donors,  -- doesn't seem available in Frakture ad_summary tables
     SAFE_CAST(ad_summary.attributed_revenue - ad_summary.attributed_recurring_revenue AS numeric) AS one_time_revenue,
     SAFE_CAST(ad_summary.attributed_transactions - ad_summary.attributed_recurring_transactions  AS int) AS one_time_gifts,
-    SAFE_CAST(NULL  AS numeric) AS new_monthly_revenue, -- doesn't seem available in Frakture ad_summary tables
-    SAFE_CAST(NULL  AS int) AS new_monthly_gifts, -- doesn't seem available in Frakture ad_summary tables
+    SAFE_CAST(attributed_recurring_revenue  AS numeric) AS total_monthly_revenue,
+    SAFE_CAST(attributed_recurring_transactions  AS int) AS total_monthly_gifts,
     SAFE_CAST(ad_summary.attributed_recurring_revenue  AS numeric) AS total_monthly_revenue,
     SAFE_CAST(ad_summary.attributed_recurring_transactions  AS int) AS total_monthly_gifts,
     SAFE_CAST(ad_summary.campaign  AS STRING) AS campaign,
