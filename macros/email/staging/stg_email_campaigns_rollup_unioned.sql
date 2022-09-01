afad5ef596cf58997b999d0c3ba992b5681872fe
Frydafly <fryda.guedes@gmail.com>
2022-09-01 14:35:14 -0500

    overhaul of campaign dates reporting

--- macros/email/staging/stg_email_campaigns_rollup_unioned.sql
+++ macros/email/staging/stg_email_campaigns_rollup_unioned.sql
@@ -1,4 +1,4 @@
 {% macro create_stg_email_campaigns_rollup_unioned() %}
-{% set relations = dbt_arc_functions.relations_that_match_regex('^stg_.*_email_campaigns_rollup$') %}
+{% set relations = dbt_arc_functions.relations_that_match_regex('^stg_.*_email_campaigns_and_dates_rollup_join$') %}
 {{ dbt_utils.union_relations(relations) }}
 {% endmacro %}
---
+++ macros/frakture_global_message_email/staging/stg_frakture_global_message_email_campaigns_and_dates_rollup_join.sql
@@ -0,0 +1,17 @@
+{% macro create_stg_frakture_global_message_email_campaigns_and_dates_rollup_join(
+    campaigns='stg_frakture_global_message_email_campaigns_rollup'
+    campaign_dates='stg_frakture_global_message_email_campaign_dates_rollup'
+    ) %}
+SELECT DISTINCT campaigns.message_id,
+    campaigns.crm_entity,
+    campaigns.source_code_entity,
+    campaigns.audience,
+    campaigns.recurtype,
+    campaigns.campaign_category,
+    campaigns.campaign_name,
+    campaign_dates.campaign_start_timestamp,
+    campaign_dates.campaign_latest_timestamp
+FROM {{ ref(campaigns) }} campaigns
+FULL JOIN {{ ref(campaign_dates) }} campaign_dates
+USING (campaign_name)
+{% endmacro %}
