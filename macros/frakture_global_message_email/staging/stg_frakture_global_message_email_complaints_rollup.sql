d7c630db35401d021ff3c5e8ddb4bcdd32038564
Derrick Mondowney <109629735+dmbluestate@users.noreply.github.com>
2022-09-21 22:13:32 -0400

    add complaints to email models

---
+++ macros/frakture_global_message_email/staging/stg_frakture_global_message_email_complaints_rollup.sql
@@ -0,0 +1,7 @@
+{% macro create_stg_frakture_global_message_email_actions_rollup(
+    reference_name='stg_frakture_global_message_email_summary') %}
+SELECT SAFE_CAST(message_id AS STRING) AS message_id,  
+  SUM(SAFE_CAST(complaints AS INT)) AS complaints
+FROM {{ ref(reference_name) }} 
+GROUP BY 1
+{% endmacro %}
