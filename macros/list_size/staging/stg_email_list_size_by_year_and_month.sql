42893e566598405653f8a8036d41b85fdc8fd142
Frydafly <fryda.guedes@gmail.com>
2022-09-22 11:19:52 -0500

    list size models made

---
+++ macros/list_size/staging/stg_email_list_size_by_year_and_month.sql
@@ -0,0 +1,14 @@
+{% macro create_stg_email_list_size_year_and_month(
+    reference_name='mart_email_performance_with_revenue'
+) %}
+SELECT
+extract(YEAR from best_guess_timestamp) as extract_year,
+extract(MONTH from best_guess_timestamp) as extract_month,
+MAX(recipients) AS max_recipients,
+MAX(recipients - total_bounces) AS max_delivered
+FROM  {{ ref(reference_name) }} mart_email
+GROUP BY 1
+
+{% endmacro %}
+
+
