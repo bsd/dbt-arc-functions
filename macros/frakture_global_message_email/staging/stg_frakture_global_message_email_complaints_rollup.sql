429c36d184884abdd518fc4b017c83d06b576e14
Derrick Mondowney <109629735+dmbluestate@users.noreply.github.com>
2022-09-21 22:41:31 -0400

    correct typo

--- macros/frakture_global_message_email/staging/stg_frakture_global_message_email_complaints_rollup.sql
+++ macros/frakture_global_message_email/staging/stg_frakture_global_message_email_complaints_rollup.sql
@@ -1,4 +1,4 @@
-{% macro create_stg_frakture_global_message_email_actions_rollup(
+{% macro create_stg_frakture_global_message_email_complaints_rollup(
     reference_name='stg_frakture_global_message_email_summary') %}
 SELECT SAFE_CAST(message_id AS STRING) AS message_id,  
   SUM(SAFE_CAST(complaints AS INT)) AS complaints
