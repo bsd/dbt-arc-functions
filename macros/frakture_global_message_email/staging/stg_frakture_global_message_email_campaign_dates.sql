cab62e5e0fd41c3ca75969f9a3d216e15fbe0434
Tim Ryan <ryantimjohn@gmail.com>
2022-09-26 17:34:39 -0400

    change campaign to campaign_name

--- macros/frakture_global_message_email/staging/stg_frakture_global_message_email_campaign_dates.sql
+++ macros/frakture_global_message_email/staging/stg_frakture_global_message_email_campaign_dates.sql
@@ -3,6 +3,6 @@
 SELECT 
   SAFE_CAST(publish_date as TIMESTAMP) as campaign_timestamp,
   SAFE_CAST(campaign_name AS STRING) AS crm_campaign,
-  SAFE_CAST(coalesce(campaign,campaign_label) AS STRING) AS source_code_campaign
+  SAFE_CAST(coalesce(campaign_name,campaign_label) AS STRING) AS source_code_campaign
 FROM {{ ref(reference_name) }}
 {% endmacro %}
