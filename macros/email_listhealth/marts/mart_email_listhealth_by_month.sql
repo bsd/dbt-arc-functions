de232c60012b36e724fc51f95a7be887e59b7d76
Frydafly <fryda.guedes@gmail.com>
2022-09-27 17:44:58 -0500

    missing comma i always do this

--- macros/email_listhealth/marts/mart_email_listhealth_by_month.sql
+++ macros/email_listhealth/marts/mart_email_listhealth_by_month.sql
@@ -5,8 +5,7 @@ SELECT
 SAFE_CAST(concat_date AS DATE) AS date_month,
 SAFE_CAST(max_recipients AS INT) AS max_recipients,
 SAFE_CAST(max_delivered AS INT) AS max_delivered,
-
-SAFE_CAST(max_delivered AS INT) - SAFE_CAST(delivered_prev_month as INT) as diff_delivered_prev_month
+SAFE_CAST(max_delivered AS INT) - SAFE_CAST(delivered_prev_month as INT) as diff_delivered_prev_month,
 SAFE_CAST(total_unsubscribes AS INT) AS total_unsubscribes,
 SAFE_CAST(total_hard_bounces AS INT) AS total_hard_bounces,
 SAFE_CAST(total_complaints AS INT) AS total_complaints 
