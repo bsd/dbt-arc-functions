83bc8e67d8d5f88528168ac1b88534cbd0d1aaea
Frydafly <fryda.guedes@gmail.com>
2022-09-27 17:46:44 -0500

    needs a thing

--- macros/email_listhealth/marts/mart_email_listhealth_by_month.sql
+++ macros/email_listhealth/marts/mart_email_listhealth_by_month.sql
@@ -10,7 +10,6 @@ SAFE_CAST(total_unsubscribes AS INT) AS total_unsubscribes,
 SAFE_CAST(total_hard_bounces AS INT) AS total_hard_bounces,
 SAFE_CAST(total_complaints AS INT) AS total_complaints 
 FROM  {{ ref(reference_name) }} mart_email
-WHERE concat_date IS NOT NULL
 
 {% endmacro %}
 
