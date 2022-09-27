5328adb0d668a85339b7dfc0567103f2117e52a5
Frydafly <fryda.guedes@gmail.com>
2022-09-27 17:19:52 -0500

    previous month work

--- macros/email_listhealth/marts/mart_email_listhealth_by_month.sql
+++ macros/email_listhealth/marts/mart_email_listhealth_by_month.sql
@@ -5,18 +5,12 @@ SELECT
 SAFE_CAST(concat_date AS DATE) AS date_month,
 SAFE_CAST(max_recipients AS INT) AS max_recipients,
 SAFE_CAST(max_delivered AS INT) AS max_delivered,
-LAG(max_delivered) AS delivered_previous_month,
-max_delivered - LAG(max_delivered)
-    OVER (ORDER BY date_month) AS diff_delivered_previous_month,
+SAFE_CAST(max_delivered AS INT) - SAFE_CAST(delivered_prev_month as INT) as diff_delivered_prev_month
 SAFE_CAST(total_unsubscribes AS INT) AS total_unsubscribes,
 SAFE_CAST(total_hard_bounces AS INT) AS total_hard_bounces,
 SAFE_CAST(total_complaints AS INT) AS total_complaints 
 FROM  {{ ref(reference_name) }} mart_email
 WHERE concat_date IS NOT NULL
-ORDER BY 1 DESC
 
 {% endmacro %}
-
-
-
 
