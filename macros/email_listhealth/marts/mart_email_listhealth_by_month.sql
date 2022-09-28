0b73fb6a710e7588e421933e9c6dd47822cfe94c
Frydafly <fryda.guedes@gmail.com>
2022-09-28 14:59:32 -0500

    big change

--- macros/email_listhealth/marts/mart_email_listhealth_by_month.sql
+++ macros/email_listhealth/marts/mart_email_listhealth_by_month.sql
@@ -2,13 +2,13 @@
     reference_name='stg_email_listhealth_with_prev_delivered'
 ) %}
 SELECT
-SAFE_CAST(concat_date AS DATE) AS date_month,
-SAFE_CAST(max_recipients AS INT) AS max_recipients,
-SAFE_CAST(max_delivered AS INT) AS max_delivered,
+date_month,
+max_recipients,
+max_delivered,
 SAFE_CAST(max_delivered AS INT) - SAFE_CAST(delivered_prev_month as INT) as diff_delivered_prev_month,
-SAFE_CAST(total_unsubscribes AS INT) AS total_unsubscribes,
-SAFE_CAST(total_hard_bounces AS INT) AS total_hard_bounces,
-SAFE_CAST(total_complaints AS INT) AS total_complaints 
+total_unsubscribes,
+total_hard_bounces,
+total_complaints 
 FROM  {{ ref(reference_name) }} mart_email
 
 {% endmacro %}
