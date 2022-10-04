de0da84b5ab7832e5c0811875b451a86239b4624
Frydafly <fryda.guedes@gmail.com>
2022-10-04 10:45:00 -0500

    create test_group from variant and message_set

--- macros/frakture_global_message_email/staging/stg_frakture_global_message_email_campaigns.sql
+++ macros/frakture_global_message_email/staging/stg_frakture_global_message_email_campaigns.sql
@@ -5,7 +5,9 @@ SELECT DISTINCT SAFE_CAST(message_id AS STRING) AS message_id,
   SAFE_CAST(account_prefix as STRING) as source_code_entity,
   SAFE_CAST(audience as STRING) as audience,
   SAFE_CAST(recurtype as STRING) as recurtype,
-  SAFE_CAST(message_set as STRING) as test_group, 
+  CASE WHEN variant IS NOT NULL then SAFE_CAST(message_set as STRING) 
+  WHEN variant IS NULL then NULL end
+  as test_group, 
   SAFE_CAST(message_set as STRING) as campaign_category,
   SAFE_CAST(campaign_name AS STRING) AS crm_campaign,
   SAFE_CAST(coalesce(campaign,campaign_label) AS STRING) AS source_code_campaign
