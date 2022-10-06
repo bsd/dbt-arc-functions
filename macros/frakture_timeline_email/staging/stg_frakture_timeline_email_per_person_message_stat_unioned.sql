67bc68e80ba905d4ee1a9734a9c773ff2d559faf
Frydafly <fryda.guedes@gmail.com>
2022-10-06 12:04:52 -0500

    better regex

--- macros/frakture_timeline_email/staging/stg_frakture_timeline_email_per_person_message_stat_unioned.sql
+++ macros/frakture_timeline_email/staging/stg_frakture_timeline_email_per_person_message_stat_unioned.sql
@@ -1,5 +1,5 @@
 {% macro create_stg_frakture_timeline_per_person_message_stat_unioned() %}
-{% set relations = dbt_arc_functions.relations_that_match_regex('[A-Za-z0-9]_per_person_message_stat$',
+{% set relations = dbt_arc_functions.relations_that_match_regex('^\w+([.-]?\w+)*_[A-Za-z0-9]{3}_per_person_message_stat$',
   is_source=True,
   source_name='frakture_timeline_email',
   schema_to_search='src_frakture') 
