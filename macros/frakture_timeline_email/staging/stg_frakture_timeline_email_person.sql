fbecce9aef9acce6b9ca5c0e827bb207c6dc8a39
Frydafly <fryda.guedes@gmail.com>
2022-10-06 11:15:51 -0500

    oops

--- macros/frakture_timeline_email/staging/stg_frakture_timeline_email_person.sql
+++ macros/frakture_timeline_email/staging/stg_frakture_timeline_email_person.sql
@@ -1,8 +1,10 @@
-{% macro create_stg_frakture_timeline_email_person_unioned() %}
+{% macro create_stg_frakture_timeline_email_person() %}
 {% set relations = dbt_arc_functions.relations_that_match_regex('[A-Za-z0-9]_person$',
   is_source=True,
   source_name='frakture_timeline_email',
   schema_to_search='src_frakture') 
 %}
-SELECT DISTINCT * FROM ({{ dbt_utils.union_relations(relations) }})
+
+SELECT DISTINCT * FROM {{ source('frakture_timeline_email',relations) }}
+
 {% endmacro %}
