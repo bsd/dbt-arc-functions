a5ad100fde79f3f227fe35b901bb0d8c4ec4774e
Frydafly <fryda.guedes@gmail.com>
2022-10-06 15:55:10 -0500

    fixed regex

--- macros/frakture_timeline_email/staging/stg_frakture_timeline_email_person_unioned.sql
+++ macros/frakture_timeline_email/staging/stg_frakture_timeline_email_person_unioned.sql
@@ -1,5 +1,5 @@
 {% macro create_stg_frakture_timeline_email_person_unioned() %}
-{% set relations = dbt_arc_functions.relations_that_match_regex('^\w+([.-]?\w+)*_[A-Za-z0-9]{3}_person$',
+{% set relations = dbt_arc_functions.relations_that_match_regex('^\w+([.-]?\w+)_[A-Za-z0-9]{3}_person$',
   is_source=True,
   source_name='frakture_timeline_email',
   schema_to_search='src_frakture') 
