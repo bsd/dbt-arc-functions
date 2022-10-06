3e26abedd3d24108e3a16eac781be450e11dd6db
Frydafly <fryda.guedes@gmail.com>
2022-10-06 15:38:58 -0500

    try var relations instead of union macro

--- macros/frakture_timeline_email/staging/stg_frakture_timeline_email_person_unioned.sql
+++ macros/frakture_timeline_email/staging/stg_frakture_timeline_email_person_unioned.sql
@@ -4,5 +4,5 @@
   source_name='frakture_timeline_email',
   schema_to_search='src_frakture') 
 %}
-{{ dbt_utils.union_relations(relations) }}
-{% endmacro %}
+SELECT DISTINCT * FROM {{ var("relations") }}
+WHERE remote_person_id IS NOT NULL
