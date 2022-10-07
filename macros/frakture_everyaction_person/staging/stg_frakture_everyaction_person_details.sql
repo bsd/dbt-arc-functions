0b28f85139497e67b5d98ea71a8cb029164c35c9
Frydafly <fryda.guedes@gmail.com>
2022-10-07 09:20:42 -0500

    correct label for macro

--- macros/frakture_everyaction_person/staging/stg_frakture_everyaction_person_details.sql
+++ macros/frakture_everyaction_person/staging/stg_frakture_everyaction_person_details.sql
@@ -1,4 +1,4 @@
-{% macro create_stg_frakture_everyaction_person_jobs(
+{% macro create_stg_frakture_everyaction_person_details(
     reference_name='stg_everyaction_person_table_unioned') %}
 SELECT DISTINCT SAFE_CAST(remote_person_id as INT) as person_id,
 SAFE_CAST(email_domain as VARCHAR) as email_domain
