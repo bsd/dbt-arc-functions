6448a7655c6ea5fd3e0b4833dab8c8b797d224cb
Frydafly <fryda.guedes@gmail.com>
2022-10-07 08:54:45 -0500

    create mart for everyaction to test first

--- macros/frakture_global_message_paidmedia/staging/stg_frakture_global_message_paidmedia_clicks_daily_rollup.sql
+++ macros/frakture_global_message_paidmedia/staging/stg_frakture_global_message_paidmedia_clicks_daily_rollup.sql
@@ -1,6 +1,7 @@
 {% macro create_stg_frakture_global_message_paidmedia_clicks_daily_rollup(
     reference_name='stg_frakture_global_message_paidmedia_ad_summary_by_date') %}
-SELECT DISTINCT SAFE_CAST(message_id AS STRING) AS message_id,  SAFE_CAST(ad_summary_by_date.date AS TIMESTAMP) AS date_timestamp,
+SELECT DISTINCT SAFE_CAST(message_id AS STRING) AS message_id,  
+SAFE_CAST(ad_summary_by_date.date AS TIMESTAMP) AS date_timestamp,
   SAFE_CAST(ad_summary_by_date.clicks AS INT) AS total_clicks,
   SAFE_CAST(ad_summary_by_date.clicks AS INT) AS unique_clicks
 FROM {{ ref(reference_name) }} ad_summary_by_date
