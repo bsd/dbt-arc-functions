2f41ac91887002419904d0c9f67bdb3f7f224724
Frydafly <fryda.guedes@gmail.com>
2022-10-07 10:21:26 -0500

    safecast to string instead of varchar

--- macros/frakture_everyaction_person/staging/stg_frakture_everyaction_person_details.sql
+++ macros/frakture_everyaction_person/staging/stg_frakture_everyaction_person_details.sql
@@ -1,6 +1,6 @@
 {% macro create_stg_frakture_everyaction_person_details(
     reference_name='stg_frakture_everyaction_person_table_unioned') %}
 SELECT DISTINCT SAFE_CAST(remote_person_id as INT) as person_id,
-SAFE_CAST(email_domain as VARCHAR) as email_domain
+SAFE_CAST(email_domain as STRING) as email_domain
 FROM  {{ ref(reference_name) }} 
 {% endmacro %}
