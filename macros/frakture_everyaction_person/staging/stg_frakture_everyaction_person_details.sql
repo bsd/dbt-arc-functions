9a8a9e22b421b6b8d9f7c71acbf3f84caa92ff79
Frydafly <fryda.guedes@gmail.com>
2022-10-07 09:22:45 -0500

    fix name

--- macros/frakture_everyaction_person/staging/stg_frakture_everyaction_person_details.sql
+++ macros/frakture_everyaction_person/staging/stg_frakture_everyaction_person_details.sql
@@ -1,5 +1,5 @@
 {% macro create_stg_frakture_everyaction_person_details(
-    reference_name='stg_everyaction_person_table_unioned') %}
+    reference_name='stg_frakture_everyaction_person_table_unioned') %}
 SELECT DISTINCT SAFE_CAST(remote_person_id as INT) as person_id,
 SAFE_CAST(email_domain as VARCHAR) as email_domain
 FROM  {{ ref(reference_name) }} 
