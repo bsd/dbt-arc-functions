a963717a8a63d4b8e832419c8ba87b8d62b3a79f
Frydafly <fryda.guedes@gmail.com>
2022-10-19 19:17:51 -0500

    fixed formatting

--- macros/frakture_global_message_paidmedia/staging/stg_frakture_global_message_paidmedia_campaigns.sql
+++ macros/frakture_global_message_paidmedia/staging/stg_frakture_global_message_paidmedia_campaigns.sql
@@ -1,15 +1,15 @@
 {% macro create_stg_frakture_global_message_paidmedia_campaigns(
     reference_name='stg_frakture_global_message_paidmedia_ad_summary_by_date') %}
-SELECT DISTINCT SAFE_CAST(campaign_id AS STRING) AS campaign_id
+SELECT SELECT DISTINCT SAFE_CAST(campaign_id AS STRING) AS campaign_id,
   SAFE_CAST(message_id AS STRING) AS message_id,
-    CASE WHEN REGEXP_CONTAINS(ad_summary.type,'(?i)search')=True THEN 'search'
-    CASE WHEN REGEXP_CONTAINS(ad_summary.type,'(?i)ad')=True THEN 'search'
-    WHEN REGEXP_CONTAINS(ad_summary.type,'(?i)display')=True THEN 'display'
-    WHEN REGEXP_CONTAINS(ad_summary.type,'(?i)video')=True THEN 'display'
+    CASE WHEN REGEXP_CONTAINS(type,'(?i)search')=True THEN 'search'
+    WHEN REGEXP_CONTAINS(type,'(?i)ad')=True THEN 'search'
+    WHEN REGEXP_CONTAINS(type,'(?i)display')=True THEN 'display'
+    WHEN REGEXP_CONTAINS(type,'(?i)video')=True THEN 'display'
     WHEN ad_summary.channel IN ('ad') THEN 'search'
     WHEN ad_summary.channel IN ('facebook_ad', 'promoted_tweet') THEN 'social'
     ELSE CONCAT(ad_summary.channel, ' - ', ad_summary.type) 
-    END AS channel_category,
+  END AS channel_category,
   SAFE_CAST(channel AS STRING) AS channel,
   SAFE_CAST(type AS STRING) AS channel_type,
   SAFE_CAST(campaign_name AS STRING) AS campaign_name,
