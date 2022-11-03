1cea6e634b13670afb6b8d49ff95216ad21e8ee1
Frydafly <fryda.guedes@gmail.com>
2022-11-03 12:43:22 -0500

    added campaigns, complaints, jobs

--- macros/frakture_actionkit_email/staging/stg_frakture_actionkit_email_jobs.sql
+++ macros/frakture_actionkit_email/staging/stg_frakture_actionkit_email_jobs.sql
@@ -1,7 +1,6 @@
 {% macro create_stg_frakture_actionkit_email_jobs(
     reference_name='stg_frakture_actionkit_email_summary_unioned') %}
 SELECT SAFE_CAST(message_id AS STRING) AS message_id,
-    SAFE_CAST(campaign_id AS STRING) AS campaign_id,
     SAFE_CAST(from_name AS STRING) AS from_name,
     SAFE_CAST(from_email AS STRING) AS from_email,
     SAFE_CAST(publish_date AS TIMESTAMP) AS best_guess_timestamp,
@@ -9,9 +8,8 @@ SELECT SAFE_CAST(message_id AS STRING) AS message_id,
     SAFE_CAST(NULL AS TIMESTAMP) AS pickup_timestamp,
     SAFE_CAST(NULL AS TIMESTAMP) AS delivered_timestamp,
     SAFE_CAST(label AS STRING) AS email_name,
-    SAFE_CAST(subject AS STRING) AS email_subject,
-    SAFE_CAST(campaign_label AS STRING) AS campaign_name,
-    SAFE_CAST(tag as STRING) as campaign_category
+    SAFE_CAST(subject AS STRING) AS email_subject, 
+    SAFE_CAST(final_primary_source_code AS STRING) as source_code
 FROM {{ ref(reference_name) }}
 
 {% endmacro %}
