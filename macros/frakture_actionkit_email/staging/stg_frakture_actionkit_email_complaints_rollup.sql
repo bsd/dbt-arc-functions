d3f09ac5ecbb307b93bd479b2cd8a4d4e7ca548d
Frydafly <fryda.guedes@gmail.com>
2022-11-03 15:25:55 -0500

    yes

--- macros/frakture_actionkit_email/staging/stg_frakture_actionkit_email_complaints_rollup.sql
+++ macros/frakture_actionkit_email/staging/stg_frakture_actionkit_email_complaints_rollup.sql
@@ -1,5 +1,5 @@
 {% macro create_stg_frakture_actionkit_email_complaints_rollup(
-    reference_name='stg_frakture_actionkit_email_summary') %}
+    reference_name='stg_frakture_actionkit_email_summary_unioned') %}
 SELECT SAFE_CAST(message_id AS STRING) AS message_id,  
   SUM(SAFE_CAST(email_spam_complaints AS INT)) AS complaints
 FROM {{ ref(reference_name) }} 
