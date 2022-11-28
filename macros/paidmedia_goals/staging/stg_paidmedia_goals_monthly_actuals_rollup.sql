070f90730e2837611a61028dc68d5e1f51571073
Frydafly <fryda.guedes@gmail.com>
2022-11-28 13:31:36 -0600

    safe casting

--- macros/paidmedia_goals/staging/stg_paidmedia_goals_monthly_actuals_rollup.sql
+++ macros/paidmedia_goals/staging/stg_paidmedia_goals_monthly_actuals_rollup.sql
@@ -2,14 +2,14 @@
     reference_name='mart_paidmedia_daily_revenue_performance') %}
 SELECT
      FORMAT_DATE('%B %Y', CAST(date_timestamp AS date)) as month_year,
-     channel_category as channel,
-      objective,
-      case when channel_type is not null then channel || ' ' || lower(channel_type) 
-      else channel end
+     SAFE_CAST(lower(channel_category) as STRING) as channel,
+      SAFE_CAST(lower(objective) as string) as objective,
+      case when channel_type is not null then SAFE_CAST(lower(channel) || ' ' || lower(channel_type) as string)
+      else SAFE_CAST(lower(channel) as string) end
       as platform,
-      SUM(spend_amount) as actual_spend,
-      SUM(total_revenue) as actual_revenue,
-      SUM(total_gifts) as actual_donations
+      SAFE_CAST(SUM(spend_amount) as float64) as actual_spend,
+      SAFE_CAST(SUM(total_revenue) as float64) as actual_revenue,
+      SAFE_CAST(SUM(total_gifts) as float64) as actual_donations
 FROM {{ ref(reference_name) }} 
 GROUP BY 1, 2, 3, 4
 {% endmacro %}
