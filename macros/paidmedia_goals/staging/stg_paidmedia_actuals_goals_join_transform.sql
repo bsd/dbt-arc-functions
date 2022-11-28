8ebe26df5d76282bdc5e0c6e7cad1ee7059bb0a4
Frydafly <fryda.guedes@gmail.com>
2022-11-28 14:07:42 -0600

    fixed case when statements

--- macros/paidmedia_goals/staging/stg_paidmedia_actuals_goals_join_transform.sql
+++ macros/paidmedia_goals/staging/stg_paidmedia_actuals_goals_join_transform.sql
@@ -3,7 +3,7 @@
 SELECT
      month_year,
      case when regexp_contains(channel, 'soc') = true then 'Social'
-     case when regexp_contains(platform, 'soc') = true then 'Social'
+     when regexp_contains(platform, 'soc') = true then 'Social'
      when regexp_contains(channel, 'sear') = true then 'Search'
      when regexp_contains(platform, 'sear') = true then 'Search'
      when regexp_contains(channel, 'disp') = true then 'Display'
@@ -18,14 +18,14 @@ SELECT
     when regexp_contains(objective, 'fun') = true then 'Fundraising'
     else objective end as objective,
       case when regexp_contains(platform, 'goog') = true then 'Google'
-      case when regexp_contains(platform, 'bing') = true then 'Bing'
-      case when regexp_contains(platform, 'facebook') = true then 'Facebook'
-      case when regexp_contains(platform, 'fb') = true then 'Facebook'
-      case when regexp_contains(platform, 'instagram') = true then 'Facebook'
-      case when regexp_contains(platform, 'youtube') = true then 'Google'
-      case when regexp_contains(platform, 'yt') = true then 'Google'
-      case when regexp_contains(platform, 'linked') = true then 'LinkedIn'
-      case when regexp_contains(platform, 'yahoo') = true then 'Yahoo'
+      when regexp_contains(platform, 'bing') = true then 'Bing'
+      when regexp_contains(platform, 'facebook') = true then 'Facebook'
+      when regexp_contains(platform, 'fb') = true then 'Facebook'
+      when regexp_contains(platform, 'instagram') = true then 'Facebook'
+      when regexp_contains(platform, 'youtube') = true then 'Google'
+      when regexp_contains(platform, 'yt') = true then 'Google'
+      when regexp_contains(platform, 'linked') = true then 'LinkedIn'
+      when regexp_contains(platform, 'yahoo') = true then 'Yahoo'
         else platform end as platform,
       sum(actual_spend) as actual_spend,
       sum(actual_revenue) as actual_revenue,
