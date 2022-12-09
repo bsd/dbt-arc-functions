95978480494e8aa2068e8c1a1ffd2808c847ae39
Frydafly <fryda.guedes@gmail.com>
2022-12-09 13:48:37 -0600

    added some date features

--- macros/paidmedia_pacing/marts/mart_paidmedia_adhoc_pacing_actuals.sql
+++ macros/paidmedia_pacing/marts/mart_paidmedia_adhoc_pacing_actuals.sql
@@ -1,7 +1,25 @@
 {% macro create_mart_paidmedia_adhoc_pacing_actuals(
-    reference_name='mart_paidmedia_adhoc_pacing_actuals_rollup') %}
+    reference_name='stg_paidmedia_adhoc_pacing_actuals_rollup') %}
 
-SELECT DISTINCT * FROM {{ref('reference_name')}}
+SELECT 
+date_day,
+date_trunc(date_day, WEEK) as date_week_sunday,
+date_trunc(date_day, WEEK(MONDAY)) as date_week_monday,
+date_trunc(date_day, ISOWEEK) as date_isoweek,
+date_trunc(date_day, MONTH) as date_month,
+date_trunc(date_day, QUARTER) as date_quarter,
+date_truc(date_day, YEAR) as date_year,
+best_guess_entity,
+objective,
+channel,
+channel_type,
+platform,
+campaign_name,
+audience
+actual_spend,
+actual_revenue
+
+ FROM {{ref('reference_name')}}
 
 
 {% endmacro %}
