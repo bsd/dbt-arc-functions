252cbb7de1abe038f85c4758d9a9aa5a36c2d9d1
Frydafly <fryda.guedes@gmail.com>
2023-01-06 12:23:12 -0600

    consistency

--- macros/paidmedia_pacing/staging/stg_paidmedia_pacing_budget_with_datespine.sql
+++ macros/paidmedia_pacing/staging/stg_paidmedia_pacing_budget_with_datespine.sql
@@ -3,12 +3,10 @@
 with base as (
 
 SELECT 
--- create date_day field that generates date array from start_date to end_date
 generate_date_array(campaign_start_date, campaign_end_date) as date_day,
 campaign_name,
--- divide budget by days_in_campaign to get daily budget
 SAFE_DIVIDE(budget , date_diff(campaign_end_date, campaign_start_date, day)) as daily_budget,
-descriptions -- duplicate description for each day
+descriptions 
 FROM {{ ref('stg_adhoc_google_spreadsheets_paidmedia_pacing_budget') }}
 
 )
@@ -22,7 +20,7 @@ daily_budget
 -- cross join with unnested date_day array to create a row for each day
 CROSS JOIN UNNEST(date_day) as date_day
 -- filter out dates that are outside of the campaign start and end dates
-WHERE date_day >= start_date AND date_day <= end_date
+WHERE date_day >= campaign_start_date AND date_day <= campaign_end_date
 
 
 {% endmacro %} 
