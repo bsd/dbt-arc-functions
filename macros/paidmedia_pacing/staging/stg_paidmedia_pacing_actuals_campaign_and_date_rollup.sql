022bcba32212cc9029e7005d9a755a36c6284483
Frydafly <fryda.guedes@gmail.com>
2023-01-17 12:48:47 -0600

    unrecognized name -- actuals

--- macros/paidmedia_pacing/staging/stg_paidmedia_pacing_actuals_campaign_and_date_rollup.sql
+++ macros/paidmedia_pacing/staging/stg_paidmedia_pacing_actuals_campaign_and_date_rollup.sql
@@ -4,7 +4,7 @@ SELECT
 safe_cast(date_timestamp as date) as date_day,
 lower(safe_cast(campaign_name as string)) as campaign_name,
 sum(spend_amount) as daily_spend,
-sum(actuals.daily_spend) over (partition by campaign_name order by date_day) as cumulative_spend
+sum(daily_spend) over (partition by campaign_name order by date_day) as cumulative_spend
 
 FROM {{ref(reference_name)}}
 GROUP BY 1, 2
