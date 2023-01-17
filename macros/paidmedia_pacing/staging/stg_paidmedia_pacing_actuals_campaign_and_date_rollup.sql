9091cf11461b8c69f2b128d48961d0d15de63d4d
Frydafly <fryda.guedes@gmail.com>
2023-01-17 13:02:55 -0600

    CTE to improve functions

--- macros/paidmedia_pacing/staging/stg_paidmedia_pacing_actuals_campaign_and_date_rollup.sql
+++ macros/paidmedia_pacing/staging/stg_paidmedia_pacing_actuals_campaign_and_date_rollup.sql
@@ -1,12 +1,22 @@
 {% macro create_stg_paidmedia_pacing_actuals_campaign_and_date_rollup(
     reference_name='mart_paidmedia_daily_revenue_performance') %}
+
+with safe_casting as (
 SELECT
 safe_cast(date_timestamp as date) as date_day,
 lower(safe_cast(campaign_name as string)) as campaign_name,
+safe_cast(spend_amount as int) as spend_amount
+FROM {{ref(reference_name)}}
+
+)
+
+SELECT
+date_day,
+campaign_name,
 sum(spend_amount) as daily_spend,
 sum(spend_amount) over (partition by campaign_name order by date_day) as cumulative_spend
 
-FROM {{ref(reference_name)}}
+FROM safe_casting
 GROUP BY 1, 2
 
 {% endmacro %}
