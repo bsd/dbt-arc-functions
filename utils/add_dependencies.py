9a4bde3beceb2eb5bcce4ac5066929c8cb85d902
Frydafly <fryda.guedes@gmail.com>
2023-01-28 13:48:58 -0600

    working with click and also ensuring dependencies are installed before running

--- utils/add_dependencies.py
+++ utils/add_dependencies.py
@@ -1,19 +1,24 @@
-#!/usr/bin/env python3
-# coding: utf-8
-
-import re
 import subprocess
+import click
 from os import path
 
-
-def main(dbt_base_path=""):
+@click.command()
+@click.option('--dbt_base_path', default='', help='The base directory of your dbt project as an absolute path')
+def main(dbt_base_path):
+    # Check if dbt is installed on the system
+    try:
+        subprocess.run(["dbt", "--version"], check=True)
+    except subprocess.CalledProcessError:
+        click.echo("Error: dbt command not found. Please install dbt before running this script.")
+        return
+        
     if not dbt_base_path:
-        dbt_base_path = input("Please enter the base directory of your dbt project as an absolute path:\n")
+        dbt_base_path = click.prompt("Please enter the base directory of your dbt project as an absolute path")
     bash_command = f"cd {dbt_base_path} ;dbt run"
     matches = [["dependency", "filename"]]
     process = subprocess.run(f"cd {dbt_base_path} ;dbt deps", capture_output=True, shell=True)
     output = process.stdout.decode()
-    print(output)
+    click.echo(output)
     run_count = 0
     while matches and run_count < 10:
         run_count += 1
@@ -26,14 +31,13 @@ def main(dbt_base_path=""):
             with open(path.join(dbt_base_path, filename), 'w') as f:
                 content = dependency + '\n' + content
                 f.write(content)
-                print(f"\nUpdated the file: {filename}\nWith: {dependency}")
+                click.echo(f"\nUpdated the file: {filename}\nWith: {dependency}")
         if not matches:
             matches = re.findall(r'Syntax error: Expected "\(" or keyword SELECT or keyword WITH but got ";"', output)
-            if matches: print("\nWe have to run again to process some intermediate table builds.\n")
+            if matches: click.echo("\nWe have to run again to process some intermediate table builds.\n")
         if run_count >= 10:
-            print("\nThis program will break now because dbt run has happened 10 times and we're still getting errors.\n")
-    print(output)
-
+            click.echo("\nThis program will break now because dbt run has happened 10 times and we're still getting errors.\n")
+    click.echo(output)
 
 if __name__ == '__main__':
     main()
