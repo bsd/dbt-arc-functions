003c591c816a352d0a7dcc07566bcedff74d4d5e
Frydafly <fryda.guedes@gmail.com>
2023-02-21 18:02:58 -0600

    docstring added

--- utils/add_github_actions_workflows.py
+++ utils/add_github_actions_workflows.py
@@ -56,6 +56,7 @@ MERGE_STRING = """push:
     branches: [ main ]
 """
 PR_STRING = """pull_request:"""
+
 TRIGGER_DICT = {
     'prod': {'trigger': 'merge', 'trigger_string': MERGE_STRING},
     'dev': {'trigger': 'pr', 'trigger_string': PR_STRING}
@@ -63,14 +64,14 @@ TRIGGER_DICT = {
 
 
 def get_dbt_base_path():
-    """ This function gets the dbt_base_path in the user's computer
-    """
+    """This function prompts the user to enter the base directory
+        of their dbt project as an absolute path and returns it."""
     return input(
         "Please enter the base directory of your dbt project as an absolute path:\n")
 
 
 def initialize_yaml():
-    """ this function initializes yaml """
+    """This function initializes and returns the ruamel.yaml object."""
     yaml = ruamel.yaml.YAML()
     yaml.indent(mapping=4, sequence=4, offset=2)
     yaml.preserve_quotes = True
@@ -78,7 +79,7 @@ def initialize_yaml():
 
 
 def load_workflow_yml(workflow_yml_path, yaml):
-    """ this function takes the path to the github actions workflow and returns a formatted yaml """
+    """ This function takes the path to the github actions workflow and returns a formatted yaml """
     with open(workflow_yml_path, 'r') as f:
         content = f.read()
         workflow_yml = yaml.load(content)
@@ -86,13 +87,20 @@ def load_workflow_yml(workflow_yml_path, yaml):
 
 
 def convert_cloud_job_url_to_api_run_url(dbt_cloud_job_url):
-    """_summary_
+    """
+    Converts a dbt Cloud job URL to a URL that can be used to trigger a dbt Cloud API run.
 
     Args:
-        dbt_cloud_job_url (_type_): _description_
+        dbt_cloud_job_url (str): A string representing the URL of a dbt Cloud job. 
+        The URL should have the following format:
+        https://cloud.getdbt.com/<account_name>/deploy/<deployment_id>/projects/
+            <project_id>/jobs/<job_id>
 
     Returns:
-        _type_: _description_
+        str: A string representing the URL of a dbt Cloud API run that 
+        can be triggered using the URL returned by this method.
+        The URL should have the following format:
+        https://cloud.getdbt.com/api/v2/accounts/<account_id>/runs/<run_id>/
     """
     dbt_cloud_job_url = dbt_cloud_job_url.replace("next/", "")
     dbt_cloud_job_url = dbt_cloud_job_url.replace(
@@ -102,15 +110,32 @@ def convert_cloud_job_url_to_api_run_url(dbt_cloud_job_url):
 
 def create_dbt_run_yml_from_template(
         dbt_run_yml_template, dbt_cloud_api_run_url, trigger_yml):
-    """_summary_
+    """
+    Creates a new dbt run YAML configuration file by copying a template YAML file 
+    and replacing placeholders with actual values.
 
     Args:
-        dbt_run_yml_template (_type_): _description_
-        dbt_cloud_api_run_url (_type_): _description_
-        trigger_yml (_type_): _description_
+        dbt_run_yml_template: A dictionary that represents the dbt run YAML configuration template.
+        dbt_cloud_api_run_url: A string that represents the URL
+            of the dbt Cloud job run API endpoint.
+        trigger_yml: A dictionary that represents the YAML configuration 
+            for the GitHub Actions trigger.
 
     Returns:
-        _type_: _description_
+        A dictionary that represents the new dbt run YAML configuration file 
+        with placeholders replaced with actual values.
+
+    Raises:
+        TypeError: If any of the input arguments has an invalid type.
+        KeyError: If any of the required keys is missing from the input dictionary.
+
+    Examples:
+        dbt_run_yml_template = {'jobs': {'dbt_run': 
+                                {'steps': [{'run': 'dbt run --models source:table1+table2'}]}}}
+        dbt_cloud_api_run_url = 
+            'https://cloud.getdbt.com/api/v2/accounts/1234/projects/acme/jobs/5678/runs/91011/'
+        trigger_yml = {'push': {'branches': ['main']}}
+        dbt_run_yml = create_dbt_run_yml_from_template(dbt_run_yml_template, dbt_cloud_api_run_url, trigger_yml)
     """
     dbt_run_yml = deepcopy(dbt_run_yml_template)
     dbt_run_yml['jobs']['dbt_run']['steps'][0]['run'] = dbt_run_yml['jobs']['dbt_run']['steps'][0]['run'].replace('{dbt_cloud_api_run_url}',
@@ -121,14 +146,15 @@ def create_dbt_run_yml_from_template(
 
 
 def create_workflow_path_and_folders(dbt_base_path, workflow_filename):
-    """_summary_
+    """
+    Creates the necessary folder structure for a GitHub Actions workflow file and returns its path.
 
     Args:
-        dbt_base_path (_type_): _description_
-        workflow_filename (_type_): _description_
+        dbt_base_path (str): The base path of the dbt project.
+        workflow_filename (str): The filename of the workflow file.
 
     Returns:
-        _type_: _description_
+        str: The full path to the workflow file, including its filename.
     """
     github_folder_path = os.path.join(dbt_base_path, ".github")
     if not os.path.exists(github_folder_path):
@@ -140,33 +166,40 @@ def create_workflow_path_and_folders(dbt_base_path, workflow_filename):
 
 
 def write_workflow(workflow_path, workflow_yml, yaml):
-    """_summary_
+    """
+    Write the workflow_yml to the specified workflow_path using the provided yaml library.
 
     Args:
-        workflow_path (_type_): _description_
-        workflow_yml (_type_): _description_
-        yaml (_type_): _description_
+        workflow_path (str): The file path to write the workflow_yml to.
+        workflow_yml (dict): A dictionary containing the contents of the GitHub Actions workflow.
+        yaml (obj): An instance of the yaml library to use for dumping the workflow_yml.
+
+    Returns:
+        None.
     """
     with open(workflow_path, "w") as f:
         yaml.dump(workflow_yml, f)
 
 
 def create_dbt_run_workflow(environment, dbt_base_path, yaml):
-    """_summary_
+    """Create a GitHub workflow YAML file for running a DBT job on a trigger.
 
     Args:
-        environment (_type_): _description_
-        dbt_base_path (_type_): _description_
-        yaml (_type_): _description_
+        environment (str): The environment to create the workflow for. Must be one of the keys in the `trigger_dict` dictionary.
+        dbt_base_path (str): The base path of the DBT project.
+        yaml (yaml): A YAML instance used to parse and dump YAML data.
+
+    Returns:
+        None
     """
-    environment_dict = trigger_dict[environment]
+    environment_dict = TRIGGER_DICT[environment]
     trigger, trigger_string = environment_dict['trigger'], environment_dict['trigger_string']
-    input(prompt_create_environment_and_job.format(
+    input(PROMPT_CREATE_ENVIRONMENT_AND_JOB.format(
         environment=environment, trigger=trigger))
     dbt_run_filename = 'dbt_run_on_trigger.yml'
     dbt_run_yml_path = os.path.join('yml_files', dbt_run_filename)
     dbt_run_yml_template = load_workflow_yml(dbt_run_yml_path, yaml)
-    dbt_cloud_job_url = input(prompt_dbt_cloud_job_url)
+    dbt_cloud_job_url = input(PROMPT_DBT_CLOUD_JOB_URL)
     dbt_cloud_api_run_url = convert_cloud_job_url_to_api_run_url(
         dbt_cloud_job_url)
     trigger_yml = yaml.load(trigger_string)
@@ -180,11 +213,15 @@ def create_dbt_run_workflow(environment, dbt_base_path, yaml):
 
 
 def main(dbt_base_path='', yaml=None):
-    """_summary_
+    """
+    The main() function is the entry point for the script. It prompts the user to start the script, 
+    initializes the yaml library and calls create_dbt_run_workflow() function 
+    for both production and development environments to create their respective workflows. 
+    Finally, it prints a termination message.
 
     Args:
-        dbt_base_path (str, optional): _description_. Defaults to ''.
-        yaml (_type_, optional): _description_. Defaults to None.
+        dbt_base_path (str, optional): The base path for the dbt project. Defaults to ''.
+        yaml (Any, optional): An instance of the yaml library. Defaults to None.
     """
     if not dbt_base_path:
         dbt_base_path = get_dbt_base_path()
