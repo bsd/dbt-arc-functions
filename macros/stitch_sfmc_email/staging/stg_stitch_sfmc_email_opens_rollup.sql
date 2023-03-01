5df7cbeae6c12df5ace87b5b8b5de1a3f2b05544
Derrick Mondowney <109629735+dmbluestate@users.noreply.github.com>
2023-03-01 18:18:09 -0500

    removed extra sources from stitch_sfmc_email.yml file.

--- macros/stitch_sfmc_email/staging/stg_stitch_sfmc_email_opens_rollup.sql
+++ macros/stitch_sfmc_email/staging/stg_stitch_sfmc_email_opens_rollup.sql
@@ -1,6 +1,6 @@
 {% macro create_stg_stitch_sfmc_email_opens_rollup(
     reference_name='stg_src_stitch_email_open') %}
-SELECT SAFE_CAST(message_id AS STRING) AS message_id,
+SELECT SAFE_CAST(job_id AS STRING) AS message_id,
   SAFE_CAST(count(*) as INT) AS opens
 FROM {{ ref(reference_name) }} 
 GROUP BY 1
