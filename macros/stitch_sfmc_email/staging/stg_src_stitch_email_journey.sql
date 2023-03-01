96e41c35416b364055f3bb2648e834ec762fa87a
Frydafly <fryda.guedes@gmail.com>
2023-03-01 12:52:47 -0600

    parenthesis

--- macros/stitch_sfmc_email/staging/stg_src_stitch_email_journey.sql
+++ macros/stitch_sfmc_email/staging/stg_src_stitch_email_journey.sql
@@ -1,8 +1,8 @@
 {% macro create_stg_src_stitch_email_journey() %}
-{% set relations= dbt_arc_functions.relations_that_match_regex('^journey$'),
+{% set relations= dbt_arc_functions.relations_that_match_regex('^journey$',
     is_source=True,
   source_name='stitch_sfmc_email',
-  schema_to_search='src_stitch_sfmc_authorized' %}
+  schema_to_search='src_stitch_sfmc_authorized') %}
 
 
 Select DISTINCT
