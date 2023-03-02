3dc4a5b71e0a4b3d18826d6638c1b427e0d5bd59
Derrick Mondowney <109629735+dmbluestate@users.noreply.github.com>
2023-03-02 15:19:05 -0500

    Adding nulls for actions and removing list_subscribers

--- macros/stitch_sfmc_email/staging/stg_stitch_sfmc_email_actions_rollup.sql
+++ macros/stitch_sfmc_email/staging/stg_stitch_sfmc_email_actions_rollup.sql
@@ -1,7 +1,7 @@
 {% macro create_stg_stitch_sfmc_email_actions_rollup(
     reference_name='stg_src_stitch_email_action') %}
-Select SAFE_CAST(job_id AS string) as message_id,
-SAFE_CAST(count(*) as INT) as actions
+Select  distinct SAFE_CAST(job_id AS string) as message_id,
+null as actions
 FROM {{ ref(reference_name) }} 
 GROUP BY 1
 {% endmacro %}
