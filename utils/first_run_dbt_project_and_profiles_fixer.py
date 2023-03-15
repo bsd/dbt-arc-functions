4633bad57e1a30c9f07e1b01e5d7169914e63113
Frydafly <fryda.guedes@gmail.com>
2023-03-15 10:49:22 -0500

    trying a different version

--- utils/first_run_dbt_project_and_profiles_fixer.py
+++ utils/first_run_dbt_project_and_profiles_fixer.py
@@ -130,7 +130,7 @@ def update_dbt_project(
             "{% if target.name == 'default' %}{{ dbt_artifacts.upload_results(results) }}{% endif %}"]
     # adds dbt-date to the vars and sets default timezone conversion as UTC
     # TODO: ask user for timezone conversion for their client and add it to the vars
-    dbt_project_yml['vars'] = '"dbt_date:time_zone": "UTC"'
+    dbt_project_yml['vars'] = {"'dbt_date:time_zone'": "'UTC'"}
     copy_choice = inplace_or_copy("dbt_project")
     file, extension = path.splitext(dbt_project)
     with open(file + copy_choice + extension, 'w', encoding='utf-8') as f:
