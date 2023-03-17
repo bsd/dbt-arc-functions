915952d615f0707459516958793a3c4b150c71e6
Frydafly <fryda.guedes@gmail.com>
2023-03-17 16:50:25 -0500

    so far off of assumption

--- macros/ga4_google_analytics_web/stg_google_analytics_event_unnest.sql
+++ macros/ga4_google_analytics_web/stg_google_analytics_event_unnest.sql
@@ -1,9 +1,12 @@
-{% macro create_stg_google_analytics_event_unnest() %}
+{% macro create_stg_google_analytics_event_unnest(
+    reference_name='stg_google_analytics_event'
+) %}
 
 SELECT 
 SAFE_CAST(event_date as DATE FORMAT 'YYYYMMDD') as event_date,
+SAFE_CAST(session_value.value.int_value as INT) as session_id,
 SAFE_CAST(source_value.value.string_value as STRING) as session_source,
-SAFE_CAST(device.web_info.hostname as STRING) as hostname,
+SAFE_CAST(device.web_info.hostname as STRING) as host_name,
 SAFE_CAST(path_value.value.string_value as STRING) as page_path,
 SAFE_CAST(device.category as STRING) as device_category,
 SAFE_CAST(NULL as STRING) as user_gender,
@@ -19,17 +22,19 @@ SAFE_CAST(NULL as STRING) as search_term,
 SAFE_CAST(COALESCE(user_id, user_pseudo_id) as STRING) as user_id,
 SAFE_CAST(engagement_value.value.int_value as INT) as engagement_time_msec
 
-FROM {{ ref('stg_google_analytics_event') }}
+FROM {{ ref(reference_name) }}
 
  cross join unnest(event_params) path_value
  cross join unnest(event_params) source_value
  cross join unnest(event_params) campaign_value
  cross join unnest(event_params) engagement_value
+ cross join unnest(event_params) session_value
 
  where path_value.key = 'page_location'
  and source_value.key = 'source'
  and campaign_value.key = 'campaign'
  and engagement_value.key = 'engagement_time_msec'
+ and session_value.key = 'ga_session_id'
 
 {% endmacro %}
 
