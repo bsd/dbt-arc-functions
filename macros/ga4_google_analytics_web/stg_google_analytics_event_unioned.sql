b7baa717cabe3cc6d3bf565b3e60661cec1c9d6f
Frydafly <fryda.guedes@gmail.com>
2023-03-22 16:55:35 -0500

    union relations

--- macros/ga4_google_analytics_web/stg_google_analytics_event_unioned.sql
+++ macros/ga4_google_analytics_web/stg_google_analytics_event_unioned.sql
@@ -2,9 +2,10 @@
 
 {{ dbt_utils.union_relations(
 
-{% set relations = dbt_utils.get_relations_by_pattern (
-
-    
+{% set relations = dbt_utils.get_relations_by_pattern ( 
+    schema_to_search = target.name,
+    table_pattern = 'stg_google_analytics_event_%',
+    database = target.database
 )%}
 
 ) }}
