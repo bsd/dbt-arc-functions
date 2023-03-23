7302aab344af34f42561eb2e8c9cf994c72434f3
Derrick Mondowney <109629735+dmbluestate@users.noreply.github.com>
2023-03-23 17:31:17 -0400

    refactored stitch_sfmc_transactions to create a stitch_sfmc specific transactions directory

---
+++ macros/stitch_sfmc_transactions/staging/stg_stitch_sfmc_uusa_transactions_unioned.sql
@@ -0,0 +1,4 @@
+{% macro create_stg_stitch_transactions_unioned() %}
+{% set relations = dbt_arc_functions.relations_that_match_regex('^stg_stitch.*_transactions$') %}
+{{ dbt_utils.union_relations(relations) }}
+{% endmacro %}
