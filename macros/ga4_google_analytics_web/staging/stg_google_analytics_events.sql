99a274236a369bc01df9d526407cdeaee6ff7821
Frydafly <fryda.guedes@gmail.com>
2023-03-23 12:46:21 -0500

    no select distinct from array source

--- macros/ga4_google_analytics_web/staging/stg_google_analytics_events.sql
+++ macros/ga4_google_analytics_web/staging/stg_google_analytics_events.sql
@@ -1,6 +1,5 @@
 {% macro create_stg_google_analytics_events() %}
 
-SELECT DISTINCT *
- FROM {{ source('ga4_google_analytics_web', 'events')}}
+SELECT * FROM {{ source('ga4_google_analytics_web', 'events')}}
 
 {% endmacro %}
