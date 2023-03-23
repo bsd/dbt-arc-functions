223b451cbd1f5a4057949e01fb2459e49a061b7d
Frydafly <fryda.guedes@gmail.com>
2023-03-23 13:43:52 -0500

    add terms

--- macros/ga4_google_analytics_web/staging/stg_google_analytics_event_unioned.sql
+++ macros/ga4_google_analytics_web/staging/stg_google_analytics_event_unioned.sql
@@ -1,6 +1,11 @@
 {% macro create_stg_google_analytics_event_unioned() %}
 
-{% set relations = dbt_arc_functions.relations_that_match_regex('^stg_google_analytics_event_.+$',) %}
+
+{% set relations = dbt_arc_functions.relations_that_match_regex('^stg_google_analytics_event_.+$',
+  is_source=False,
+  schema_to_search=target.schema) 
+
+%}
 
 SELECT DISTINCT * FROM ({{ dbt_utils.union_relations(relations) }})
 
