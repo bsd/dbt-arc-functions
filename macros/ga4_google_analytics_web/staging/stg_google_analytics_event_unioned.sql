43c18c83d9b10548c9974867495876f2f6832b80
Frydafly <fryda.guedes@gmail.com>
2023-03-23 11:32:15 -0500

    fix set relations unioned

--- macros/ga4_google_analytics_web/staging/stg_google_analytics_event_unioned.sql
+++ macros/ga4_google_analytics_web/staging/stg_google_analytics_event_unioned.sql
@@ -1,13 +1,11 @@
 {% macro create_stg_google_analytics_event_unioned() %}
 
-{{ dbt_utils.union_relations(
-
 {% set relations = dbt_utils.get_relations_by_pattern ( 
     schema_to_search = target.name,
     table_pattern = 'stg_google_analytics_event_%',
-    database = target.database
-)%}
+    database = target.database)
+%}
 
-) }}
+SELECT DISTINCT * FROM ({{ dbt_utils.union_relations(relations) }})
 
 {% endmacro %}
