d1f53ce182951073b8175e4be3787d6b025400ef
Frydafly <fryda.guedes@gmail.com>
2023-03-23 13:09:30 -0500

    without naming variables

--- macros/ga4_google_analytics_web/staging/stg_google_analytics_event_unioned.sql
+++ macros/ga4_google_analytics_web/staging/stg_google_analytics_event_unioned.sql
@@ -1,11 +1,12 @@
 {% macro create_stg_google_analytics_event_unioned() %}
 
-{% set relations = dbt_utils.get_relations_by_pattern ( 
-    schema_pattern = target.name,
-    table_pattern = 'stg_google_analytics_event%',
-    database = target.database)
-%}
+{% set relations = dbt_utils.get_relations_by_pattern(target.name, 'stg_google_analytics_event_%') %}
 
 SELECT DISTINCT * FROM ({{ dbt_utils.union_relations(relations) }})
 
 {% endmacro %}
+
+
+-- Example using the union_relations macro
+--{% set event_relations = dbt_utils.get_relations_by_pattern('venue%', 'clicks') %}
+--{{ dbt_utils.union_relations(relations = event_relations) }}
