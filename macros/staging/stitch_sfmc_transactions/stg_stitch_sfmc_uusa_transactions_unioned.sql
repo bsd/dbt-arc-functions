1c53dc2f9cd3220e3979cd8dc984e4e47c7f51c7
Derrick Mondowney <109629735+dmbluestate@users.noreply.github.com>
2023-03-23 17:38:51 -0400

    Delete unneccessary directory

--- macros/staging/stitch_sfmc_transactions/stg_stitch_sfmc_uusa_transactions_unioned.sql
+++
@@ -1,4 +0,0 @@
-{% macro create_stg_stitch_transactions_unioned() %}
-{% set relations = dbt_arc_functions.relations_that_match_regex('^stg_stitch.*_transactions$') %}
-{{ dbt_utils.union_relations(relations) }}
-{% endmacro %}
