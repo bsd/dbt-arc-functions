a5791f45018f2aa4a5c680169b48ca3ac29b509f
Frydafly <fryda.guedes@gmail.com>
2023-03-23 13:35:58 -0500

    correct regexp

--- macros/ga4_google_analytics_web/staging/stg_google_analytics_event_unioned.sql
+++ macros/ga4_google_analytics_web/staging/stg_google_analytics_event_unioned.sql
@@ -1,6 +1,6 @@
 {% macro create_stg_google_analytics_event_unioned() %}
 
-{% set relations = dbt_arc_functions.relations_that_match_regex('^stg_google_analytics_event_%',) %}
+{% set relations = dbt_arc_functions.relations_that_match_regex('^stg_google_analytics_event_.+$',) %}
 
 SELECT DISTINCT * FROM ({{ dbt_utils.union_relations(relations) }})
 
