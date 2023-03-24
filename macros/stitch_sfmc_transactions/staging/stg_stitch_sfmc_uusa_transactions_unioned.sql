f0d311a33a2f6210527abcbe274ad77a4547b4c4
Derrick Mondowney <109629735+dmbluestate@users.noreply.github.com>
2023-03-23 20:22:55 -0400

    add dependency statement to macro

--- macros/stitch_sfmc_transactions/staging/stg_stitch_sfmc_uusa_transactions_unioned.sql
+++ macros/stitch_sfmc_transactions/staging/stg_stitch_sfmc_uusa_transactions_unioned.sql
@@ -1,5 +1,5 @@
 -- depends_on: {{ ref('stg_stitch_sfmc_fundraiseup_filtered_recent_transactions') }}
 {% macro create_stg_stitch_transactions_unioned() %}
-{% set relations = dbt_arc_functions.relations_that_match_regex('^stg_stitch.*_transactions$') %}
+{% set relations = dbt_arc_functions.relations_that_match_regex('^stg_stitch_.*_transactions$') %}
 {{ dbt_utils.union_relations(relations) }}
 {% endmacro %}
