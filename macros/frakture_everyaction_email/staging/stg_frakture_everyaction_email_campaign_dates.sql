5a07e70ba9d26ca7a8c5d4a26f3fdf5f27804015
Frydafly <fryda.guedes@gmail.com>
2023-04-05 14:52:03 -0500

    updated everyaction macros

---
+++ macros/frakture_everyaction_email/staging/stg_frakture_everyaction_email_campaign_dates.sql
@@ -0,0 +1,8 @@
+{% macro create_stg_frakture_everyaction_email_campaign_dates(
+    reference_name='stg_frakture_everyaction_email_summary_unioned') %}
+SELECT 
+  SAFE_CAST(publish_date as TIMESTAMP) as campaign_timestamp,
+  SAFE_CAST(campaign_label AS STRING) AS crm_campaign,
+  SAFE_CAST(campaign AS STRING) AS source_code_campaign
+FROM {{ ref(reference_name) }}
+{% endmacro %}
--- macros/frakture_everyaction_email/staging/stg_frakture_everyaction_email_transactions_sourced_rollup.sql
+++ macros/frakture_everyaction_email/staging/stg_frakture_everyaction_email_transactions_sourced_rollup.sql
@@ -1,14 +1,36 @@
 {% macro create_stg_frakture_everyaction_email_transactions_sourced_rollup(
-    reference_name='stg_frakture_everyaction_email_summary_unioned') %}
-SELECT SAFE_CAST(message_id AS STRING) AS message_id,
-    SAFE_CAST(email_summary.revenue AS numeric) AS total_revenue,
-    SAFE_CAST(email_summary.transactions AS int) AS total_gifts,
-    SAFE_CAST(NULL AS int) AS total_donors,  -- doesn't seem available in Frakture email_summary tables
-    SAFE_CAST(email_summary.revenue - email_summary.attributed_recurring_revenue AS numeric) AS one_time_revenue,
-    SAFE_CAST(email_summary.transactions - email_summary.attributed_recurring_transactions  AS int) AS one_time_gifts,
-    SAFE_CAST(NULL  AS numeric) AS new_monthly_revenue, -- doesn't seem available in Frakture email_summary tables
-    SAFE_CAST(NULL  AS int) AS new_monthly_gifts, -- doesn't seem available in Frakture email_summary tables
-    SAFE_CAST(email_summary.attributed_recurring_revenue  AS numeric) AS total_monthly_revenue,
-    SAFE_CAST(email_summary.attributed_recurring_transactions  AS int) AS total_monthly_gifts
- FROM  {{ ref(reference_name) }} email_summary
+    email_summary='stg_frakture_everyaction_email_summary_unioned',
+    transactions='stg_frakture_everyaction_transactions_summary_unioned'
+    ) %}
+
+WITH GROUPED as (
+SELECT 
+    email_summary.message_id AS message_id,
+    SUM(SAFE_CAST(transactions.amount AS numeric)) AS total_revenue,
+    COUNT(DISTINCT transactions.remote_transaction_id) AS total_gifts,
+    COUNT(DISTINCT transactions.remote_person_id) AS total_donors, 
+    SUM(CASE WHEN transactions.is_recurring = '0' then transactions.amount end) AS one_time_revenue,
+    COUNT(CASE WHEN transactions.is_recurring = '0' then transactions.remote_transaction_id end) AS one_time_gifts,
+    SUM(CASE WHEN transactions.recurring_number = 1 then transactions.amount end) AS new_monthly_revenue,
+    COUNT(CASE WHEN transactions.recurring_number = 1 then transactions.remote_transaction_id end) AS new_monthly_gifts,
+    SUM(CASE WHEN transactions.is_recurring = '1' then transactions.amount end) AS total_monthly_revenue,
+    COUNT(CASE WHEN transactions.is_recurring = '1' then transactions.remote_transaction_id end) AS total_monthly_gifts
+ FROM  {{ ref(email_summary) }} email_summary
+ FULL OUTER JOIN {{ ref(transactions) }} transactions 
+ ON CAST(email_summary.source_code as STRING) = CAST(transactions.source_code as STRING)
+ GROUP BY 1 )
+
+SELECT 
+    SAFE_CAST(message_id as STRING) AS message_id,
+    SAFE_CAST(total_revenue as NUMERIC) AS total_revenue,
+    SAFE_CAST(total_gifts as INT) AS total_gifts,
+    SAFE_CAST(total_donors as INT) AS total_donors,
+    SAFE_CAST(one_time_gifts as INT) AS one_time_gifts,
+    SAFE_CAST(one_time_revenue as NUMERIC) AS one_time_revenue,
+    SAFE_CAST(new_monthly_revenue  AS numeric) AS new_monthly_revenue,
+    SAFE_CAST(new_monthly_gifts  AS int) AS new_monthly_gifts,
+    SAFE_CAST(total_monthly_revenue AS numeric) AS total_monthly_revenue,
+    SAFE_CAST(total_monthly_gifts  AS int) AS total_monthly_gifts
+FROM GROUPED
+
 {% endmacro %}
