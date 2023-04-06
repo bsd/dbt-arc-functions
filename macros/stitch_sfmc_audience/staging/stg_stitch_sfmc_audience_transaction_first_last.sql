dfb91f30e61b4c0b2143e22f8d9a2cb3985c2472
Frydafly <fryda.guedes@gmail.com>
2023-04-06 13:27:07 -0500

    adde back the date stuff

--- macros/stitch_sfmc_audience/staging/stg_stitch_sfmc_audience_transaction_first_last.sql
+++ macros/stitch_sfmc_audience/staging/stg_stitch_sfmc_audience_transaction_first_last.sql
@@ -4,24 +4,10 @@
 
 SELECT 
 transaction_id,
-SUM(amount) OVER (PARTITION BY person_id ORDER BY transaction_date) AS cumulative_amount,
-COUNT(*) OVER (PARTITION BY person_id ORDER BY transaction_date) AS cumulative_gifts,
-SUM(CASE 
-  WHEN recurring = 0 THEN amount 
-  ELSE 0 
-END) OVER (PARTITION BY person_id ORDER BY transaction_date) AS cumulative_one_time_amount,
-COUNT(CASE 
-  WHEN recurring = 0 THEN 1
-  ELSE 0 
-END) OVER (PARTITION BY person_id ORDER BY transaction_date) AS cumulative_one_time_gifts,
-SUM(CASE 
-  WHEN recurring = 1 THEN amount 
-  ELSE 0 
-END) OVER (PARTITION BY person_id ORDER BY transaction_date) AS cumulative_recur_amount,
-COUNT(CASE 
-  WHEN recurring = 1 THEN 1
-  ELSE 0 
-END) OVER (PARTITION BY person_id ORDER BY transaction_date) AS cumulative_recur_gifts
+ MAX(transaction_date) OVER (PARTITION BY person_id) AS latest_transaction_date,
+  LAG(MAX(transaction_date) OVER (PARTITION BY person_id)) 
+  OVER (PARTITION BY person_id ORDER BY transaction_date) 
+  AS previous_latest_transaction_date
 
 FROM {{ ref(reference_name) }}
 
