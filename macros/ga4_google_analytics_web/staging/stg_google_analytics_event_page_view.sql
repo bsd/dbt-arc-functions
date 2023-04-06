0bc4af95f6b827c8c01e48661b78a943046ad641
Frydafly <fryda.guedes@gmail.com>
2023-04-06 11:14:19 -0500

    removed comment blocks

--- macros/ga4_google_analytics_web/staging/stg_google_analytics_event_page_view.sql
+++ macros/ga4_google_analytics_web/staging/stg_google_analytics_event_page_view.sql
@@ -7,7 +7,6 @@
 SELECT 
 SAFE_CAST(event_date as DATE FORMAT 'YYYYMMDD') as event_date,
 SAFE_CAST(session_value.value.int_value as INT) as session_id,
---SAFE_CAST(NULL as STRING) as session_source,
 SAFE_CAST(source_value.value.string_value as STRING) as session_source,
 SAFE_CAST(medium_value.value.string_value as STRING) as session_medium,
 SAFE_CAST(device.web_info.hostname as STRING) as host_name,
@@ -16,29 +15,22 @@ SAFE_CAST(device.category as STRING) as device_category,
 SAFE_CAST(NULL as STRING) as user_gender,
 SAFE_CAST(NULL as INT) as user_age,
 SAFE_CAST(NULL as STRING) as session_default_channel,
---SAFE_CAST(NULL as STRING) as campaign,
 SAFE_CAST(campaign_value.value.string_value as STRING) as campaign,
 SAFE_CAST(device.language as STRING) as device_language,
---SAFE_CAST(lang_value.value.string_value as string) as selected_language,
 SAFE_CAST(NULL as string) as selected_language,
 SAFE_CAST(event_name as STRING) as event_name,
 SAFE_CAST(NULL as STRING) as accordion_item,
 SAFE_CAST(NULL as STRING) as accordion_block,
---SAFE_CAST(accordion_value.value.string_value as STRING) as accordion_item,
---SAFE_CAST(file_value.value.string_value as STRING) as file_name,
 SAFE_CAST(NULL as STRING) as file_name,
 SAFE_CAST(NULL as STRING) as link_domain,
 SAFE_CAST(NULL as STRING) as top_nav_name,
 SAFE_CAST(NULL as STRING) as sub_menu_name,
 SAFE_CAST(engaged_value.value.int_value as INT) as engaged_session,
 SAFE_CAST(NULL as INT) as percent_scrolled,
---SAFE_CAST(search_value.value.string_value as STRING) as search_term,
 SAFE_CAST(NULL as STRING) as search_term,
---SAFE_CAST(refer_value.value.string_value as STRING) as page_referrer, 
 SAFE_CAST(NULL as STRING) as page_referrer, 
 SAFE_CAST(COALESCE(user_id, user_pseudo_id) as STRING) as user_id,
 SAFE_CAST(engagement_value.value.int_value as INT) as engagement_time_msec
---SAFE_CAST(NULL as INT) as engagement_time_msec
 
 FROM {{ ref(reference_name) }}
 
@@ -49,11 +41,6 @@ FROM {{ ref(reference_name) }}
  cross join unnest(event_params) engagement_value
  cross join unnest(event_params) session_value
  cross join unnest(event_params) engaged_value
- --cross join unnest(event_params) file_value
- --cross join unnest(event_params) lang_value
---cross join unnest(event_params) search_value
---cross join unnest(event_params) refer_value
- --cross join unnest(event_params) accordion_value
 
  where 
  event_name = 'page_view'
@@ -64,11 +51,6 @@ FROM {{ ref(reference_name) }}
  and engagement_value.key = 'engagement_time_msec'
  and session_value.key = 'ga_session_id'
  and engaged_value.key = 'engaged_session_event'
- --and file_value.key = 'file_name'
---and lang_value.key = 'language_selected'
---and search_value.key = 'search_term'
---and refer_value.key = 'page_referrer'
--- and accordion_value.key = 'accordion_item'
 
 {% endmacro %}
 
