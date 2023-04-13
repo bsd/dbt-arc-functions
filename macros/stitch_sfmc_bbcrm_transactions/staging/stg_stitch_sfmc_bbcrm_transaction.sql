d9290c60082e7d72d423d95527c8ad1a5122b99c
Derrick Mondowney <109629735+dmbluestate@users.noreply.github.com>
2023-04-12 20:24:34 -0400

    adding fields to bbcrm transaction model, adding recurring fields to both bbcrm and fru models to improve the unioned model

--- macros/stitch_sfmc_bbcrm_transactions/staging/stg_stitch_sfmc_bbcrm_transaction.sql
+++ macros/stitch_sfmc_bbcrm_transactions/staging/stg_stitch_sfmc_bbcrm_transaction.sql
@@ -10,11 +10,18 @@ select
     safe_cast(
         regexp_extract(initial_market_source, r"sfmc(\d{6})") as int
     ) as message_id,
+    initial_market_source,
+    inbound_channel,
     transaction_date,
     amount,
     appeal,
     appeal_business_unit,
-    application
+    application,
+    case
+        when lower(application) = 'recurring gift'
+        then safe_cast(1 as boolean)
+        else safe_cast(0 as boolean)
+    end as recurring
 from {{ ref(reference_name) }}
 
 {% endmacro %}
