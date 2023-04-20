03dd695f5a88928a687e9bc3f11d34b09b724361
Timothy Ryan <tryan@bluestate.co>
2023-04-20 09:37:36 -0400

    ARC-1437 add column checks
    ARC-1437 add columns check for docs
    
    ARC-1437 try pipenv
    
    ARC-1437 add pipenv to dependencies
    
    ARC-1437 add pyenv
    
    ARC-1437 try other pyenv means
    
    ARC-1437 pip install ruamel
    
    ARC-1437 ruamel-yaml
    
    ARC-1437 install old ruamel
    
    Revert "ARC-1437 install old ruamel"
    
    This reverts commit bad91a0acfa82a1b917befc9e64c06a3f9846755.
    
    Revert "ARC-1437 ruamel-yaml"
    
    This reverts commit d005e16fe9f888b28fd3d0f524b64866f14209ab.
    
    Revert "ARC-1437 pip install ruamel"
    
    This reverts commit f1e12e6944bf8aa74742254ee4078f00542e29fa.
    
    add pipenv install
    
    ssh install
    
    ARC-1437 try older python version
    
    ARC-1437 try conda
    
    ARC-1437 downgrade python
    
    ARC-1437 try ruamel-yaml
    
    ARC-1437 don't upgrade pip
    
    ARC-1437 also install ruamel
    
    ARC-1437 install pip3
    
    try inline
    
    Revert "try inline"
    
    This reverts commit f9202a44c99d61699f089fe609f140eeae6f87c7.
    
    Revert "ARC-1437 install pip3"
    
    This reverts commit fe37e3add9b244332b7dd2d7c69554055ff8b3f0.
    
    Revert "ARC-1437 also install ruamel"
    
    This reverts commit be956545888288c3738decc7af67f73be2ccaf25.
    
    Revert "ARC-1437 don't upgrade pip"
    
    This reverts commit 8c9c20f56f7a93fbe8a43e565cfbc621d61bb1fe.
    
    Revert "ARC-1437 try ruamel-yaml"
    
    This reverts commit 2640b8bd91bd4be9f7545477420684586d32ac76.
    
    Revert "ARC-1437 downgrade python"
    
    This reverts commit c72961a0737bdafc5805907b74e24a52c949975c.
    
    ARC-1437 namespaces!
    
    ARC-1437

---
+++ utils/check_columns_in_every_doc.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+import sys
+import os
+import yaml
+
+
+def main():
+    docs_without_columns = []
+
+    for root, _, files in os.walk('../documentation'):
+        for file in files:
+            file_path = os.path.join(root, file)
+            if file.endswith('.yml'):
+                with open(file_path, 'r', encoding='utf-8') as f:
+                    doc_yaml = yaml.safe_load(f)
+                try:
+                    columns = doc_yaml
+                except KeyError:
+                    docs_without_columns.append(file_path)
+                if not columns or len(columns) < 2:
+                    docs_without_columns.append(file_path)
+
+
+    if docs_without_columns:
+        for doc_without_columns in docs_without_columns:
+            print(f"The doc below doesn't have any columns:\n {doc_without_columns}\n"
+                  "Please delete the doc, then run create_docs.ipynb against a working"
+                  " client to create docs\n")
+
+        print(f"\nDocs without columns: {len(docs_without_columns)}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
