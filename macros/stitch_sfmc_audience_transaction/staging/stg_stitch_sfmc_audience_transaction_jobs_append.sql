596ee8f5619118d6f1e7ba28fd811d9a11bb29e4
Frydafly <fryda.guedes@gmail.com>
2023-04-20 15:08:30 -0500

    fomat

--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_jobs_append.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_jobs_append.sql
@@ -37,24 +37,31 @@ select
 when
     donated_current_fiscal_year_july_to_june = 1
     and donated_last_fiscal_year_july_to_june = 1
-    and donated_two_fiscal_years_ago_july_to_june = 0 
+    and donated_two_fiscal_years_ago_july_to_june = 0
     and donated_three_fiscal_years_ago_july_to_june = 0
-    -- and did not donate two years ago and before
+-- and did not donate two years ago and before
 then 'retained'
-when donated_current_fiscal_year_july_to_june = 1 and donated_last_fiscal_year_july_to_june = 0
+when
+    donated_current_fiscal_year_july_to_june = 1
+    and donated_last_fiscal_year_july_to_june = 0
 then 'new_donor'
-when donated_current_fiscal_year_july_to_june = 1
+when
+    donated_current_fiscal_year_july_to_june = 1
     and donated_last_fiscal_year_july_to_june = 1
-    and (donated_two_fiscal_years_ago_july_to_june = 1 OR donated_three_fiscal_years_ago_july_to_june = 1)
-    -- and any other year before that
-then 'retained 3+' 
--- retained 3+ also multiyear
-end 
-as donor_loyalty
+    and (
+        donated_two_fiscal_years_ago_july_to_june = 1
+        or donated_three_fiscal_years_ago_july_to_june = 1
+    )
+-- and any other year before that
+then
+    'retained 3+'
+    -- retained 3+ also multiyear
+    end as donor_loyalty
 from {{ reference_name }}
 
 {% else %}
--- This SQL statement will be used if 'variable' is empty or does not exist, it's dummy for now
+-- This SQL statement will be used if 'variable' is empty or does not exist, it's
+-- dummy for now
 select
     transaction_date,
     person_id,
@@ -86,7 +93,9 @@ when
     donated_current_fiscal_year_july_to_june = 1
     and donated_last_fiscal_year_july_to_june = 1
 then 'existing'
-when donated_current_fiscal_year_july_to_june = 1 and donated_last_fiscal_year_july_to_june = 0
+when
+    donated_current_fiscal_year_july_to_june = 1
+    and donated_last_fiscal_year_july_to_june = 0
 then 'new_donor' end as donor_loyalty
 from {{ reference_name }}
 
