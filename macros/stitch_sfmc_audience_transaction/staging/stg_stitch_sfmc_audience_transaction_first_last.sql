0ca1a16955ada9d23677c3b10c990322cbaeafcd
Frydafly <fryda.guedes@gmail.com>
2023-04-21 11:52:02 -0500

    remove analytic complexity

--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_first_last.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_first_last.sql
@@ -2,14 +2,23 @@
     reference_name="stg_stitch_sfmc_transactions_unioned"
 ) %}
 
+
+with subquery as (
+select
+        transaction_date,
+        person_id,
+        max(transaction_date) over (partition by person_id) as max_transaction_date
+    from {{ ref(reference_name) }}
+)
+
 select
     transaction_date,
     person_id,
     min(transaction_date) over (partition by person_id) as first_transaction_date,
-    lag(max(transaction_date) over (partition by person_id)) over (
+    lag(max_transaction_date) over (
         partition by person_id order by transaction_date
     ) as previous_latest_transaction_date
 
-from {{ ref(reference_name) }}
+from subquery
 
 {% endmacro %}
