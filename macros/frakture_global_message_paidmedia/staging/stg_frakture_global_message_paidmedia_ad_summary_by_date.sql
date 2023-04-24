79a8e7eaf2448ca06884739ef222e237bd627fec
Timothy Ryan <tryan@bluestate.co>
2023-04-24 09:50:59 -0400

    ARC-1437 update sqlfmt ing

--- macros/frakture_global_message_paidmedia/staging/stg_frakture_global_message_paidmedia_ad_summary_by_date.sql
+++ macros/frakture_global_message_paidmedia/staging/stg_frakture_global_message_paidmedia_ad_summary_by_date.sql
@@ -1,5 +1,10 @@
 {% macro create_stg_frakture_global_message_paidmedia_ad_summary_by_date() %}
-select distinct *
-from {{ source("frakture_global_message_paidmedia", "global_message_summary_by_date") }}
-where message_id is not null and channel != 'email'
+    select distinct *
+    from
+        {{
+            source(
+                "frakture_global_message_paidmedia", "global_message_summary_by_date"
+            )
+        }}
+    where message_id is not null and channel != 'email'
 {% endmacro %}
