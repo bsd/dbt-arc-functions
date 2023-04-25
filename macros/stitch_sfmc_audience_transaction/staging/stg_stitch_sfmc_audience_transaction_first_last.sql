74a9dd2451bc2a1666d7da67ac14b96413191381
Frydafly <fryda.guedes@gmail.com>
2023-04-25 13:43:31 -0500

    typo

--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_first_last.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_first_last.sql
@@ -16,7 +16,7 @@ select
     person_id,
     min(transaction_date_day) over (partition by person_id) as first_transaction_date,
     lag(max_transaction_date) over (
-        partition by person_id order by transaction_date
+        partition by person_id order by transaction_date_day
     ) as previous_latest_transaction_date
 
 from subquery
