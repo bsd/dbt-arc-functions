351ebc1807060b41feb34e88b3099ae1e2aa0952
Frydafly <fryda.guedes@gmail.com>
2023-04-25 13:23:18 -0500

    added transaction_date_day

--- macros/stitch_sfmc_transactions/staging/stg_stitch_sfmc_transactions_unioned.sql
+++ macros/stitch_sfmc_transactions/staging/stg_stitch_sfmc_transactions_unioned.sql
@@ -2,5 +2,11 @@
 {% set relations = dbt_arc_functions.relations_that_match_regex(
     "^stg_stitch_.*_transaction$"
 ) %}
-{{ dbt_utils.union_relations(relations) }}
+with base as ({{ dbt_utils.union_relations(relations) }})
+
+select 
+base.date_trunc('day', base.transaction_date) as transaction_date_day,
+base.*
+from base
+
 {% endmacro %}
