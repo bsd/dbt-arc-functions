b960ce7930543088db2ce0b992513ec07e104282
Frydafly <fryda.guedes@gmail.com>
2023-05-02 17:59:17 -0500

    d

--- macros/stitch_sfmc_email/staging/stg_stitch_sfmc_email_actions_rollup.sql
+++ macros/stitch_sfmc_email/staging/stg_stitch_sfmc_email_actions_rollup.sql
@@ -1,7 +1,7 @@
 {% macro create_stg_stitch_sfmc_email_actions_rollup(
     reference_name="stg_stitch_sfmc_email_actions"
 ) %}
-select distinct safe_cast(message_id as string) as message_id, 
+select distinct message_id, 
 SUM(actions) as actions
 from {{ ref(reference_name) }}
 group by 1
