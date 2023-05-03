f838a976143eeed795a2f2f74db6dfaab469edf4
Frydafly <fryda.guedes@gmail.com>
2023-05-03 18:34:18 -0500

    some case when stuff

--- macros/stitch_sfmc_email/staging/stg_stitch_sfmc_email_jobs.sql
+++ macros/stitch_sfmc_email/staging/stg_stitch_sfmc_email_jobs.sql
@@ -11,12 +11,13 @@ select distinct
     safe_cast(emailid as string) as email_id,
     safe_cast(fromname as string) as from_name,
     safe_cast(fromemail as string) as from_email,
-    safe_cast(
+    case when coalesce(schedtime, pickuptime, NULL) = '' then safe_cast(NULL as timestamp) else safe_cast(
         {{ dbt_date.convert_timezone("cast(coalesce(schedtime, pickuptime, NULL) as timestamp)") }} as timestamp
-    ) as best_guess_timestamp,
-    safe_cast( {{ dbt_date.convert_timezone("cast(coalesce(schedtime, NULL) as timestamp)") }} as timestamp) as scheduled_timestamp,
-    safe_cast({{ dbt_date.convert_timezone("cast(coalesce(pickuptime, NULL) as timestamp)") }} as timestamp) as pickup_timestamp,
-    safe_cast({{ dbt_date.convert_timezone("cast(coalesce(deliveredtime, NULL) as timestamp)") }} as timestamp) as delivered_timestamp,
+    ) end as best_guess_timestamp,
+    case when schedtime = '' then safe_cast(NULL as timestamp) 
+    else safe_cast( {{ dbt_date.convert_timezone("cast(coalesce(schedtime, NULL) as timestamp)") }} as timestamp) end as scheduled_timestamp,
+    case when pickuptime = '' then SAFE_CAST(NULL as timestamp) else safe_cast({{ dbt_date.convert_timezone("cast(coalesce(pickuptime, NULL) as timestamp)") }} as timestamp) end as pickup_timestamp,
+    case when deliveredtime = '' then SAFE_CAST(NULL as timestamp) else safe_cast({{ dbt_date.convert_timezone("cast(coalesce(deliveredtime, NULL) as timestamp)") }} as timestamp) end as delivered_timestamp,
     safe_cast(emailname as string) as email_name,
     safe_cast(emailsubject as string) as email_subject,
     safe_cast(category as string) as category,
