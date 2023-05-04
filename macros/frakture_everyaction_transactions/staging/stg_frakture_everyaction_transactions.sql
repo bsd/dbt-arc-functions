3c2b63390209187228e44c48c61b0eb5e6e85867
Frydafly <fryda.guedes@gmail.com>
2023-05-04 11:04:00 -0500

    sources will work with everyaction

--- macros/frakture_everyaction_transactions/staging/stg_frakture_everyaction_transactions.sql
+++ macros/frakture_everyaction_transactions/staging/stg_frakture_everyaction_transactions.sql
@@ -6,22 +6,22 @@
         schema_to_search="src_frakture",
     ) %}
     select
-        regexp_extract(transaction_bot_id, '([A-Za-z_]+)_[a-z0-9]{3}') as source_crm,
+        'everyaction' as source_crm,
         remote_transaction_id as transaction_id_in_source_crm,
-        person_id_int as person_id,
+        remote_person_id as person_id,
         amount - refund_amount as amount,
         safe_cast(
             {{ dbt_date.convert_timezone("cast(ts as TIMESTAMP)") }} as timestamp
         ) as transaction_timestamp,
-        recurs != 'Non-recurring' as recurring_revenue,
-        recurs_int = 1 and recurs != 'Non-recurring' as new_recurring_revenue,
-        transaction_source_code,
-        message_id as best_guess_message_id,
-        campaign as campaign,
-        channel as channel,
-        audience as audience,
-        safe_cast(transaction_bot_id as string) as crm_entity,
-        safe_cast(affiliation as string) as source_code_entity,
-        source_code_channel as channel_from_source_code
-    from {{ source(source_name, source_table_name) }}
+        case when recurs = 'monthly' then 1 else 0 end as recurring_revenue,
+        safe_cast(null as int) as new_recurring_revenue,
+        coalesce(everyaction_market_source,source_code) as transaction_source_code,
+        safe_cast(null as string) as best_guess_message_id,
+        safe_cast(null as string) as campaign,
+        'email' as channel,
+        safe_cast(null as string) as audience,
+        'everyaction' as crm_entity,
+        safe_cast(null as string) as source_code_entity,
+        safe_cast(source_code_channel as string) as channel_from_source_code
+    from {{utils.union_relations(relations)}}
 {% endmacro %}
