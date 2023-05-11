80651b2e93baa5d19867640502905ecfd0bfc470
Frydafly <fryda.guedes@gmail.com>
2023-05-11 14:51:28 -0500

    removed unused value "previous latest transaction date"

--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_first_last.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_first_last.sql
@@ -18,10 +18,7 @@
         person_id,
         min(transaction_date_day) over (
             partition by person_id
-        ) as first_transaction_date,
-        lag(max_transaction_date) over (
-            partition by person_id order by transaction_date_day
-        ) as previous_latest_transaction_date
+        ) as first_transaction_date
 
     from subquery
 
