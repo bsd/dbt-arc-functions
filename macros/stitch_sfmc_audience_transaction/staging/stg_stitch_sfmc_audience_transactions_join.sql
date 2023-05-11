6dba2eee408ee2b0f4e7d5aa1c54fe897cadab28
Frydafly <fryda.guedes@gmail.com>
2023-05-11 14:52:51 -0500

    first last concept is totally unused

--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transactions_join.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transactions_join.sql
@@ -8,8 +8,6 @@
     select
         jobs.transaction_date_day,
         jobs.person_id,
-        first_last.first_transaction_date,
-        first_last.previous_latest_transaction_date,
         cumulative.cumulative_amount_12_months,
         cumulative.cumulative_amount_24_months,
         cumulative.cumulative_amount_30_days_recur,
@@ -22,10 +20,6 @@
         yoy.donated_four_fiscal_years_ago_july_to_june
 
     from {{ ref(jobs) }} jobs
-    left join
-        {{ ref(first_last) }} first_last
-        on jobs.transaction_date_day = first_last.transaction_date_day
-        and jobs.person_id = first_last.person_id
     left join
         {{ ref(cumulative) }} cumulative
         on jobs.transaction_date_day = cumulative.transaction_date_day
