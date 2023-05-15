37877255acb808cb7bf213454b4d4539fcbe66ed
Frydafly <fryda.guedes@gmail.com>
2023-05-15 13:49:52 -0500

    standard is to have S after transactionssss

--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transactions_summary_unioned.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transactions_summary_unioned.sql
@@ -1,6 +1,6 @@
 {% macro create_stg_stitch_sfmc_audience_transactions_summary_unioned() %}
     {% set relations = dbt_arc_functions.relations_that_match_regex(
-        "^stg_stitch_.*_transaction$"
+        "^stg_stitch_.*_transactions$"
     ) %}
     with base as ({{ dbt_utils.union_relations(relations) }})
 
