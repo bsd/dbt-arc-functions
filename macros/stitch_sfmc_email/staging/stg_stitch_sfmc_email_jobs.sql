74c61603f589ffad232d3ac4adae2c9f6522373a
Frydafly <fryda.guedes@gmail.com>
2023-05-26 14:10:44 -0500

    corrections to pickup names

--- macros/stitch_sfmc_email/staging/stg_stitch_sfmc_email_jobs.sql
+++ macros/stitch_sfmc_email/staging/stg_stitch_sfmc_email_jobs.sql
@@ -5,11 +5,11 @@ select distinct
     safe_cast(fromname as string) as from_name,
     safe_cast(fromemail as string) as from_email,
     safe_cast(
-        {{ dbt_date.convert_timezone("cast(coalesce(schedtime, pickup_dt) as timestamp)") }} as timestamp
+        {{ dbt_date.convert_timezone("cast(coalesce(sched_dt, pickup_dt) as timestamp)") }} as timestamp
     ) as best_guess_timestamp,
-    safe_cast( {{ dbt_date.convert_timezone("cast(schedtime as timestamp)") }} as timestamp) as scheduled_timestamp,
-    safe_cast({{ dbt_date.convert_timezone("cast(pickuptime as timestamp)") }} as timestamp) as pickup_timestamp,
-    safe_cast({{ dbt_date.convert_timezone("cast(deliveredtime as timestamp)") }} as timestamp) as delivered_timestamp,
+    safe_cast( {{ dbt_date.convert_timezone("cast(coalesce(sched_dt, NULL) as timestamp)") }} as timestamp) as scheduled_timestamp,
+    safe_cast({{ dbt_date.convert_timezone("cast(pickup_dt, NULL as timestamp)") }} as timestamp) as pickup_timestamp,
+    safe_cast({{ dbt_date.convert_timezone("cast(delivered_dt, NULL as timestamp)") }} as timestamp) as delivered_timestamp,
     safe_cast(emailname as string) as email_name,
     safe_cast(emailsubject as string) as email_subject,
     safe_cast(category as string) as category,
