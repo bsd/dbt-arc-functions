1a70bf50ad7f463c4548c62415a0225584a5363a
Frydafly <fryda.guedes@gmail.com>
2023-05-26 14:17:36 -0500

    revert actions

--- macros/stitch_sfmc_email/staging/stg_stitch_sfmc_email_actions_rollup.sql
+++ macros/stitch_sfmc_email/staging/stg_stitch_sfmc_email_actions_rollup.sql
@@ -1,10 +1,7 @@
 
 {% macro create_stg_stitch_sfmc_email_actions_rollup(
-    reference_name="stg_stitch_sfmc_email_actions"
+    reference_name="stg_src_stitch_email_actions"
 ) %}
-select distinct message_id, sum(actions) as actions
-from {{ ref(reference_name) }}
-group by 1
 
 select distinct safe_cast(jobid as string) as message_id, null as actions
 from ({{ dbt_utils.union_relations(relations) }})
