392cd87f4037f1df787301d10d60d774589d1a1f
Frydafly <fryda.guedes@gmail.com>
2023-06-08 17:52:06 -0500

    sqlfmt

--- macros/ga4_google_analytics_web/staging/stg_ga4_session_daily_jobs.sql
+++ macros/ga4_google_analytics_web/staging/stg_ga4_session_daily_jobs.sql
@@ -1,48 +1,48 @@
-{% macro create_stg_ga4_session_daily_jobs(
-    reference_name='dim_ga4__sessions_daily'
-) %}
-    with base as (select 
-    session_partition_date as session_date,
-    session_partition_key as session_key,
-    session_default_channel_grouping as default_channel_grouping,
-    session_medium,
-    session_source,
-    session_campaign,
-    pages,
-    landing_page_hostname as hostname,
-    landing_page_path as page_path,
-    referrer,
-    geo_region,
-    device_category,
-    device_language
-    from
-        {{ref(reference_name)}}
-        ), 
-    deduplicated as (
-    select 
-    *,
-    ROW_NUMBER() OVER (partition by session_date, session_key order by session_date, session_key) as row_num
-    from base
+{% macro create_stg_ga4_session_daily_jobs(reference_name="dim_ga4__sessions_daily") %}
+    with
+        base as (
+            select
+                session_partition_date as session_date,
+                session_partition_key as session_key,
+                session_default_channel_grouping as default_channel_grouping,
+                session_medium,
+                session_source,
+                session_campaign,
+                pages,
+                landing_page_hostname as hostname,
+                landing_page_path as page_path,
+                referrer,
+                geo_region,
+                device_category,
+                device_language
+            from {{ ref(reference_name) }}
+        ),
+        deduplicated as (
+            select
+                *,
+                row_number() over (
+                    partition by session_date, session_key
+                    order by session_date, session_key
+                ) as row_num
+            from base
 
-)
+        )
 
-SELECT
-    session_date,
-    session_key,
-    default_channel_grouping,
-    session_medium,
-    session_source,
-    session_campaign,
-    pages,
-    hostname,
-    page_path,
-    referrer,
-    geo_region,
-    device_category,
-    device_language
-FROM
-    deduplicated
-WHERE
-    row_num = 1
-    
+    select
+        session_date,
+        session_key,
+        default_channel_grouping,
+        session_medium,
+        session_source,
+        session_campaign,
+        pages,
+        hostname,
+        page_path,
+        referrer,
+        geo_region,
+        device_category,
+        device_language
+    from deduplicated
+    where row_num = 1
+
 {% endmacro %}
