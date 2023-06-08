3919c75cc72d39a0ca1e5d9766324423cbdb08c8
Frydafly <fryda.guedes@gmail.com>
2023-06-08 18:39:56 -0500

    name fix

--- macros/web/staging/stg_web_daily_max_engaged_rollup_unioned.sql
+++ macros/web/staging/stg_web_daily_max_engaged_rollup_unioned.sql
@@ -1,4 +1,4 @@
-{% macro create_stg_web_daily_engagement_time_rollup_unioned() %}
+{% macro create_stg_web_daily_max_engaged_rollup_unioned() %}
     {% set relations = dbt_arc_functions.relations_that_match_regex(
         "^stg_.*_web_daily_max_engaged_users_rollup$"
     ) %}
