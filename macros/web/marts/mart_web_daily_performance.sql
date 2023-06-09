68f0832704d6b7c3a5b4ae9edbe1d98731418750
Frydafly <fryda.guedes@gmail.com>
2023-06-09 16:08:18 -0500

    yamlfix and sqlfmt

--- macros/web/marts/mart_web_daily_performance.sql
+++ macros/web/marts/mart_web_daily_performance.sql
@@ -1,16 +1,18 @@
-{% macro create_mart_web_daily_performance(reference_name="mart_web_session_and_daily_performance") %}
-select 
-session_date,
-SUM(page_views) as page_views,
-SUM(engagement_time_msec) as engagement_time_msec,
-SUM(max_engaged) as max_engaged,
-SUM(new_users) as new_users,
-SUM(unique_users) as total_users,
-sum(page_views) as page_views,
-sum(purchases) as purchases,
-count(distinct session_key) as total_sessions
+{% macro create_mart_web_daily_performance(
+    reference_name="mart_web_session_and_daily_performance"
+) %}
+    select
+        session_date,
+        sum(page_views) as page_views,
+        sum(engagement_time_msec) as engagement_time_msec,
+        sum(max_engaged) as max_engaged,
+        sum(new_users) as new_users,
+        sum(unique_users) as total_users,
+        sum(page_views) as page_views,
+        sum(purchases) as purchases,
+        count(distinct session_key) as total_sessions
 
-from {{ref(reference_name)}}
-GROUP BY session_date
+    from {{ ref(reference_name) }}
+    group by session_date
 
 {% endmacro %}
