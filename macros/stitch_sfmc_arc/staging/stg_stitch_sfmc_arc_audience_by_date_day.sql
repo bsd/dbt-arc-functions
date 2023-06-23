630524fa4792f27dc892def5722abbe922716d83
Derrick Mondowney <109629735+dmbluestate@users.noreply.github.com>
2023-06-22 21:37:46 -0400

    add macros for arc_audience_date_spine and arc_audience_by_date_day

---
+++ macros/stitch_sfmc_arc/staging/stg_stitch_sfmc_arc_audience_by_date_day.sql
@@ -0,0 +1,39 @@
+{% macro create_stg_stitch_sfmc_arc_audience_by_date_day(
+    date_spine = "stg_stitch_sfmc_arc_audience_date_spine",
+    audience_snapshot = "arc_audience_snapshot"
+) %}
+
+WITH audience_by_date_day as (
+    select
+    d.date as date_day,
+    s.subscriberkey as person_id,
+    s.__donoraudience_ as arc_audience
+from {{ ref(date_spine) }} as d
+inner join
+    {{ ref(audience_snapshot) }} as s
+    on
+        d.date >= date(CAST(CONCAT(Substr(s.dbt_valid_from, 0, 22), " America/New_York") as timestamp), "America/New_York")
+        and (d.date <= date(CAST(CONCAT(Substr(s.dbt_valid_to, 0, 22), " America/New_York") as timestamp), "America/New_York")
+        or date(CAST(CONCAT(Substr(s.dbt_valid_to, 0, 22), " America/New_York") as timestamp), "America/New_York") is null)
+), deduplicated_table as (
+    SELECT
+        date_day,
+        person_id,
+        arc_audience,
+        ROW_NUMBER()
+            OVER (PARTITION BY date_day, person_id ORDER BY arc_audience)
+            AS row_num
+    FROM audience_by_date_day
+
+)
+
+SELECT
+    date_day,
+    person_id,
+    arc_audience
+FROM deduplicated_table
+WHERE row_num = 1
+
+
+
+{% endmacro %}
--- macros/stitch_sfmc_arc/staging/stg_stitch_sfmc_arc_audience_date_spine.sql
+++ macros/stitch_sfmc_arc/staging/stg_stitch_sfmc_arc_audience_date_spine.sql
@@ -1,24 +1,23 @@
-{% macro create_stg_stitch_sfmc_arc_audience_date_spine() %}
-    {% set relations = dbt_arc_functions.relations_that_match_regex(
-        "^arc_audience$",
-        is_source=True,
-        source_name="src_stitch_sfmc_arc",
-        schema_to_search="src_stitch_sfmc_arc_authorized",
-    ) %}
+{% macro stg_stitch_sfmc_arc_audience_date_spine(
+    audience_snapshot = "arc_audience_snapshot"
+) %}
 
 select date
 from
     unnest(
         generate_date_array(
             (select min(date(CAST(CONCAT(Substr(dbt_valid_from, 0, 22), " America/New_York") as timestamp), "America/New_York"))
-            from {{ ref("arc_audience_snapshot") }}),
-
+            from {{ ref(audience_snapshot) }}),
 
             ifnull(
                 (select max(date(CAST(CONCAT(Substr(dbt_valid_to, 0, 22), " America/New_York") as timestamp), "America/New_York"))
 
-                from {{ ref("arc_audience_snapshot") }}),
+                from {{ ref(audience_snapshot) }}),
                 current_date()
             )
         )
     ) as date
+
+
+
+{% endmacro %}
