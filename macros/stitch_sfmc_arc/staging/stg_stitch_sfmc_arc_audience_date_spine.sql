52bb3ee4e342f87b31209669eb6d62a4389ed009
Derrick Mondowney <109629735+dmbluestate@users.noreply.github.com>
2023-06-22 21:15:43 -0400

    add date spine model

--- macros/stitch_sfmc_arc/staging/stg_stitch_sfmc_arc_audience_date_spine.sql
+++ macros/stitch_sfmc_arc/staging/stg_stitch_sfmc_arc_audience_date_spine.sql
@@ -1,6 +1,6 @@
 {% macro create_stg_stitch_sfmc_arc_audience_date_spine() %}
     {% set relations = dbt_arc_functions.relations_that_match_regex(
-        "^arc_person$",
+        "^arc_audience$",
         is_source=True,
         source_name="src_stitch_sfmc_arc",
         schema_to_search="src_stitch_sfmc_arc_authorized",
@@ -10,9 +10,14 @@ select date
 from
     unnest(
         generate_date_array(
-            (select min(date(dbt_valid_from)) from {{ ref("person_snapshot") }}),
+            (select min(date(CAST(CONCAT(Substr(dbt_valid_from, 0, 22), " America/New_York") as timestamp), "America/New_York"))
+            from {{ ref("arc_audience_snapshot") }}),
+
+
             ifnull(
-                (select max(date(dbt_valid_to)) from {{ ref("person_snapshot") }}),
+                (select max(date(CAST(CONCAT(Substr(dbt_valid_to, 0, 22), " America/New_York") as timestamp), "America/New_York"))
+
+                from {{ ref("arc_audience_snapshot") }}),
                 current_date()
             )
         )
