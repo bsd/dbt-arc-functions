dccc4ddd7e16ead234d9cebb169783a092e3d83e
Frydafly <fryda.guedes@gmail.com>
2023-07-19 15:00:15 -0500

    removing duplicate model and adding note to campaigns date

--- macros/stitch_sfmc_email/staging/stg_stitch_sfmc_customized_email_campaign_dates.sql
+++ macros/stitch_sfmc_email/staging/stg_stitch_sfmc_customized_email_campaign_dates.sql
@@ -1,6 +1,8 @@
 {% macro create_stg_stitch_sfmc_customized_email_campaign_dates(
     reference_name="stg_src_stitch_email_job"
 ) %}
+
+-- reference campaigns model so that no repetition is necessary in case when for campaigns
     select
         safe_cast(coalesce(sched_dt, pickup_dt) as timestamp) as campaign_timestamp,
         safe_cast(null as string) as crm_campaign,
