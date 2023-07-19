80845d3d881629ad5b9e4f67cd7e4718626f6bcb
Frydafly <fryda.guedes@gmail.com>
2023-07-19 15:25:12 -0500

    naming change

--- macros/stitch_sfmc_email/staging/stg_stitch_sfmc_email_transactions_summary_unioned.sql
+++ macros/stitch_sfmc_email/staging/stg_stitch_sfmc_email_transactions_summary_unioned.sql
@@ -1,4 +1,4 @@
-{% macro create_stg_stitch_sfmc_transactions_summary_unioned() %}
+{% macro create_stg_stitch_sfmc_email_transactions_summary_unioned() %}
     {% set relations = dbt_arc_functions.relations_that_match_regex(
         "^stg_stitch_sfmc_.*_transactions$",
     ) %}
