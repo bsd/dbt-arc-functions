32c0593f65676b98fa546743b77466e760348eb2
Frydafly <fryda.guedes@gmail.com>
2023-07-19 11:15:35 -0500

    only cases where total gifts > 0

--- macros/stitch_sfmc_email/staging/stg_stitch_sfmc_customized_email_transactions_sourced_rollup.sql
+++ macros/stitch_sfmc_email/staging/stg_stitch_sfmc_customized_email_transactions_sourced_rollup.sql
@@ -52,5 +52,6 @@
         safe_cast(total_monthly_revenue as numeric) as total_monthly_revenue,
         safe_cast(total_monthly_gifts as int) as total_monthly_gifts
     from grouped
+    having total_gifts > 0
 
 {% endmacro %}
