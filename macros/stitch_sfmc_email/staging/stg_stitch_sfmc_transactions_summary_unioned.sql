8e28d2b43735237aa024803a9ede0ac212d0a712
Frydafly <fryda.guedes@gmail.com>
2023-07-19 10:15:29 -0500

    try

--- macros/stitch_sfmc_email/staging/stg_stitch_sfmc_transactions_summary_unioned.sql
+++ macros/stitch_sfmc_email/staging/stg_stitch_sfmc_transactions_summary_unioned.sql
@@ -2,7 +2,6 @@
     {% set relations = dbt_arc_functions.relations_that_match_regex(
         "^stg_stitch_sfmc_[A-Za-z0-9]{3}_transactions$",
     ) %}
-    select distinct *
-    from ({{ dbt_utils.union_relations(relations) }})
+    {{ dbt_utils.union_relations(relations) }}
     where transaction_date <= DATE_SUB(CURRENT_DATE(), INTERVAL 5 YEAR)
 {% endmacro %}
