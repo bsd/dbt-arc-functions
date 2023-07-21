042c2aecbf96addf2c3abcf1693ffe6cca1e14fb
Timothy Ryan <tryan@bluestate.co>
2023-07-21 13:24:20 -0400

    ARC-1539 update name to create_

--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_parameterized_audience_transactions_enriched.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_parameterized_audience_transactions_enriched.sql
@@ -1,4 +1,4 @@
-{% macro stg_stitch_sfmc_parameterized_audience_transactions_enriched(    
+{% macro create_stg_stitch_sfmc_parameterized_audience_transactions_enriched(    
     best_guess_inbound_channel,
     reference_name="stg_stitch_sfmc_audience_transactions_summary_unioned"
 ) %}
