c636811d520d7058375349152d6686f74373f70e
Timothy Ryan <tryan@bluestate.co>
2023-07-25 13:27:23 -0400

    ARC-1439 yamlfix sqlfmt

--- macros/audience_budget/staging/stg_adhoc_google_spreadsheets_audience_monthly_budget.sql
+++ macros/audience_budget/staging/stg_adhoc_google_spreadsheets_audience_monthly_budget.sql
@@ -1,15 +1,15 @@
 {% macro create_stg_adhoc_google_spreadsheets_audience_monthly_budget() %}
-select
-    start_date,
-    end_date,
-    platform,
-    donor_audience,
-    total_revenue_budget,
-    loyalty_new_donor_targets,
-    loyalty_unknown_donor_targets,
-    loyalty_retained_donor_targets,
-    loyalty_retained_three_donor_targets,
-    loyalty_reinstated_donor_targets,
-from {{ source("audience_budget", "spreadsheet_audience_monthly_budget") }}
+    select
+        start_date,
+        end_date,
+        platform,
+        donor_audience,
+        total_revenue_budget,
+        loyalty_new_donor_targets,
+        loyalty_unknown_donor_targets,
+        loyalty_retained_donor_targets,
+        loyalty_retained_three_donor_targets,
+        loyalty_reinstated_donor_targets,
+    from {{ source("audience_budget", "spreadsheet_audience_monthly_budget") }}
 
 {% endmacro %}
