fce845cb2981fc8d362bd9eec0deda6d9c1221c2
Timothy Ryan <tryan@bluestate.co>
2023-07-27 14:47:57 -0400

    ARC-1439 cast as datetime for datespine

--- macros/audience_budget/staging/stg_adhoc_google_spreadsheets_audience_monthly_budget.sql
+++ macros/audience_budget/staging/stg_adhoc_google_spreadsheets_audience_monthly_budget.sql
@@ -1,7 +1,7 @@
 {% macro create_stg_adhoc_google_spreadsheets_audience_monthly_budget() %}
     select
-        SAFE_CAST(start_date AS TIMESTAMP) as start_date,
-        SAFE_CAST(end_date AS TIMESTAMP) as end_date,
+        SAFE_CAST(start_date AS DATETIME) as start_date,
+        SAFE_CAST(end_date AS DATETIME) as end_date,
         SAFE_CAST(platform AS STRING) as platform,
         SAFE_CAST(donor_audience AS STRING) as donor_audience,
         SAFE_CAST(total_revenue_budget AS FLOAT64) as total_revenue_budget,
