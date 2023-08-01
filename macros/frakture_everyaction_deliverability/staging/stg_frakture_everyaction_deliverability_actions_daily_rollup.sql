1e1bda5a5c9ae543692ed75e3c8988e4e36950cf
Frydafly <fryda.guedes@gmail.com>
2023-08-01 15:11:45 -0500

    name reference

--- macros/frakture_everyaction_deliverability/staging/stg_frakture_everyaction_deliverability_actions_daily_rollup.sql
+++ macros/frakture_everyaction_deliverability/staging/stg_frakture_everyaction_deliverability_actions_daily_rollup.sql
@@ -1,5 +1,5 @@
 {% macro create_stg_frakture_everyaction_deliverability_actions_daily_rollup(
-    reference_name="stg_frakture_everyaction_person_message_stat_unioned_with_domain"
+    reference_name="stg_frakture_everyaction_deliverability_message_stat_unioned_with_domain"
 ) %}
     select
         safe_cast(sent_ts as date) as sent_date,
