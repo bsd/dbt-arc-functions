12871b86e597a0aa776cd276fbf9070ddbb9f9d3
Frydafly <fryda.guedes@gmail.com>
2023-08-07 13:41:23 -0400

    typo

--- macros/frakture_sfmc_deliverability/staging/stg_frakture_sfmc_deliverability_opens_daily_rollup.sql
+++ macros/frakture_sfmc_deliverability/staging/stg_frakture_sfmc_deliverability_opens_daily_rollup.sql
@@ -1,4 +1,4 @@
-{% macro create_stg_frakture_sfmc_delierability_opens_daily_rollup(
+{% macro create_stg_frakture_sfmc_deliverability_opens_daily_rollup(
     reference_name="stg_frakture_sfmc_deliverability_message_stat_unioned_with_domain"
 ) %}
     select
