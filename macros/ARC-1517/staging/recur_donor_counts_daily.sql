f020f0b54cc3d99628bbf8a6abe5a76127c359cb
Timothy Ryan <tryan@bluestate.co>
2023-08-08 15:19:01 -0400

    sqlfmt

--- macros/ARC-1517/staging/recur_donor_counts_daily.sql
+++ macros/ARC-1517/staging/recur_donor_counts_daily.sql
@@ -1,26 +1,37 @@
-SELECT date_day,
-'daily' AS interval_type,
-donor_audience,
-join_source as platform,
-COUNT(person_id) AS total_recur_donor_counts,
-COUNT(CASE WHEN donor_loyalty = 'new' THEN 1 END) AS new_recur_donor_counts,
-COUNT(CASE WHEN donor_loyalty = 'retained' THEN 1 END) AS retained_recur_donor_counts,
-COUNT(CASE WHEN donor_loyalty = 'retained3' THEN 1 END) AS retained3_recur_donor_counts,
-COUNT(CASE WHEN donor_engagement = 'active' THEN 1 END) AS active_recur_donor_counts,
-COUNT(CASE WHEN donor_engagement = 'lapsed' THEN 1 END) AS lapsed_recur_donor_counts,
+select
+    date_day,
+    'daily' as interval_type,
+    donor_audience,
+    join_source as platform,
+    count(person_id) as total_recur_donor_counts,
+    count(case when donor_loyalty = 'new' then 1 end) as new_recur_donor_counts,
+    count(
+        case when donor_loyalty = 'retained' then 1 end
+    ) as retained_recur_donor_counts,
+    count(
+        case when donor_loyalty = 'retained3' then 1 end
+    ) as retained3_recur_donor_counts,
+    count(
+        case when donor_engagement = 'active' then 1 end
+    ) as active_recur_donor_counts,
+    count(
+        case when donor_engagement = 'lapsed' then 1 end
+    ) as lapsed_recur_donor_counts,
 
-FROM ( SELECT
-date_day,
-person_id,
-donor_audience,
-donor_engagement,
-donor_loyalty,
-gift_size_str,
-join_source,
-join_amount_str,
-join_month_year_str,
-FROM {{ ref('ARC-1511')}}
-)
+from
+    (
+        select
+            date_day,
+            person_id,
+            donor_audience,
+            donor_engagement,
+            donor_loyalty,
+            gift_size_str,
+            join_source,
+            join_amount_str,
+            join_month_year_str,
+        from {{ ref("ARC-1511") }}
+    )
 
-WHERE donor_audience = 'recurring'
-GROUP BY 1, 2, 3
+where donor_audience = 'recurring'
+group by 1, 2, 3
