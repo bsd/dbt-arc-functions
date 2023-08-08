3cc50154738013009ca1cf0705333a9532105c4f
Timothy Ryan <tryan@bluestate.co>
2023-08-08 15:18:30 -0400

    ARC-1517 sqlfmt

--- macros/ARC-1517/staging/recur_donor_counts_combined.sql
+++ macros/ARC-1517/staging/recur_donor_counts_combined.sql
@@ -1,35 +1,38 @@
-SELECT date_day,
- interval_type,
- donor_audience,
- platform,
- total_recur_donor_counts,
- new_recur_donor_counts,
- retained_recur_donor_counts,
- retained3_recur_donor_counts,
- active_recur_donor_counts,
- lapsed_recur_donor_counts,
-FROM {{ ref ('recur_donor_counts_daily')}}
-UNION ALL
-SELECT date_day,
- interval_type,
- donor_audience,
- platform,
- total_recur_donor_counts,
- new_recur_donor_counts,
- retained_recur_donor_counts,
- retained3_recur_donor_counts,
- active_recur_donor_counts,
- lapsed_recur_donor_counts,
-FROM {{ ref ('recur_donor_counts_monthly')}}
-UNION ALL
-SELECT date_day,
- interval_type,
- donor_audience,
- platform,
- total_recur_donor_counts,
- new_recur_donor_counts,
- retained_recur_donor_counts,
- retained3_recur_donor_counts,
- active_recur_donor_counts,
- lapsed_recur_donor_counts,
-FROM {{ ref ('recur_donor_counts_yearly')}}
+select
+    date_day,
+    interval_type,
+    donor_audience,
+    platform,
+    total_recur_donor_counts,
+    new_recur_donor_counts,
+    retained_recur_donor_counts,
+    retained3_recur_donor_counts,
+    active_recur_donor_counts,
+    lapsed_recur_donor_counts,
+from {{ ref("recur_donor_counts_daily") }}
+union all
+select
+    date_day,
+    interval_type,
+    donor_audience,
+    platform,
+    total_recur_donor_counts,
+    new_recur_donor_counts,
+    retained_recur_donor_counts,
+    retained3_recur_donor_counts,
+    active_recur_donor_counts,
+    lapsed_recur_donor_counts,
+from {{ ref("recur_donor_counts_monthly") }}
+union all
+select
+    date_day,
+    interval_type,
+    donor_audience,
+    platform,
+    total_recur_donor_counts,
+    new_recur_donor_counts,
+    retained_recur_donor_counts,
+    retained3_recur_donor_counts,
+    active_recur_donor_counts,
+    lapsed_recur_donor_counts,
+from {{ ref("recur_donor_counts_yearly") }}
