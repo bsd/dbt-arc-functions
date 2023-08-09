219e909e0c4365e3dda57669e18c4f12b5e7dc60
Timothy Ryan <tryan@bluestate.co>
2023-08-09 15:01:26 -0400

    ARC-1516 make macros

--- macros/ARC-1516/staging/onetime_donor_count_budget_combined.sql
+++ macros/ARC-1516/staging/onetime_donor_count_budget_combined.sql
@@ -1,32 +1,35 @@
-select
-    date_day,
-    interval_type,
-    donor_audience,
-    join_source,
-    1 x_donor_count_budget,
-    1 x_new_donor_count_budget,
-    1 x_donor_count_budget_cumulative,
-    1 x_new_donor_count_cumulative,
-from {{ ref("onetime_donor_count_budget_daily") }}
-union all
-select
-    date_day,
-    interval_type,
-    donor_audience,
-    join_source,
-    1 x_donor_count_budget,
-    1 x_new_donor_count_budget,
-    1 x_donor_count_budget_cumulative,
-    1 x_new_donor_count_cumulative,
-from {{ ref("onetime_donor_count_budget_monthly") }}
-union all
-select
-    date_day,
-    interval_type,
-    donor_audience,
-    join_source,
-    1 x_donor_count_budget,
-    1 x_new_donor_count_budget,
-    1 x_donor_count_budget_cumulative,
-    1 x_new_donor_count_cumulative,
-from {{ ref("onetime_donor_count_budget_yearly") }}
+{% macro create_onetime_donor_count_budget_combined() %}
+    select
+        date_day,
+        interval_type,
+        donor_audience,
+        join_source,
+        onetime_donor_count_budget,
+        onetime_new_donor_count_budget,
+        onetime_donor_count_budget_cumulative,
+        onetime_new_donor_count_cumulative
+    from {{ ref("onetime_donor_count_budget_daily") }}
+    union all
+    select
+        date_day,
+        interval_type,
+        donor_audience,
+        join_source,
+        onetime_donor_count_budget,
+        onetime_new_donor_count_budget,
+        onetime_donor_count_budget_cumulative,
+        onetime_new_donor_count_cumulative
+    from {{ ref("onetime_donor_count_budget_monthly") }}
+    union all
+    select
+        date_day,
+        interval_type,
+        donor_audience,
+        join_source,
+        onetime_donor_count_budget,
+        onetime_new_donor_count_budget,
+        onetime_donor_count_budget_cumulative,
+        onetime_new_donor_count_cumulative
+    from {{ ref("onetime_donor_count_budget_yearly") }}
+
+{% endmacro %}
--- macros/ARC-1516/staging/onetime_donor_count_budget_yearly.sql
+++ macros/ARC-1516/staging/onetime_donor_count_budget_yearly.sql
@@ -1,31 +1,43 @@
-select
-    last_day(date_day, year) as date_day,
-    'yearly' as interval_type,
-    donor_audience,
-    platform as join_source,
-    sum(total_revenue_budget_by_day) as onetime_donor_count_budget,
-    sum(loyalty_new_donor_targets_by_day) as onetime_new_donor_count_budget,
-    sum(total_revenue_budget_by_days) over (
-        partition by
-            donor_audience,
-            platform,
-            {{
-                dbt_arc_functions.get_fiscal_year(
-                    "date_day", var("fiscal_year_start")
-                )
-            }}
-        order by date_day
-    ) as onetime_donor_count_budget_cumulative,
-    sum(loyalty_new_donor_targets_by_day) over (
-        partition by
-            donor_audience,
-            platform,
-            {{
-                dbt_arc_functions.get_fiscal_year(
-                    "date_day", var("fiscal_year_start")
-                )
-            }}
-        order by date_day
-    ) as onetime_new_donor_count_cumulative,
-from {{ ref("stg_audience_budget_by_day") }}
-where donor_audience != 'recurring'
+{% macro create_onetime_donor_count_budget_yearly() %}
+    with
+        sums as (
+            select
+                last_day(date_day, year) as date_day,
+                'yearly' as interval_type,
+                donor_audience,
+                platform as join_source,
+                sum(total_revenue_budget_by_day) as onetime_donor_count_budget,
+                sum(loyalty_new_donor_targets_by_day) as onetime_new_donor_count_budget
+            from {{ ref("stg_audience_budget_by_day") }}
+            where donor_audience != 'recurring'
+            group by 1, 2, 3, 4
+        )
+
+    select
+        date_day,
+        interval_type,
+        donor_audience,
+        join_source,
+        onetime_donor_count_budget,
+        onetime_new_donor_count_budget,
+        sum(onetime_donor_count_budget) over (
+            partition by
+                {{
+                    dbt_arc_functions.get_fiscal_year(
+                        "date_day", var("fiscal_year_start")
+                    )
+                }}
+            order by date_day
+        ) as onetime_donor_count_budget_cumulative,
+        sum(onetime_new_donor_count_budget) over (
+            partition by
+                {{
+                    dbt_arc_functions.get_fiscal_year(
+                        "date_day", var("fiscal_year_start")
+                    )
+                }}
+            order by date_day
+        ) as onetime_new_donor_count_cumulative
+    from sums
+
+{% endmacro %}
