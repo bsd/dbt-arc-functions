e78d2167c8b9cfa1f07f5795f753f4944ecbe167
Timothy Ryan <tryan@bluestate.co>
2023-08-15 14:55:38 -0400

    ARC-1551 parameterize file

---
+++ macros/stitch_sfmc_email/staging/stg_stitch_sfmc_parameterized_email_campaigns.sql
@@ -0,0 +1,19 @@
+{% macro create_stg_stitch_sfmc_parameterized_email_campaigns(
+    reference_name="stg_src_stitch_email_job",
+    audience,
+    source_code_campaign
+) %}
+
+    select distinct
+        safe_cast(job_id as string) as message_id,
+        safe_cast('sfmc' as string) as crm_entity,
+        safe_cast(null as string) as source_code_entity,
+        {{ audience }} as audience,
+        safe_cast(null as string) as recurtype,
+        safe_cast(null as string) as campaign_category,
+        safe_cast(null as string) as crm_campaign,
+        {{source_code_campaign}} as source_code_campaign
+
+    from {{ ref(reference_name) }}
+
+{% endmacro %}
