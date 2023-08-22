e6458adc58603870296a92cb05178d7e3413d20b
Frydafly <fryda.guedes@gmail.com>
2023-08-22 13:34:39 -0500

    correct

--- macros/stitch_sfmc_deliverability/staging/stg_stitch_sfmc_deliverability_jobs_distinct.sql
+++ macros/stitch_sfmc_deliverability/staging/stg_stitch_sfmc_deliverability_jobs_distinct.sql
@@ -1,5 +1,5 @@
 {% macro create_stg_stitch_sfmc_deliverability_jobs_distinct() %}
-    with union as (
+    with unions as (
         select job_id, event_dt, domain from {{ref('stg_src_stitch_email_action')}}
         UNION DISTINCT 
         select job_id, event_dt, domain from {{ref('stg_src_stitch_email_bounce')}}
@@ -15,5 +15,5 @@
         safe_cast(job_id as string) as message_id,
         safe_cast(event_dt as date) as sent_date,
         safe_cast(domain as string) as email_domain
-    from {{ ref(reference_name) }}
+    from unions
 {% endmacro %}
