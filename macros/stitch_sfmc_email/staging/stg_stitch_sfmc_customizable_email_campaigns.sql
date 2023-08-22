f15bb27d111ddd565edaff7b2f879f61c89ab6bb
Timothy Ryan <tryan@bluestate.co>
2023-08-22 10:12:55 -0400

    ARC-1551 migrate to customizable design

--- macros/stitch_sfmc_email/staging/stg_stitch_sfmc_customizable_email_campaigns.sql
+++ macros/stitch_sfmc_email/staging/stg_stitch_sfmc_customizable_email_campaigns.sql
@@ -1,4 +1,4 @@
-{% macro create_stg_stitch_sfmc_parameterized_email_campaigns(
+{% macro create_stg_stitch_sfmc_customizable_email_campaigns(
     audience,
     source_code_campaign,
     reference_name="stg_src_stitch_email_job")
@@ -6,13 +6,13 @@
 
     select distinct
         safe_cast(job_id as string) as message_id,
-        safe_cast('sfmc' as string) as crm_entity,
+        safe_cast(null as string) as crm_entity,
         safe_cast(null as string) as source_code_entity,
-        {{ audience }} as audience,
+        safe_cast(null as string) as audience,
         safe_cast(null as string) as recurtype,
         safe_cast(null as string) as campaign_category,
         safe_cast(null as string) as crm_campaign,
-        {{source_code_campaign}} as source_code_campaign
+        safe_cast(null as string) as source_code_campaign
 
     from {{ ref(reference_name) }}
 
