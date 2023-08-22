66c7fe9f2a6ab7b7b57f881df86d90746e5a9c0c
Frydafly <fryda.guedes@gmail.com>
2023-08-22 11:47:50 -0500

    typo

--- macros/stitch_sfmc_deliverability/staging/stg_stitch_sfmc_deliverability_actions_daily_rollup.sql
+++ macros/stitch_sfmc_deliverability/staging/stg_stitch_sfmc_deliverability_actions_daily_rollup.sql
@@ -4,7 +4,7 @@
     select
         safe_cast(event_dt as date) as sent_date,
         safe_cast(job_id as string) as message_id,
-        safe_cast(domain string) as email_domain,
+        safe_cast(domain as string) as email_domain,
         0 as actions
     from {{ ref(reference_name) }}
     group by 1, 2, 3
