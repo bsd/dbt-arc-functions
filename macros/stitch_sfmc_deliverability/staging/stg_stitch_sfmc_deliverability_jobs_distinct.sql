a702d4e62565667644397b729d153bd0500ae106
Frydafly <fryda.guedes@gmail.com>
2023-08-22 11:40:07 -0500

    distinct jobs name

--- macros/stitch_sfmc_deliverability/staging/stg_stitch_sfmc_deliverability_jobs_distinct.sql
+++ macros/stitch_sfmc_deliverability/staging/stg_stitch_sfmc_deliverability_jobs_distinct.sql
@@ -1,4 +1,4 @@
-{% macro create_stg_stitch_sfmc_deliverability_jobs(
+{% macro create_stg_stitch_sfmc_deliverability_jobs_distinct(
     reference_name="stg_src_stitch_email_sent"
 ) %}
     select distinct
