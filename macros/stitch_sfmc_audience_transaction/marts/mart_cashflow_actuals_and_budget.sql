aba9e84a74f067ee55f5d79ac4515aae54ee3b7f
Derrick Mondowney <109629735+dmbluestate@users.noreply.github.com>
2023-08-22 14:52:33 -0400

    changed macro name

--- macros/stitch_sfmc_audience_transaction/marts/mart_cashflow_actuals_and_budget.sql
+++ macros/stitch_sfmc_audience_transaction/marts/mart_cashflow_actuals_and_budget.sql
@@ -1,6 +1,6 @@
 {% macro create_mart_cashflow_actuals_and_budget(
     audience_transactions="stg_stitch_sfmc_arc_audience_union_transaction_joined_enriched",
-    budget_revenue="stg_audience_budget_by_day_enriched",
+    budget_revenue="stg_audience_budget_by_day_enriched"
 
 ) %}
 
