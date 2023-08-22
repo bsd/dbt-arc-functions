ee407c328545260d03608ddfca7e37b889c0edf9
Frydafly <fryda.guedes@gmail.com>
2023-08-22 14:49:46 -0500

    sqlfmt .

--- macros/stitch_sfmc_deliverability/staging/stg_stitch_sfmc_deliverability_jobs_distinct.sql
+++ macros/stitch_sfmc_deliverability/staging/stg_stitch_sfmc_deliverability_jobs_distinct.sql
@@ -1,16 +1,22 @@
 {% macro create_stg_stitch_sfmc_deliverability_jobs_distinct() %}
-    with unions as (
-        select job_id, event_dt, domain from {{ref('stg_src_stitch_email_action')}}
-        UNION DISTINCT 
-        select job_id, event_dt, domain from {{ref('stg_src_stitch_email_bounce')}}
-        UNION DISTINCT 
-        select job_id, event_dt, domain from {{ref('stg_src_stitch_email_click')}}
-        UNION DISTINCT
-        select job_id, event_dt, domain from {{ref('stg_src_stitch_email_open')}}
-        UNION DISTINCT
-        select job_id, event_dt, domain from {{ref('stg_src_stitch_email_unsubscribe')}}
-    )
-    
+    with
+        unions as (
+            select job_id, event_dt, domain
+            from {{ ref("stg_src_stitch_email_action") }}
+            union distinct
+            select job_id, event_dt, domain
+            from {{ ref("stg_src_stitch_email_bounce") }}
+            union distinct
+            select job_id, event_dt, domain
+            from {{ ref("stg_src_stitch_email_click") }}
+            union distinct
+            select job_id, event_dt, domain
+            from {{ ref("stg_src_stitch_email_open") }}
+            union distinct
+            select job_id, event_dt, domain
+            from {{ ref("stg_src_stitch_email_unsubscribe") }}
+        )
+
     select distinct
         safe_cast(job_id as string) as message_id,
         safe_cast(event_dt as date) as sent_date,
