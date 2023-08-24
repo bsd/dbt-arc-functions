1c9b5daf5d7d70bb557f7eb73453833adfd6af5c
Frydafly <fryda.guedes@gmail.com>
2023-08-24 16:41:01 -0500

    sqlfmt

--- macros/stitch_sfmc_email/staging/stg_src_stitch_email_action.sql
+++ macros/stitch_sfmc_email/staging/stg_src_stitch_email_action.sql
@@ -1,23 +1,22 @@
 {% macro create_stg_src_stitch_email_action() %}
 
-            select
-                cast(0 as int64) as account_id,
-                cast(0 as int64) as oyb_account_id,
-                cast(0 as int64) as job_id,
-                cast(0 as int64) as list_id,
-                cast(0 as int64) as batch_id,
-                cast(0 as int64) as subscriber_id,
-                '' as subscriber_key,
-                datetime(''
-                ) as event_dt,
-                cast('' as string) as domain,
-                cast('' as string) as url,
-                cast('' as string) as link_name,
-                cast('' as string) as link_content,
-                cast('' as bool) as is_unique,
-                cast('' as string) as triggerrer_send_definition_object_id,
-                cast('' as string) as triggered_send_customer_key,
-            from {{ source("stitch_sfmc_email", "click") }}  -- is click because UUSA does not have action
-            where jobid is not null
+    select
+        cast(0 as int64) as account_id,
+        cast(0 as int64) as oyb_account_id,
+        cast(0 as int64) as job_id,
+        cast(0 as int64) as list_id,
+        cast(0 as int64) as batch_id,
+        cast(0 as int64) as subscriber_id,
+        '' as subscriber_key,
+        datetime('') as event_dt,
+        cast('' as string) as domain,
+        cast('' as string) as url,
+        cast('' as string) as link_name,
+        cast('' as string) as link_content,
+        cast('' as bool) as is_unique,
+        cast('' as string) as triggerrer_send_definition_object_id,
+        cast('' as string) as triggered_send_customer_key,
+    from {{ source("stitch_sfmc_email", "click") }}  -- is click because UUSA does not have action
+    where jobid is not null
 
 {% endmacro %}
--- macros/stitch_sfmc_email/staging/stg_src_stitch_email_journeyactivity.sql
+++ macros/stitch_sfmc_email/staging/stg_src_stitch_email_journeyactivity.sql
@@ -1,13 +1,13 @@
 {% macro create_stg_src_stitch_email_journeyactivity() %}
 
-            select distinct
-                __versionid_ as version_id,
-                activityid as activity_id,
-                activityname as activity_name,
-                activityexternalkey as activity_external_key,
-                journeyactivityobjectid as journey_activity_object_id,
-                activitytype as activity_type
-            from {{ source("stitch_sfmc_email", "journeyactivity") }}
-            where activityid is not null
+    select distinct
+        __versionid_ as version_id,
+        activityid as activity_id,
+        activityname as activity_name,
+        activityexternalkey as activity_external_key,
+        journeyactivityobjectid as journey_activity_object_id,
+        activitytype as activity_type
+    from {{ source("stitch_sfmc_email", "journeyactivity") }}
+    where activityid is not null
 
 {% endmacro %}
