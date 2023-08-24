2cc370873472d5319dcd9658cbe22d1e524caf71
Timothy Ryan <tryan@bluestate.co>
2023-08-24 12:25:11 -0400

    ARC-1503 yamlfix and sqlfmt

--- macros/stitch_sfmc_arc/marts/mart_stitch_sfmc_arc_revenue_and_donor_count_by_lifetime_gifts.sql
+++ macros/stitch_sfmc_arc/marts/mart_stitch_sfmc_arc_revenue_and_donor_count_by_lifetime_gifts.sql
@@ -1,21 +1,26 @@
 {% macro create_mart_stitch_sfmc_arc_revenue_and_donor_count_by_lifetime_gifts(
-    reference_name='stg_stitch_sfmc_arc_audience_union_transaction_joined') %}
-SELECT
-    EXTRACT(YEAR FROM transaction_date_day) AS transaction_date_year,
-    EXTRACT(MONTH FROM transaction_date_day) AS transaction_date_month,
-    transaction_date_day AS transaction_date_day,
-    CASE
-        WHEN gift_count < 6 THEN "less than 6"
-        WHEN gift_count BETWEEN 6 AND 12 THEN "6-12"
-        WHEN gift_count BETWEEN 13 AND 24 THEN "13-24"
-        WHEN gift_count BETWEEN 25 AND 36 THEN "25-36"
-        ELSE "37+" END
-        AS recurring_gift_cumulative_str,
-    COUNT(DISTINCT person_id) AS donors,
-    SUM(amount) AS summed_amount,
-FROM {{ ref(reference_name) }}
-WHERE recurring = true
-GROUP BY 1, 2, 3, 4
-ORDER BY 1, 2, 3, 4
+    reference_name="stg_stitch_sfmc_arc_audience_union_transaction_joined"
+) %}
+    select
+        extract(year from transaction_date_day) as transaction_date_year,
+        extract(month from transaction_date_day) as transaction_date_month,
+        transaction_date_day as transaction_date_day,
+        case
+            when gift_count < 6
+            then "less than 6"
+            when gift_count between 6 and 12
+            then "6-12"
+            when gift_count between 13 and 24
+            then "13-24"
+            when gift_count between 25 and 36
+            then "25-36"
+            else "37+"
+        end as recurring_gift_cumulative_str,
+        count(distinct person_id) as donors,
+        sum(amount) as summed_amount,
+    from {{ ref(reference_name) }}
+    where recurring = true
+    group by 1, 2, 3, 4
+    order by 1, 2, 3, 4
 
 {% endmacro %}
