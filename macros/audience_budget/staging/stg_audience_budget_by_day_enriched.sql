a1263d7ec43c9beda66b2b6e57bc516b11a66c23
Derrick Mondowney <109629735+dmbluestate@users.noreply.github.com>
2023-08-29 16:36:28 -0400

    adjusted macro for stg_audience_budget_by_day_enriched.sql

--- macros/audience_budget/staging/stg_audience_budget_by_day_enriched.sql
+++ macros/audience_budget/staging/stg_audience_budget_by_day_enriched.sql
@@ -7,6 +7,11 @@ select
     date_spine_year,
     date_spine_month,
     date_spine_day,
+    {{ dbt_arc_functions.get_fiscal_year(
+        'date_day',
+        var('fiscal_year_start')) }}
+        AS fiscal_year,
+    DATE(date_day) as date_day,
     total_revenue_budget_by_day,
     loyalty_new_donor_targets_by_day,
     loyalty_unknown_donor_targets_by_day,
@@ -21,9 +26,7 @@ select
         when donor_audience = 'Leadership Giving' then 'midlevel'
         when donor_audience = 'Major' then 'major'
         when donor_audience = 'Unite' then 'unite'
-    end as donor_audience,
-    DATE(date_day) as date_day
-
+    end as donor_audience
 
 from {{ ref(budget_by_day) }}
 
