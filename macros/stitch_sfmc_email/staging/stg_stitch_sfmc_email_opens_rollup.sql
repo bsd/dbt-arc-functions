0e0f4027ae5cf5ab54827478a9b839f9eea2bb43
Frydafly <fryda.guedes@gmail.com>
2023-09-05 16:00:23 -0500

    sqlfmt.

--- macros/stitch_sfmc_email/staging/stg_stitch_sfmc_email_opens_rollup.sql
+++ macros/stitch_sfmc_email/staging/stg_stitch_sfmc_email_opens_rollup.sql
@@ -2,7 +2,8 @@
     reference_name="stg_src_stitch_email_open"
 ) %}
     select
-        safe_cast(job_id as string) as message_id, safe_cast(count(subscriber_key) as int) as opens
+        safe_cast(job_id as string) as message_id,
+        safe_cast(count(subscriber_key) as int) as opens
     from {{ ref(reference_name) }}
     group by 1
 {% endmacro %}
