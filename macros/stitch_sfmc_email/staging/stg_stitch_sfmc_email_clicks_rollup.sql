3b3ac26a47c5c67353ef5cbfdccc6c72a8b545ce
Frydafly <fryda.guedes@gmail.com>
2023-09-05 15:57:48 -0500

    bounces logic

--- macros/stitch_sfmc_email/staging/stg_stitch_sfmc_email_clicks_rollup.sql
+++ macros/stitch_sfmc_email/staging/stg_stitch_sfmc_email_clicks_rollup.sql
@@ -2,7 +2,7 @@
     reference_name="stg_src_stitch_email_click"
 ) %}
     select
-        safe_cast(job_id as string) as message_id, safe_cast(count(*) as int) as clicks
+        safe_cast(job_id as string) as message_id, safe_cast(count(subscriber_key) as int) as clicks
     from {{ ref(reference_name) }}
     group by 1
 {% endmacro %}
