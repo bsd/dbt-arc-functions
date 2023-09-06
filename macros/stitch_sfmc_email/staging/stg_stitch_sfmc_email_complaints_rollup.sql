137fd1be15381fd374017592d18ff5deec6c371e
Frydafly <fryda.guedes@gmail.com>
2023-09-06 15:12:34 -0500

    distinct subscriber key

--- macros/stitch_sfmc_email/staging/stg_stitch_sfmc_email_complaints_rollup.sql
+++ macros/stitch_sfmc_email/staging/stg_stitch_sfmc_email_complaints_rollup.sql
@@ -3,7 +3,7 @@
 ) %}
     select
         safe_cast(job_id as string) as message_id,
-        safe_cast(count(subscriber_key) as int) as complaints
+        safe_cast(count(distinct subscriber_key) as int) as complaints
     from {{ ref(reference_name) }}
     group by 1
 {% endmacro %}
