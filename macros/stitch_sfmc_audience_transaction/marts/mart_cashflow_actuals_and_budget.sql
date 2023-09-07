97973239cf4ebae7d4ea0d4d8c7761e840bd9b8f
Frydafly <fryda.guedes@gmail.com>
2023-09-07 13:55:17 -0500

    sqlfmt .

--- macros/stitch_sfmc_audience_transaction/marts/mart_cashflow_actuals_and_budget.sql
+++ macros/stitch_sfmc_audience_transaction/marts/mart_cashflow_actuals_and_budget.sql
@@ -15,97 +15,95 @@
             from {{ ref(audience_transactions) }} as audience_transactions
             group by 1, 2, 3, 4
             order by 4 desc
+        ),
+        original_mart as (
+            select
+                coalesce(
+                    safe_cast(base.fiscal_year as int64),
+                    safe_cast(budget_revenue.fiscal_year as int64)
+                ) as fiscal_year,
+                coalesce(
+                    safe_cast(extract(year from base.date_day) as int64),
+                    safe_cast(budget_revenue.date_spine_year as int64)
+                ) as year,
+                coalesce(
+                    safe_cast(extract(month from base.date_day) as int64),
+                    safe_cast(budget_revenue.date_spine_month as int64)
+                ) as month,
+                coalesce(
+                    safe_cast(extract(day from base.date_day) as int64),
+                    safe_cast(budget_revenue.date_spine_day as int64)
+                ) as day,
+                coalesce(base.date_day, budget_revenue.date_day) as date_day,
+                coalesce(
+                    base.donor_audience, budget_revenue.donor_audience
+                ) as donor_audience,
+                coalesce(base.channel, budget_revenue.platform) as channel,
+                coalesce(base.total_revenue_actuals, 0) as total_revenue_actuals,
+                coalesce(base.total_gifts_actuals, 0) as total_gifts_actuals,
+                budget_revenue.total_revenue_budget_by_day
+                as total_revenue_budget_by_day,
+                sum(total_revenue_actuals) over (
+                    partition by
+                        coalesce(base.donor_audience, budget_revenue.donor_audience),
+                        coalesce(base.channel, budget_revenue.platform),
+                        coalesce(base.fiscal_year, budget_revenue.fiscal_year)
+                    order by coalesce(base.date_day, budget_revenue.date_day)
+                ) as total_revenue_cumulative_fiscal_year
+            from base
+            full join
+                {{ ref(budget_revenue) }} as budget_revenue
+                on base.date_day = date(budget_revenue.date_day)
+                and base.donor_audience = budget_revenue.donor_audience
+                and base.channel = budget_revenue.platform
+            order by 2, 3, 4, 5, 6
+        ),
+        dateoffset as (
+            select
+                donor_audience,
+                date_day,
+                total_revenue_actuals,
+                total_revenue_budget_by_day,
+                date_sub(date_day, interval 1 year) as prev_year_date_day,
+                date_sub(date_day, interval 2 year) as prev_two_year_date_day
+            from original_mart
+        ),
+
+        prevyear as (
+            select
+                donor_audience,
+                date_day,
+                total_revenue_actuals as prev_year_total_revenue_actuals,
+                total_revenue_budget_by_day as prev_year_total_revenue_budget
+            from dateoffset
+        ),
+
+        prevtwoyears as (
+            select
+                donor_audience,
+                date_day,
+                total_revenue_actuals as prev_two_year_total_revenue_actuals,
+                total_revenue_budget_by_day as prev_two_year_total_revenue_budget
+            from dateoffset
         )
-, original_mart as (
+
     select
-        coalesce(
-            safe_cast(base.fiscal_year as int64),
-            safe_cast(budget_revenue.fiscal_year as int64)
-        ) as fiscal_year,
-        coalesce(
-            safe_cast(extract(year from base.date_day) as int64),
-            safe_cast(budget_revenue.date_spine_year as int64)
-        ) as year,
-        coalesce(
-            safe_cast(extract(month from base.date_day) as int64),
-            safe_cast(budget_revenue.date_spine_month as int64)
-        ) as month,
-        coalesce(
-            safe_cast(extract(day from base.date_day) as int64),
-            safe_cast(budget_revenue.date_spine_day as int64)
-        ) as day,
-        coalesce(base.date_day, budget_revenue.date_day) as date_day,
-        coalesce(base.donor_audience, budget_revenue.donor_audience) as donor_audience,
-        coalesce(base.channel, budget_revenue.platform) as channel,
-        coalesce(base.total_revenue_actuals, 0) as total_revenue_actuals,
-        coalesce(base.total_gifts_actuals, 0) as total_gifts_actuals,
-        budget_revenue.total_revenue_budget_by_day as total_revenue_budget_by_day,
-        sum(total_revenue_actuals) over (
-            partition by
-                coalesce(base.donor_audience, budget_revenue.donor_audience),
-                coalesce(base.channel, budget_revenue.platform),
-                coalesce(base.fiscal_year, budget_revenue.fiscal_year)
-            order by coalesce(base.date_day, budget_revenue.date_day)
-        ) as total_revenue_cumulative_fiscal_year
-    from base
-    full join
-        {{ ref(budget_revenue) }} as budget_revenue
-        on base.date_day = date(budget_revenue.date_day)
-        and base.donor_audience = budget_revenue.donor_audience
-        and base.channel = budget_revenue.platform
-    order by 2, 3, 4, 5, 6)
-
-, DateOffset AS (
-  SELECT
-    donor_audience,
-    date_day,
-    total_revenue_actuals,
-    total_revenue_budget_by_day,
-    DATE_SUB(date_day, INTERVAL 1 YEAR) AS prev_year_date_day,
-    DATE_SUB(date_day, INTERVAL 2 YEAR) AS prev_two_year_date_day
-  FROM
-    original_mart
-),
-
-PrevYear AS (
-  SELECT
-    donor_audience,
-    date_day,
-    total_revenue_actuals AS prev_year_total_revenue_actuals,
-    total_revenue_budget_by_day AS prev_year_total_revenue_budget
-  FROM
-    DateOffset
-),
-
-PrevTwoYears AS (
-  SELECT
-    donor_audience,
-    date_day,
-    total_revenue_actuals AS prev_two_year_total_revenue_actuals,
-    total_revenue_budget_by_day AS prev_two_year_total_revenue_budget
-  FROM
-    DateOffset
-)
-
-SELECT
-  t1.*,
-  t2.prev_year_total_revenue_actuals,
-  t2.prev_year_total_revenue_budget,
-  t3.prev_two_year_total_revenue_actuals,
-  t3.prev_two_year_total_revenue_budget
-FROM
-  DateOffset AS t1
-LEFT JOIN PrevYear AS t2
-ON
-  t1.donor_audience = t2.donor_audience
-  AND t1.prev_year_date_day = t2.date_day -- Same day last year
-LEFT JOIN PrevTwoYears AS t3
-ON
-  t1.donor_audience = t3.donor_audience
-  AND t1.prev_two_year_date_day = t3.date_day -- Same day two years back
-WHERE
-  t1.date_day >= DATE_SUB(CURRENT_DATE(), INTERVAL 1 YEAR) -- Filter by the last year's data
-  AND t1.date_day <= DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY) -- Adjust as needed
-
+        t1.*,
+        t2.prev_year_total_revenue_actuals,
+        t2.prev_year_total_revenue_budget,
+        t3.prev_two_year_total_revenue_actuals,
+        t3.prev_two_year_total_revenue_budget
+    from dateoffset as t1
+    left join
+        prevyear as t2
+        on t1.donor_audience = t2.donor_audience
+        and t1.prev_year_date_day = t2.date_day  -- Same day last year
+    left join
+        prevtwoyears as t3
+        on t1.donor_audience = t3.donor_audience
+        and t1.prev_two_year_date_day = t3.date_day  -- Same day two years back
+    where
+        t1.date_day >= date_sub(current_date(), interval 1 year)  -- Filter by the last year's data
+        and t1.date_day <= date_sub(current_date(), interval 1 day)  -- Adjust as needed
 
 {% endmacro %}
