604b60b6bd43df93659a1bc79f076a7c2d236679
Frydafly <fryda.guedes@gmail.com>
2023-09-12 11:30:06 -0500

    remove extra comma

--- macros/stitch_sfmc_audience_transaction/marts/mart_stitch_sfmc_audience_transaction_recur_revenue_by_monthly_cohort.sql
+++ macros/stitch_sfmc_audience_transaction/marts/mart_stitch_sfmc_audience_transaction_recur_revenue_by_monthly_cohort.sql
@@ -2,6 +2,6 @@
     reference_name="stg_stitch_sfmc_audience_transaction_monthly_recurring_rollup_with_activation"
 ) %}
     select
-        join_month_year, join_month_year_str, activation, total_revenue, total_donors,
+        join_month_year, join_month_year_str, activation, total_revenue, total_donors
     from {{ ref(reference_name) }}
 {% endmacro %}
