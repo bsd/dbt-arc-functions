0226818383eaf82f86bffe581a2dafb61238c9a8
Frydafly <fryda.guedes@gmail.com>
2023-09-14 12:19:21 -0500

    adding extra columns needed from union transaction

--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_with_join_date.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_with_join_date.sql
@@ -1,10 +1,14 @@
 {% macro create_stg_stitch_sfmc_audience_transaction_with_join_date(
-    reference_0_name="stg_stitch_sfmc_arc_audience_union_transaction_joined",
+    reference_0_name="stg_stitch_sfmc_arc_audience_union_transaction_joined_enriched",
     reference_1_name="stg_stitch_sfmc_audience_transaction_first_gift"
 ) %}
     select
         union_transaction.transaction_date_day as transaction_date_day,
         union_transaction.person_id as person_id,
+        union_transaction.coalesced_audience as donor_audience,
+        union_transaction.channel as channel, -- should be join source 
+        union_transaction.gift_size_string,
+        union_transaction.recurring,
         first_gift.join_month_year_date as join_month_year_date,
         union_transaction.amount as amount
     from {{ ref(reference_0_name) }} as union_transaction
