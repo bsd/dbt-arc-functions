29daf11e2fcc1e97823f899899cb8a14bf707f32
Frydafly <fryda.guedes@gmail.com>
2023-09-14 12:21:53 -0500

    adding audience and channel

--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_monthly_recurring_rollup.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_monthly_recurring_rollup.sql
@@ -4,10 +4,12 @@
     select
         date_trunc(transaction_date_day, month) as transaction_month_year_date,
         date_trunc(join_month_year_date, month) as join_month_year,
+        donor_audience,
+        channel,
         sum(amount) as total_revenue,
         safe_cast(count(distinct person_id) as integer) as total_donors
     from {{ ref(reference_name) }}
-    group by 1, 2
-    order by 1, 2
+    group by 1, 2, 3, 4
+    order by 1, 2, 3, 4
 
 {% endmacro %}
