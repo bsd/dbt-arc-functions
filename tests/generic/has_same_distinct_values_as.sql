df6e7598482a3ba2127e47e6254490fe0c55c88f
Timothy Ryan <tryan@bluestate.co>
2023-09-15 13:03:57 -0400

    ARC-1535 use on this = that

--- tests/generic/has_same_distinct_values_as.sql
+++ tests/generic/has_same_distinct_values_as.sql
@@ -20,7 +20,7 @@ other_model as (
 
 select *
 from this_model
-full outer join other_model using (test_column)
+full outer join other_model on this_model.test_column = other_model.test_column
 where this_model.test_column is null or other_model.test_column is null
 
 {% endtest %}
