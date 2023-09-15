4d13a9cf2c5636a77cfd1f1395ce486c952d3129
Timothy Ryan <tryan@bluestate.co>
2023-09-15 17:15:52 -0400

    ARC-1517 sqlfmt

--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_recur_donor_counts_combined.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_recur_donor_counts_combined.sql
@@ -1,41 +1,41 @@
 {% macro create_stg_stitch_sfmc_audience_transaction_recur_donor_counts_combined() %}
-select
-    date_day,
-    interval_type,
-    donor_audience,
-    platform,
-    total_recur_donor_counts,
-    new_recur_donor_counts,
-    retained_recur_donor_counts,
-    retained3_recur_donor_counts,
-    active_recur_donor_counts,
-    lapsed_recur_donor_counts,
-from {{ ref("stg_stitch_sfmc_audience_transaction_recur_donor_counts_daily") }}
-union all
-select
-    date_day,
-    interval_type,
-    donor_audience,
-    platform,
-    total_recur_donor_counts,
-    new_recur_donor_counts,
-    retained_recur_donor_counts,
-    retained3_recur_donor_counts,
-    active_recur_donor_counts,
-    lapsed_recur_donor_counts,
-from {{ ref("stg_stitch_sfmc_audience_transaction_recur_donor_counts_monthly") }}
-union all
-select
-    date_day,
-    interval_type,
-    donor_audience,
-    platform,
-    total_recur_donor_counts,
-    new_recur_donor_counts,
-    retained_recur_donor_counts,
-    retained3_recur_donor_counts,
-    active_recur_donor_counts,
-    lapsed_recur_donor_counts,
-from {{ ref("stg_stitch_sfmc_audience_transaction_recur_donor_counts_yearly") }}
+    select
+        date_day,
+        interval_type,
+        donor_audience,
+        platform,
+        total_recur_donor_counts,
+        new_recur_donor_counts,
+        retained_recur_donor_counts,
+        retained3_recur_donor_counts,
+        active_recur_donor_counts,
+        lapsed_recur_donor_counts,
+    from {{ ref("stg_stitch_sfmc_audience_transaction_recur_donor_counts_daily") }}
+    union all
+    select
+        date_day,
+        interval_type,
+        donor_audience,
+        platform,
+        total_recur_donor_counts,
+        new_recur_donor_counts,
+        retained_recur_donor_counts,
+        retained3_recur_donor_counts,
+        active_recur_donor_counts,
+        lapsed_recur_donor_counts,
+    from {{ ref("stg_stitch_sfmc_audience_transaction_recur_donor_counts_monthly") }}
+    union all
+    select
+        date_day,
+        interval_type,
+        donor_audience,
+        platform,
+        total_recur_donor_counts,
+        new_recur_donor_counts,
+        retained_recur_donor_counts,
+        retained3_recur_donor_counts,
+        active_recur_donor_counts,
+        lapsed_recur_donor_counts,
+    from {{ ref("stg_stitch_sfmc_audience_transaction_recur_donor_counts_yearly") }}
 
 {% endmacro %}
--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_recur_donor_counts_yearly.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_recur_donor_counts_yearly.sql
@@ -1,27 +1,28 @@
 {% macro create_stg_stitch_sfmc_audience_transaction_recur_donor_counts_yearly() %}
-select
-    last_day(date_day, year) as date_day,
-    'yearly' as interval_type,
-    donor_audience,
-    join_source as platform,
-    count(person_id) as total_recur_donor_counts,
-    count(case when donor_loyalty = 'new' then 1 end) as new_recur_donor_counts,
-    count(
-        case when donor_loyalty = 'retained' then 1 end
-    ) as retained_recur_donor_counts,
-    count(
-        case when donor_loyalty = 'retained3' then 1 end
-    ) as retained3_recur_donor_counts,
-    count(
-        case when donor_engagement = 'active' then 1 end
-    ) as active_recur_donor_counts,
-    count(
-        case when donor_engagement = 'lapsed' then 1 end
-    ) as lapsed_recur_donor_counts,
+    select
+        last_day(date_day, year) as date_day,
+        'yearly' as interval_type,
+        donor_audience,
+        join_source as platform,
+        count(person_id) as total_recur_donor_counts,
+        count(case when donor_loyalty = 'new' then 1 end) as new_recur_donor_counts,
+        count(
+            case when donor_loyalty = 'retained' then 1 end
+        ) as retained_recur_donor_counts,
+        count(
+            case when donor_loyalty = 'retained3' then 1 end
+        ) as retained3_recur_donor_counts,
+        count(
+            case when donor_engagement = 'active' then 1 end
+        ) as active_recur_donor_counts,
+        count(
+            case when donor_engagement = 'lapsed' then 1 end
+        ) as lapsed_recur_donor_counts,
 
-from {{ ref("stg_stitch_sfmc_audience_transactions_enriched_rollup_join_person_and_transaction") }}
+    from
+        {{ ref("stg_stitch_sfmc_audience_transactions_enriched_rollup_join_person_and_transaction") }}
 
-where donor_audience = 'recurring'
-group by 1, 2, 3, 4
+    where donor_audience = 'recurring'
+    group by 1, 2, 3, 4
 
 {% endmacro %}
