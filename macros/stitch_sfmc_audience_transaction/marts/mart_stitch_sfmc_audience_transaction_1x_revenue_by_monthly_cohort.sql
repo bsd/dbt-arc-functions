a72948504dfaf2865cba37ad0d2b4af8129b45c3
Frydafly <fryda.guedes@gmail.com>
2023-09-19 14:12:01 -0500

    join source vs channel

--- macros/stitch_sfmc_audience_transaction/marts/mart_stitch_sfmc_audience_transaction_1x_revenue_by_monthly_cohort.sql
+++ macros/stitch_sfmc_audience_transaction/marts/mart_stitch_sfmc_audience_transaction_1x_revenue_by_monthly_cohort.sql
@@ -6,6 +6,8 @@
             select
                 join_month_year,
                 join_month_year_str,
+                transaction_month_year_date,
+                transaction_month_year_str,
                 donor_audience,
                 channel,
                 join_gift_size_string,
@@ -15,23 +17,28 @@
                 sum(total_revenue) over (
                     partition by
                         join_month_year_str,
+                        transaction_month_year_str,
                         donor_audience,
                         channel,
                         join_gift_size_string,
-                        join_month_year
-                    order by join_month_year
+                        activation
+                    order by transaction_month_year_date
                 ) as total_revenue_cumulative_cohort,
+                case when activation = 'Act00' then total_donors end as activation_donors
             from {{ ref(reference_name) }}
         )
 
     select
-        base.*,
-        (
-            total_donors / (case when activation = 'Act00' then total_donors end)
-        ) as retention_rate,
-        (
-            total_revenue_cumulative_cohort
-            / case when activation = 'Act00' then total_donors end
-        ) as value_per_donor
+    join_month_year,
+    join_month_year_str,
+    donor_audience,
+    channel,
+    join_gift_size_string,
+    activation,
+    total_revenue,
+    total_donors,
+    total_revenue_cumulative_cohort,
+    case when activation_donors = 0 then 0 else total_donors / activation_donors end as retention_rate,
+    case when activation_donors = 0 then 0 else total_revenue_cumulative_cohort / activation_donors end as value_per_donor
     from base
 {% endmacro %}
