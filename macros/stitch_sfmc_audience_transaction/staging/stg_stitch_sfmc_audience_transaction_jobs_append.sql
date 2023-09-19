5ebddb6fddb2da48071d31ba4ab678f6e6136cac
Frydafly <fryda.guedes@gmail.com>
2023-09-19 15:43:20 -0500

    Revert "making parameterized to help us in the future"
    This reverts commit 460c788cacaaa4ea0cc7902ae1186acc67c81332.

--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_jobs_append.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_jobs_append.sql
@@ -1,21 +1,5 @@
-{% macro create_stg_stitch_sfmc_audience_transaction_parameterized_jobs_append(
+{% macro create_stg_stitch_sfmc_audience_transaction_jobs_append(
     reference_name="stg_stitch_sfmc_audience_transactions_join"
-    client_donor_audience="case
-                    when cumulative_amount_12_months >= 25000
-                    then 'major'
-                    when
-                        cumulative_amount_24_months between 1000 and 24999
-                        and cumulative_amount_12_months < 25000
-                    then 'midlevel'
-                    when
-                        cumulative_amount_30_days_recur > 0
-                        and cumulative_amount_24_months < 1000
-                        and cumulative_amount_12_months < 25000
-                    then 'recurring'
-                    when cumulative_amount_24_months between 1 and 999
-                    then 'grassroots'
-                    else null
-                end"
 ) %}
 
     with
@@ -25,7 +9,6 @@
             select distinct
                 transaction_date_day,
                 person_id,
-                ({{ client_donor_audience }}) as donor_audience,
                 case
                     when cumulative_amount_12_months >= 25000
                     then 'major'
@@ -41,7 +24,7 @@
                     when cumulative_amount_24_months between 1 and 999
                     then 'grassroots'
                     else null
-                end as blue_state_donor_audience,
+                end as donor_audience,
                 case
                     when donated_within_14_months = 0
                     then 'lapsed'
@@ -56,14 +39,12 @@
                 transaction_date_day,
                 person_id,
                 donor_audience,
-                blue_state_donor_audience,
                 donor_engagement,
                 row_number() over (
                     partition by
                         transaction_date_day,
                         person_id,
                         donor_audience,
-                        blue_state_donor_audience,
                         donor_engagement
                     order by transaction_date_day desc
                 ) as row_number
@@ -73,8 +54,7 @@
     select
         transaction_date_day,
         person_id,
-        donor_audience,
-        blue_state_donor_audience donor_engagement
+        donor_audience
     from dedupe
     where row_number = 1
 
