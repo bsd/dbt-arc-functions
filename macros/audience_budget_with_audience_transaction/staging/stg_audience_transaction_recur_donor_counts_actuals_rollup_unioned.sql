be207ef85ca263b0357587dc2606e8842b91f045
Timothy Ryan <tryan@bluestate.co>
2023-09-20 12:28:00 -0400

    ARC-1519 add documentation

--- macros/audience_budget_with_audience_transaction/staging/stg_audience_transaction_recur_donor_counts_actuals_rollup_unioned.sql
+++ macros/audience_budget_with_audience_transaction/staging/stg_audience_transaction_recur_donor_counts_actuals_rollup_unioned.sql
@@ -1,7 +1,7 @@
 {% macro create_stg_audience_transaction_recur_donor_counts_actuals_rollup_unioned() %}
-{% set relations = dbt_arc_functions.relations_that_match_regex(
+    {% set relations = dbt_arc_functions.relations_that_match_regex(
     "^stg_\w+_audience_transaction_recur_donor_counts_combined$"
 ) %}
-{{ dbt_utils.union_relations(relations) }}
+    {{ dbt_utils.union_relations(relations) }}
 
 {% endmacro %}
