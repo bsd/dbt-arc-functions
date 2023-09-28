3f5ff092420889df980212238cdea372aca44909
Tim Ryan <ryantimjohn@gmail.com>
2023-09-28 11:40:45 -0400

    Update mart_arc_revenue_recur_actuals_by_day.sql

--- macros/arc_revenue/marts/mart_arc_revenue_recur_actuals_by_day.sql
+++ macros/arc_revenue/marts/mart_arc_revenue_recur_actuals_by_day.sql
@@ -15,13 +15,13 @@ transactions.donor_loyalty,
 person.first_gift_join_source as channel,
 person.join_gift_size_string as join_amount_string,
 CASE
-    WHEN REGEXP_CONTAINS(join_amount_string,"0[-]25") THEN 1
-    WHEN REGEXP_CONTAINS(join_amount_string,"26[-]100") THEN 2
-    WHEN REGEXP_CONTAINS(join_amount_string,"101[-]250") THEN 3
-    WHEN REGEXP_CONTAINS(join_amount_string,"251[-]500") THEN 4
-    WHEN REGEXP_CONTAINS(join_amount_string,"501[-]1000") THEN 5
-    WHEN REGEXP_CONTAINS(join_amount_string,"1001[-]10000") THEN 6
-    WHEN REGEXP_CONTAINS(join_amount_string,"10000+") THEN 7
+    WHEN REGEXP_CONTAINS(join_gift_size_string,"0[-]25") THEN 1
+    WHEN REGEXP_CONTAINS(join_gift_size_string,"26[-]100") THEN 2
+    WHEN REGEXP_CONTAINS(join_gift_size_string,"101[-]250") THEN 3
+    WHEN REGEXP_CONTAINS(join_gift_size_string,"251[-]500") THEN 4
+    WHEN REGEXP_CONTAINS(join_gift_size_string,"501[-]1000") THEN 5
+    WHEN REGEXP_CONTAINS(join_gift_size_string,"1001[-]10000") THEN 6
+    WHEN REGEXP_CONTAINS(join_gift_size_string,"10000+") THEN 7
 END join_amount_string_sort,
 sum(transactions.amount) as total_revenue,
 sum(transactions.gift_count) as total_gifts
