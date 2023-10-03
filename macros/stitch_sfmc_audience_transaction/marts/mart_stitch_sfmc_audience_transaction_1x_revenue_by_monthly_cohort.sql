859a55dde6122c6a7af69fb5daef5e9af06e37dc
Frydafly <fryda.guedes@gmail.com>
2023-10-03 14:50:35 -0500

    make activation donors

--- macros/stitch_sfmc_audience_transaction/marts/mart_stitch_sfmc_audience_transaction_1x_revenue_by_monthly_cohort.sql
+++ macros/stitch_sfmc_audience_transaction/marts/mart_stitch_sfmc_audience_transaction_1x_revenue_by_monthly_cohort.sql
@@ -1,40 +1,38 @@
 {% macro create_mart_stitch_sfmc_audience_transaction_1x_revenue_by_monthly_cohort(
     reference_name="stg_stitch_sfmc_audience_transaction_monthly_1x_rollup_with_activation"
 ) %}
-    with
-        base as (
+    
+ with
+        activation_donors_base as (
             select
-                join_month_year,
                 join_month_year_str,
-                transaction_month_year_date,
-                transaction_month_year_str,
                 donor_audience,
                 channel,
                 join_gift_size_string,
-                activation,
-                total_revenue,
-                total_donors,
-                sum(total_revenue) over (
-                    partition by
-                        join_month_year_str,
-                        donor_audience,
-                        join_source,
-                        join_gift_size_string
-                    order by activation
-                ) as total_revenue_cumulative_cohort,  
-                case
-                    when activation = 'Act00'
-                    then
-                        first_value(total_donors) over (
-                            partition by
-                                join_month_year_str,
-                                donor_audience,
-                                channel,
-                                join_gift_size_string
-                            order by transaction_month_year_date
-                        )
-                end as activation_donors
+                max(case when activation = 'Act00' then total_donors end) as activation_donors
             from {{ ref(reference_name) }}
+            group by join_month_year_str, donor_audience, join_source, join_gift_size_string
+        ),
+        base as (
+            select
+                a.join_month_year,
+                a.join_month_year_str,
+                a.transaction_month_year_date,
+                a.transaction_month_year_str,
+                a.donor_audience,
+                a.channel,
+                a.join_gift_size_string,
+                a.activation,
+                a.total_revenue,
+                a.total_donors,
+                a.total_revenue_cumulative_cohort,
+                b.activation_donors as activation_donors
+            from {{ ref(reference_name) }} a
+            join activation_donors_base b
+            on a.join_month_year_str = b.join_month_year_str
+            and a.donor_audience = b.donor_audience
+            and a.channel = b.channel
+            and a.join_gift_size_string = b.join_gift_size_string
         )
 
     select
@@ -58,4 +56,7 @@
             else total_revenue_cumulative_cohort / activation_donors
         end as value_per_donor
     from base
+
+
+
 {% endmacro %}
--- macros/stitch_sfmc_audience_transaction/marts/mart_stitch_sfmc_audience_transaction_recur_revenue_by_monthly_cohort.sql
+++ macros/stitch_sfmc_audience_transaction/marts/mart_stitch_sfmc_audience_transaction_recur_revenue_by_monthly_cohort.sql
@@ -1,40 +1,41 @@
 {% macro create_mart_stitch_sfmc_audience_transaction_recur_revenue_by_monthly_cohort(
     reference_name="stg_stitch_sfmc_audience_transaction_monthly_recurring_rollup_with_activation"
 ) %}
+    
+    {% macro create_mart_stitch_sfmc_audience_transaction_recur_revenue_by_monthly_cohort(
+    reference_name="stg_stitch_sfmc_audience_transaction_monthly_recurring_rollup_with_activation"
+) %}
     with
-        base as (
+        activation_donors_base as (
             select
-                join_month_year,
                 join_month_year_str,
-                transaction_month_year_date,
-                transaction_month_year_str,
                 donor_audience,
                 join_source,
                 join_gift_size_string,
-                activation,
-                total_revenue,
-                total_donors,
-                sum(total_revenue) over (
-                    partition by
-                        join_month_year_str,
-                        donor_audience,
-                        join_source,
-                        join_gift_size_string
-                    order by activation
-                ) as total_revenue_cumulative_cohort,  -- this isn't correct
-                case
-                    when activation = 'Act00'
-                    then
-                        first_value(total_donors) over (
-                            partition by
-                                join_month_year_str,
-                                donor_audience,
-                                channel,
-                                join_gift_size_string
-                            order by transaction_month_year_date
-                        )
-                end as activation_donors
+                max(case when activation = 'Act00' then total_donors end) as activation_donors
             from {{ ref(reference_name) }}
+            group by join_month_year_str, donor_audience, join_source, join_gift_size_string
+        ),
+        base as (
+            select
+                a.join_month_year,
+                a.join_month_year_str,
+                a.transaction_month_year_date,
+                a.transaction_month_year_str,
+                a.donor_audience,
+                a.join_source,
+                a.join_gift_size_string,
+                a.activation,
+                a.total_revenue,
+                a.total_donors,
+                a.total_revenue_cumulative_cohort,
+                b.activation_donors as activation_donors
+            from {{ ref(reference_name) }} a
+            join activation_donors_base b
+            on a.join_month_year_str = b.join_month_year_str
+            and a.donor_audience = b.donor_audience
+            and a.join_source = b.join_source
+            and a.join_gift_size_string = b.join_gift_size_string
         )
 
     select
@@ -60,3 +61,5 @@
     from base
 
 {% endmacro %}
+
+
