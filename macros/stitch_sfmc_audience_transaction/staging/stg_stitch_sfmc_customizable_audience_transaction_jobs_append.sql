931df4bb9b39f214441ace637dc6be52dbbd7a96
Frydafly <fryda.guedes@gmail.com>
2023-10-06 11:29:03 -0500

    remove unused customizable model

--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_customizable_audience_transaction_jobs_append.sql
+++
@@ -1,9 +0,0 @@
-{% macro create_stg_stitch_sfmc_customizable_audience_transaction_jobs_append() %}
-    select
-        transaction_date_day,
-        person_id,
-        donor_audience,
-        donor_engagement,
-        donor_loyalty,
-    from {{ ref("stg_stitch_sfmc_audience_transaction_jobs_append") }}
-{% endmacro %}
