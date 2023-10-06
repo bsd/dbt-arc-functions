70479f5836aba32869d3141bfd588e0ec273c637
Frydafly <fryda.guedes@gmail.com>
2023-10-06 11:44:43 -0500

    only values used here are donated within 13 and 14  months

--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_yoy.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_yoy.sql
@@ -2,80 +2,10 @@
     reference_name="stg_stitch_sfmc_audience_transactions_summary_unioned"
 ) %}
 
-    with
-        fiscal_years as (
-            select
-                date_trunc(
-                    date_add(transaction_date_day, interval 6 month), year
-                ) as fiscal_year_start,
-                date_trunc(
-                    date_add(transaction_date_day, interval 18 month), year
-                ) as fiscal_year_end,
-                *
-            from {{ ref(reference_name) }}
-        )
 
     select
         transaction_date_day,
         person_id,
-        1 as donated_current_fiscal_year_july_to_june,
-        max(
-            case
-                when
-                    transaction_date_day >= date_trunc(
-                        date_add(fiscal_years.fiscal_year_start, interval - 1 year),
-                        year
-                    )
-                    and transaction_date_day < fiscal_years.fiscal_year_start
-                then 1
-                else 0
-            end
-        ) as donated_last_fiscal_year_july_to_june,
-        max(
-            case
-                when
-                    transaction_date_day >= date_trunc(
-                        date_add(fiscal_years.fiscal_year_start, interval - 2 year),
-                        year
-                    )
-                    and transaction_date_day < date_trunc(
-                        date_add(fiscal_years.fiscal_year_start, interval - 1 year),
-                        year
-                    )
-                then 1
-                else 0
-            end
-        ) as donated_two_fiscal_years_ago_july_to_june,
-        max(
-            case
-                when
-                    transaction_date_day >= date_trunc(
-                        date_add(fiscal_years.fiscal_year_start, interval - 3 year),
-                        year
-                    )
-                    and transaction_date_day < date_trunc(
-                        date_add(fiscal_years.fiscal_year_start, interval - 2 year),
-                        year
-                    )
-                then 1
-                else 0
-            end
-        ) as donated_three_fiscal_years_ago_july_to_june,
-        max(
-            case
-                when
-                    transaction_date_day >= date_trunc(
-                        date_add(fiscal_years.fiscal_year_start, interval - 4 year),
-                        year
-                    )
-                    and transaction_date_day < date_trunc(
-                        date_add(fiscal_years.fiscal_year_start, interval - 3 year),
-                        year
-                    )
-                then 1
-                else 0
-            end
-        ) as donated_four_fiscal_years_ago_july_to_june,
         max(
             case
                 when
@@ -96,7 +26,7 @@
                 else 0
             end
         ) as donated_within_13_months
-    from fiscal_years
+    from {{ ref(reference_name)}}
     group by 1, 2
 
 {% endmacro %}
