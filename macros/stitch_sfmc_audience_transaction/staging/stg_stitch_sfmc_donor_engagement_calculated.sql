b1c6dc6641a92708cc9ba0ce03f40166f0496879
Frydafly <fryda.guedes@gmail.com>
2023-10-12 11:50:30 -0500

    comma

--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_donor_engagement_calculated.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_donor_engagement_calculated.sql
@@ -2,7 +2,7 @@
     transaction_jobs="stg_stitch_sfmc_audience_transaction_jobs_append"
 ) %}
 
-    select person_id, transaction_date_day, donor_engagement,
+    select person_id, transaction_date_day, donor_engagement
     from {{ ref(transaction_jobs) }}
 
 {% endmacro %}
