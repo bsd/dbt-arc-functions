79eece23369e305825462651bfacb6987fe8b89d
Frydafly <fryda.guedes@gmail.com>
2023-10-12 17:56:57 -0500

    fix for bounces

--- macros/frakture_everyaction_deliverability/staging/stg_frakture_everyaction_deliverability_bounces_daily_rollup.sql
+++ macros/frakture_everyaction_deliverability/staging/stg_frakture_everyaction_deliverability_bounces_daily_rollup.sql
@@ -1,16 +1,18 @@
 {% macro create_stg_frakture_everyaction_deliverability_bounces_daily_rollup(
     reference_name="stg_frakture_everyaction_deliverability_message_stat_unioned_with_domain"
 ) %}
-WITH unique_bounces AS (
+
+
+with unique_bounces AS (
     SELECT
-        safe_cast(sent_ts as date) as sent_date,
-        safe_cast(message_id as string) as message_id,
-        safe_cast(email_domain as string) as email_domain,
+        safe_cast(event_dt as date) as sent_date,
+        safe_cast(job_id as string) as message_id,
+        safe_cast(domain as string) as email_domain,
         subscriber_key,
-        ROW_NUMBER() OVER (PARTITION BY message_id, subscriber_key ORDER BY bounce_ts) AS bounce_row_number,
-        safe_cast(hard_bounce as int) as hard_bounce,
-        safe_cast(soft_bounce as int) as soft_bounce
-    FROM {{ ref(reference_name) }}
+        ROW_NUMBER() OVER (PARTITION BY job_id, subscriber_key ORDER BY event_dt) AS bounce_row_number,
+        safe_cast((case when bounce_category_id = '1' then 1 else 0 end) as int) as hard_bounce,
+        safe_cast((case when bounce_category_id = '2' then 1 else 0 end) as int) as soft_bounce
+    FROM {{ref(reference_name)}}
 )
 
 SELECT
@@ -22,4 +24,5 @@ SELECT
     SUM(CASE WHEN bounce_row_number = 1 THEN hard_bounce + soft_bounce ELSE 0 END) AS total_bounces
 FROM unique_bounces
 GROUP BY 1, 2, 3
+
 {% endmacro %}
