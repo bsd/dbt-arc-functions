dc64a3bb1fbdb6ac7c1f0faef09ce438a9f610e4
Frydafly <fryda.guedes@gmail.com>
2023-10-12 17:46:52 -0500

    changes to everyaction

--- macros/frakture_everyaction_deliverability/staging/stg_frakture_everyaction_deliverability_opens_daily_rollup.sql
+++ macros/frakture_everyaction_deliverability/staging/stg_frakture_everyaction_deliverability_opens_daily_rollup.sql
@@ -1,11 +1,22 @@
 {% macro create_stg_frakture_everyaction_deliverability_opens_daily_rollup(
     reference_name="stg_frakture_everyaction_deliverability_message_stat_unioned_with_domain"
 ) %}
-    select
+WITH unique_opens AS (
+    SELECT
         safe_cast(sent_ts as date) as sent_date,
         safe_cast(message_id as string) as message_id,
         safe_cast(email_domain as string) as email_domain,
-        sum(safe_cast(opened as int)) as opens
-    from {{ ref(reference_name) }}
-    group by 1, 2, 3
+        subscriber_key,
+        ROW_NUMBER() OVER (PARTITION BY message_id, subscriber_key ORDER BY open_ts) AS open_row_number,
+        safe_cast(opened as int) as opened
+    FROM {{ ref(reference_name) }}
+)
+
+SELECT
+    sent_date,
+    message_id,
+    email_domain,
+    SUM(CASE WHEN open_row_number = 1 THEN opened ELSE 0 END) AS opens
+FROM unique_opens
+GROUP BY 1, 2, 3
 {% endmacro %}
--- macros/frakture_everyaction_deliverability/staging/stg_frakture_everyaction_deliverability_recipients_daily_rollup.sql
+++ macros/frakture_everyaction_deliverability/staging/stg_frakture_everyaction_deliverability_recipients_daily_rollup.sql
@@ -1,11 +1,22 @@
 {% macro create_stg_frakture_everyaction_deliverability_recipients_daily_rollup(
     reference_name="stg_frakture_everyaction_deliverability_message_stat_unioned_with_domain"
 ) %}
-    select
+WITH unique_recipients AS (
+    SELECT
         safe_cast(sent_ts as date) as sent_date,
         safe_cast(message_id as string) as message_id,
         safe_cast(email_domain as string) as email_domain,
-        sum(safe_cast(received as int)) as recipients
-    from {{ ref(reference_name) }}
-    group by 1, 2, 3
+        subscriber_key,
+        ROW_NUMBER() OVER (PARTITION BY message_id, subscriber_key ORDER BY received_ts) AS recipient_row_number,
+        safe_cast(received as int) as received
+    FROM {{ ref(reference_name) }}
+)
+
+SELECT
+    sent_date,
+    message_id,
+    email_domain,
+    SUM(CASE WHEN recipient_row_number = 1 THEN received ELSE 0 END) AS recipients
+FROM unique_recipients
+GROUP BY 1, 2, 3
 {% endmacro %}
