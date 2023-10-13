078008e55fa472392fac8db7c0eaa6dcfbd38d3a
Timothy Ryan <tryan@bluestate.co>
2023-10-13 10:56:35 -0400

    ARC-1628 make gift_size_str

--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_onetime_donor_counts_actuals_rollup.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_onetime_donor_counts_actuals_rollup.sql
@@ -14,25 +14,25 @@
                     )
                 }} as fiscal_year,
                 count(
-                    distinct case when gift_size_string is not null then person_id end
+                    distinct case when gift_size_str is not null then person_id end
                 ) as total_onetime_donor_counts,
                 count(
                     distinct case
                         when
-                            donor_loyalty = 'new_donor' and gift_size_string is not null
+                            donor_loyalty = 'new_donor' and gift_size_str is not null
                         then person_id
                     end
                 ) as new_onetime_donor_counts,
                 count(
                     distinct case
-                        when donor_loyalty = 'retained' and gift_size_string is not null
+                        when donor_loyalty = 'retained' and gift_size_str is not null
                         then person_id
                     end
                 ) as retained_onetime_donor_counts,
                 count(
                     distinct case
                         when
-                            donor_loyalty = 'retained3' and gift_size_string is not null
+                            donor_loyalty = 'retained3' and gift_size_str is not null
                         then person_id
                     end
                 ) as retained3_onetime_donor_counts,
@@ -40,7 +40,7 @@
                     distinct case
                         when
                             donor_engagement = 'reinstated'
-                            and gift_size_string is not null
+                            and gift_size_str is not null
                         then person_id
                     end
                 ) as reinstated_onetime_donor_counts,
