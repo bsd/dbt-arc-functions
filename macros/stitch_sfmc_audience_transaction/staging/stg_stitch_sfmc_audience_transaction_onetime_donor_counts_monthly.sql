5e91165d92ff876286ef8de87d093169b1c917f5
Timothy Ryan <tryan@bluestate.co>
2023-10-24 16:21:53 -0400

    ARC-1668 update macro names

--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_onetime_donor_counts_monthly.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_onetime_donor_counts_monthly.sql
@@ -1,4 +1,4 @@
-{% macro create_stg_stitch_sfmc_audience_transaction_onetime_donor_counts_actuals_rollup(
+{% macro create_stg_stitch_sfmc_audience_transaction_onetime_donor_counts_monthly(
     reference_name="stg_stitch_sfmc_audience_transactions_enriched_rollup_join_person_and_transaction"
 ) %}
     with
