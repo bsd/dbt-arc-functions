eb262594108af16605517b3b4b326c9c480dd80d
Timothy Ryan <tryan@bluestate.co>
2023-10-27 17:16:44 -0400

    ARC-1630 fix AND

--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_onetime_donor_counts_daily.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_onetime_donor_counts_daily.sql
@@ -53,37 +53,48 @@
                     distinct case when donor_engagement = 'lapsed' then person_id end
                 ) as lapsed_onetime_donor_counts,
                 count(
-                    distinct case when gift_size_str is not null then person_id and nth_transaction_this_fiscal_year = 1 end
+                    distinct case
+                        when
+                            gift_size_str is not null
+                            and nth_transaction_this_fiscal_year = 1
+                        then person_id
+                    end
                 ) as unique_total_onetime_donor_counts,
                 count(
                     distinct case
-                        when donor_loyalty = 'new_donor' and gift_size_str is not null
+                        when
+                            donor_loyalty = 'new_donor'
+                            and gift_size_str is not null
+                            and nth_transaction_this_fiscal_year = 1
                         then person_id
-                    and nth_transaction_this_fiscal_year = 1 end
+                    end
                 ) as unique_new_onetime_donor_counts,
                 count(
                     distinct case
                         when
                             donor_loyalty = 'retained_donor'
                             and gift_size_str is not null
+                            and nth_transaction_this_fiscal_year = 1
                         then person_id
-                    and nth_transaction_this_fiscal_year = 1 end
+                    end
                 ) as unique_retained_onetime_donor_counts,
                 count(
                     distinct case
                         when
                             donor_loyalty = 'retained_3+_donor'
                             and gift_size_str is not null
+                            and nth_transaction_this_fiscal_year = 1
                         then person_id
-                    and nth_transaction_this_fiscal_year = 1 end
+                    end
                 ) as unique_retained3_onetime_donor_counts,
                 count(
                     distinct case
                         when
                             donor_loyalty = 'reactivated_donor'
                             and gift_size_str is not null
+                            and nth_transaction_this_fiscal_year = 1
                         then person_id
-                    and nth_transaction_this_fiscal_year = 1 end
+                    end
                 ) as unique_reinstated_onetime_donor_counts,
 
             from {{ ref(reference_name) }}
