367e3c9330f9e9785e6d8b56c7d9d474ccc8d2be
Timothy Ryan <tryan@bluestate.co>
2023-10-27 15:58:03 -0400

    ARC-1630 use existing date spine

--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_person_with_donor_engagement.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_person_with_donor_engagement.sql
@@ -1,25 +1,12 @@
 {% macro create_stg_stitch_sfmc_audience_transaction_person_with_donor_engagement(
     stg_stitch_sfmc_audience_transaction_person_with_all_txns="stg_stitch_sfmc_audience_transaction_person_with_all_txns"
 ) %}
-
-    with
-         date_spine AS (
-  SELECT
-    *
-  FROM
-    UNNEST( GENERATE_DATE_ARRAY( (
-        SELECT
-          MIN(DATE(transaction_date))
-        FROM
-          {{ref('stg_stitch_sfmc_audience_transactions_summary_unioned')}} ), (
-        SELECT
-          Current_date ) ) ) AS date_day )
 SELECT
   donor_engagement_start_and_end_dates.person_id,
   date_spine.date_day AS date_day,
   donor_engagement_start_and_end_dates.donor_engagement
 FROM
-  date_spine
+  {{ref('stg_stitch_sfmc_audience_transaction_calculated_date_spine')}} as date_spine
 INNER JOIN
   {{ref('stg_stitch_sfmc_audience_transaction_person_with_all_txns')}} as donor_engagement_start_and_end_dates
 ON
