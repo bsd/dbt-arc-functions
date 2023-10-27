ef5ff93c7a093d0ae199cf43f195b67dfcd3ba9f
Timothy Ryan <tryan@bluestate.co>
2023-10-27 16:02:23 -0400

    ARC-1630 use materialized date spine

--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_person_with_donor_engagement.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_person_with_donor_engagement.sql
@@ -3,15 +3,15 @@
 ) %}
 SELECT
   donor_engagement_start_and_end_dates.person_id,
-  date_spine.date_day AS date_day,
+  date_spine.date AS date_day,
   donor_engagement_start_and_end_dates.donor_engagement
 FROM
   {{ref('stg_stitch_sfmc_audience_transaction_calculated_date_spine')}} as date_spine
 INNER JOIN
   {{ref('stg_stitch_sfmc_audience_transaction_person_with_all_txns')}} as donor_engagement_start_and_end_dates
 ON
-  date_spine.date_day >= donor_engagement_start_and_end_dates.start_date
-  AND date_spine.date_day <= donor_engagement_start_and_end_dates.end_date
+  date_spine.date >= donor_engagement_start_and_end_dates.start_date
+  AND date_spine.date <= donor_engagement_start_and_end_dates.end_date
   OR donor_engagement_start_and_end_dates.end_date IS NULL
 ORDER BY 1, 2
 {% endmacro %}
