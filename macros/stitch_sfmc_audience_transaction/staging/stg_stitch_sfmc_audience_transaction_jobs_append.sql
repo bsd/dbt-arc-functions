c019834d49dab31d9b4fb575898f3ca961b63919
Frydafly <fryda.guedes@gmail.com>
2023-10-31 11:57:40 -0500

    edit donor audience definitions to fit new discussions

--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_jobs_append.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_jobs_append.sql
@@ -2,18 +2,21 @@
     reference_name="stg_stitch_sfmc_audience_transactions_summary_unioned"
 ) %}
 
+/*
+UUSA Client Audience Requirements: 
+Major: 25,000 cumulative or more in the last 24 months including 1x and recurring gifts.
+Midlevel: 1,000 to 24,999 cumulative in the last 24 months including 1x and recurring
+Sustainer: Has a recurring gift in the last 90 days and is not in either midlevel or major
+Mass: Is not in Sustainer, Midlevel or Major. 
+
+*/
+
     with
         calculations as (
             select
                 transaction_date_day,
                 person_id,
                 sum(amount) as daily_revenue,
-                -- Calculate cumulative amount for the past 12 months
-                sum(amount) over (
-                    partition by person_id
-                    order by unix_seconds(timestamp(transaction_date_day))  -- Convert date to Unix timestamp
-                    range between 31556952 preceding and current row  -- 31,556,952 seconds in 12 months
-                ) as cumulative_amount_12_months,
                 -- Calculate cumulative amount for the past 24 months
                 sum(amount) over (
                     partition by person_id
@@ -36,10 +39,10 @@
                         sum(amount) over (
                             partition by person_id
                             order by unix_seconds(timestamp(transaction_date_day))  -- Convert date to Unix timestamp
-                            range between 2592000 preceding and current row  -- 2,592,000 seconds in 30 days
+                            range between 7776000 preceding and current row  -- unix seconds in 90 days
                         )
                     else 0
-                end as cumulative_amount_30_days_recur,
+                end as cumulative_amount_90_days_recur,
                 -- add at least 1 recurring donation... EVER? (new definition of
                 -- sustainer)
                 sum(amount) over (
@@ -55,12 +58,11 @@
                 transaction_date_day,
                 person_id,
                 sum(daily_revenue) as daily_revenue,
-                sum(cumulative_amount_12_months) as cumulative_amount_12_months,
                 sum(cumulative_amount_24_months) as cumulative_amount_24_months,
                 sum(
                     cumulative_amount_24_months_non_recur
                 ) as cumulative_amount_24_months_non_recur,
-                sum(cumulative_amount_30_days_recur) as cumulative_amount_30_days_recur,
+                sum(cumulative_amount_90_days_recur) as cumulative_amount_30_days_recur,
                 sum(cumulative_amount_14_months) as cumulative_amount_14_months
             from calculations
             group by 1, 2
@@ -72,42 +74,26 @@
                 transaction_date_day,
                 person_id,
                 case
-                    when
-                        cumulative_amount_30_days_recur > 0  -- confirmed by laura to leave?
-                        and daily_revenue < 80
-                    then 'Monthly'
-                    -- sustainers = at least 1 recur gift in BBCRM
-                    when cumulative_amount_12_months >= 25000  -- check!
+                    when cumulative_amount_24_months >= 25000 
                     then 'Major'
                     when
-                        cumulative_amount_24_months between 1000 and 24999  -- check! 
-                        and daily_revenue >= 80
-                    -- what if someone gave more than 24999 in 24 months but not in 12
-                    -- months? won't fit into major OR recurring Or mass!!!
+                        cumulative_amount_24_months between 1000 and 24999 
                     then 'Leadership Giving'
-                    -- midlevel = cumulative $1,000 - $24,999 over 24 months
-                    -- (including all gifts)
-                    when cumulative_amount_24_months_non_recur < 1000
-                    then 'Mass'
-                    -- mass = non-recurring; $1-$999; not monthly, midlevel, major, or
-                    -- unite
-                    else null
-                end as bluestate_donor_audience,
+                    when
+                    cumulative_amount_90_days_recur > 0
+                    then 'Monthly'
+                    else 'Mass'
+                end as bluestate_donor_audience, -- modeled after UUSA
                 case
-                    when cumulative_amount_12_months >= 25000
+                    when cumulative_amount_24_months >= 25000 
                     then 'Major'
                     when
-                        cumulative_amount_24_months between 1000 and 24999
-                        and cumulative_amount_12_months < 25000
+                        cumulative_amount_24_months between 1000 and 24999 
                     then 'Leadership Giving'
                     when
-                        cumulative_amount_30_days_recur > 0
-                        and cumulative_amount_24_months < 1000
-                        and cumulative_amount_12_months < 25000
+                    cumulative_amount_90_days_recur > 0
                     then 'Monthly'
-                    when cumulative_amount_24_months between 1 and 999
-                    then 'Mass'
-                    else null
+                    else 'Mass'
                 end as donor_audience,
                 case
                     when cumulative_amount_14_months < 1
