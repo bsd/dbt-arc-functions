c650a4869472951bef0ff0d475be2b0e4c66cd4f
Timothy Ryan <tryan@bluestate.co>
2023-10-31 14:45:24 -0400

    ARC-1630 update function call

--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_onetime_donor_counts_daily.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_onetime_donor_counts_daily.sql
@@ -1,5 +1,5 @@
 {% macro create_stg_stitch_sfmc_audience_transaction_onetime_donor_counts_daily(
     reference_name="stg_stitch_sfmc_audience_transactions_enriched_rollup_join_person_and_transaction"
 ) %}
-    {{ util_stg_stitch_sfmc_audience_transaction_frequency_donor_counts_interval('onetime','day', reference_name) }}
+    {{ dbt_arc_functions.util_stg_stitch_sfmc_audience_transaction_frequency_donor_counts_interval('onetime','day', reference_name) }}
 {% endmacro %}
