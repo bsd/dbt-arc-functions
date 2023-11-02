de89c43ad48153f9fae72fecbae467e5a0be45b7
Frydafly <fryda.guedes@gmail.com>
2023-11-02 17:57:14 -0500

    removed contignency on date spine model

--- macros/stitch_sfmc_arc/staging/stg_stitch_sfmc_donor_engagement_by_date_day.sql
+++ macros/stitch_sfmc_arc/staging/stg_stitch_sfmc_donor_engagement_by_date_day.sql
@@ -1,15 +1,26 @@
 {% macro create_stg_stitch_sfmc_donor_engagement_by_date_day(
-    donor_engagement_scd_table="stg_stitch_sfmc_donor_engagement_scd",
-    donor_engagement_date_spine="stg_stitch_sfmc_donor_engagement_date_spine"
+    donor_engagement_scd_table="stg_stitch_sfmc_donor_engagement_scd"
 ) %}
 
-    with
-        engagement_by_date_day as (
+with date_spine as (
+select date
+from
+    unnest(
+        generate_date_array(
+            (select min(start_date), from {{ ref(donor_engagement) }}),
+            ifnull(
+                (select max(start_date) from {{ ref(donor_engagement) }}), current_date()
+            )
+        )
+    ) as date_day
+
+)
+        , engagement_by_date_day as (
             select
-                date_spine.date as date_day,
+                date_spine.date_day,
                 donor_engagement_scd.person_id,
                 donor_engagement_scd.donor_engagement
-            from {{ ref(donor_engagement_date_spine) }} as date_spine
+            from date_spine
             inner join
                 {{ ref(donor_engagement_scd_table) }} as donor_engagement_scd
                 on date_spine.date >= date(donor_engagement_scd.start_date)
