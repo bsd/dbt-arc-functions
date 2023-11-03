db08c1ee3a27255d1411414c57d6d0f52e09bf1f
Frydafly <fryda.guedes@gmail.com>
2023-11-03 12:57:29 -0500

    rewriting and documenting of transaction 1x revenue by monthly cohort

--- macros/stitch_sfmc_audience_transaction/marts/mart_stitch_sfmc_audience_transaction_1x_revenue_by_monthly_cohort.sql
+++ macros/stitch_sfmc_audience_transaction/marts/mart_stitch_sfmc_audience_transaction_1x_revenue_by_monthly_cohort.sql
@@ -2,6 +2,30 @@
     reference_name="stg_stitch_sfmc_audience_transaction_monthly_1x_rollup_with_activation"
 ) %}
 
+/*
+
+This query calculates the first_activation_donor_count for each monthly cohort,
+which is the number of donors that the cohort had in their first activation month.
+
+ To do this, the query first joins two tables:
+
+ * The `monthly_1x_rollup` table, which contains aggregated data for each month,
+ donor audience, channel, and join gift size.
+ * The `activation_donors_base` table, which contains the number of donors
+ that each cohort had in their first activation month.
+
+ The query then calculates the cumulative revenue for each cohort, which is the
+ total revenue that the cohort has generated since their first activation month.
+
+ Finally, the query calculates the first_activation_donor_count for each cohort by
+ coalescing the number of donors from the `activation_donors_base` table with
+ the maximum number of donors from the `monthly_1x_rollup` table for the cohort's
+ first activation month. This ensures that the first_activation_donor_count is always
+ populated, even if there is no record in the `activation_donors_base` table for
+ the cohort's first activation month.
+
+*/
+
 with
     activation_donors_base as (
         select
@@ -12,37 +36,46 @@ with
             max(
                 case when activation = 'Act00' then total_donors end
             ) as activation_donors
-        from {{ ref(reference_name) }}
+        from {{ ref(reference_name) }} monthly_1x_rollup
         group by join_month_year_str, donor_audience, channel, join_gift_size_string
     ),
     base as (
         select
-            a.join_month_year,
-            a.join_month_year_str,
-            a.transaction_month_year_date,
-            a.transaction_month_year_str,
-            a.donor_audience,
-            a.channel,
-            a.join_gift_size_string,
-            a.activation,
-            a.total_revenue,
-            a.total_donors,
-            sum(a.total_revenue) over (
+            monthly_1x_rollup.join_month_year,
+            monthly_1x_rollup.join_month_year_str,
+            monthly_1x_rollup.transaction_month_year_date,
+            monthly_1x_rollup.transaction_month_year_str,
+            monthly_1x_rollup.donor_audience,
+            monthly_1x_rollup.channel,
+            monthly_1x_rollup.join_gift_size_string,
+            monthly_1x_rollup.activation,
+            monthly_1x_rollup.total_revenue,
+            monthly_1x_rollup.total_donors,
+            sum(monthly_1x_rollup.total_revenue) over (
                 partition by
-                    a.join_month_year_str,
-                    a.donor_audience,
-                    a.channel,
-                    a.join_gift_size_string
-                order by a.activation
+                    monthly_1x_rollup.join_month_year_str,
+                    monthly_1x_rollup.donor_audience,
+                    monthly_1x_rollup.channel,
+                    monthly_1x_rollup.join_gift_size_string
+                order by monthly_1x_rollup.activation
             ) as total_revenue_cumulative_cohort,
-            b.activation_donors as activation_donors
-        from {{ ref(reference_name) }} a
-        join
-            activation_donors_base b
-            on a.join_month_year_str = b.join_month_year_str
-            and a.donor_audience = b.donor_audience
-            and a.channel = b.channel
-            and a.join_gift_size_string = b.join_gift_size_string
+             coalesce(
+        activation_donors_base.activation_donors,
+        (
+          select
+            max(
+              case when activation = 'Act00' then total_donors end
+            ) as activation_donors
+          from monthly_1x_rollup
+        )
+      ) as activation_donors
+        from {{ ref(reference_name) }} monthly_1x_rollup
+        left join
+            activation_donors_base
+            on monthly_1x_rollup.join_month_year_str = activation_donors_base.join_month_year_str
+            and monthly_1x_rollup.donor_audience = activation_donors_base.donor_audience
+            and monthly_1x_rollup.channel = activation_donors_base.channel
+            and monthly_1x_rollup.join_gift_size_string = activation_donors_base.join_gift_size_string
     )
 
 select
