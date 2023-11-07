d17897b27d1f141e9687d8b3831d5ad54c7d4d65
Timothy Ryan <tryan@bluestate.co>
2023-11-07 17:03:34 -0500

    ARC-1659 cast null values to correct type

--- macros/stitch_facebook_paidmedia/staging/stg_stitch_facebook_paidmedia_campaigns.sql
+++ macros/stitch_facebook_paidmedia/staging/stg_stitch_facebook_paidmedia_campaigns.sql
@@ -7,10 +7,10 @@
         ad_id as message_id,
         'social' as channel_category,
         'facebook' as channel,
-        null as channel_type,
+        cast(null as string) as channel_type,
         campaign_name as campaign_name,
-        null as crm_entity,
-        null as source_code_entity,
-        null as preview_url
+        cast(null as string) as crm_entity,
+        cast(null as string) as source_code_entity,
+        cast(null as string) as preview_url
     from {{ source(source_name, source_table) }}
 {% endmacro %}
