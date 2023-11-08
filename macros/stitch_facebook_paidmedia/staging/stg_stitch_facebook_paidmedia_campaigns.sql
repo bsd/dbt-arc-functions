4911d6ec7674b732e87d320b973632116fed309b
Tim Ryan <ryantimjohn@gmail.com>
2023-11-08 11:15:12 -0500

    ARC-1659 update facebook to facebook_ad

--- macros/stitch_facebook_paidmedia/staging/stg_stitch_facebook_paidmedia_campaigns.sql
+++ macros/stitch_facebook_paidmedia/staging/stg_stitch_facebook_paidmedia_campaigns.sql
@@ -6,7 +6,7 @@
         campaign_id as campaign_id,
         ad_id as message_id,
         'social' as channel_category,
-        'facebook' as channel,
+        'facebook_ad' as channel,
         cast(null as string) as channel_type,
         campaign_name as campaign_name,
         cast(null as string) as crm_entity,
