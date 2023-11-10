8f63ff4a78faaa2eb419c30ea54dc2936a4c954b
Frydafly <fryda.guedes@gmail.com>
2023-11-10 17:26:53 -0600

    back to

--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_first_gift.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_first_gift.sql
@@ -21,52 +21,56 @@ with first_transactions as (
     from {{ ref(transactions) }}
     group by person_id
 )
+
+
     select
         first_transactions.person_id,
         first_transactions.first_transaction_date,
         cast(timestamp_trunc(first_transactions.first_transaction_date, day) as date) as join_month_year_date,
         format_timestamp('%b %Y', timestamp_trunc(first_transactions.first_transaction_date, month)) as join_month_year_str,
         transactions.inbound_channel as first_gift_join_source,
-        case first_tranacions.first_gift_amount_int
-            when between 0 and 25
+        case 
+            when first_transactions.first_gift_amount_int between 0 and 25
             then '0-25'
-            when between 26 and 100
+            when first_transactions.first_gift_amount_int between 26 and 100
             then '26-100'
-            when between 101 and 250
+            when first_transactions.first_gift_amount_int between 101 and 250
             then '101-250'
-            when between 251 and 500
+            when first_transactions.first_gift_amount_int between 251 and 500
             then '251-500'
-            when between 501 and 1000
+            when first_transactions.first_gift_amount_int between 501 and 1000
             then '501-1000'
-            when between 1001 and 10000
+            when first_transactions.first_gift_amount_int between 1001 and 10000
             then '1001-10000'
             else '10000+'
         end
-     as join_gift_size_string,
-        case first_transactions.first_gift_amount_int
-            when between 0 and 10
+    ) as join_gift_size_string,
+    (
+        case
+            when first_transactions.first_gift_amount_int between 0 and 10
             then '0-10'
-            when between 11 and 20
+            when first_transactions.first_gift_amount_int between 11 and 20
             then '11-20'
-            when between 21 and 30
+            when first_transactions.first_gift_amount_int between 21 and 30
             then '21-30'
-            when between 31 and 40
+            when first_transactions.first_gift_amount_int between 31 and 40
             then '31-40'
-            when between 41 and 50
+            when first_transactions.first_gift_amount_int between 41 and 50
             then '41-50'
-            when between 51 and 60
+            when first_transactions.first_gift_amount_int between 51 and 60
             then '51-60'
-            when  between 61 and 70
+            when first_transactions.first_gift_amount_int between 61 and 70
             then '61-70'
-            when between 71 and 80
+            when first_transactions.first_gift_amount_int between 71 and 80
             then '71-80'
-            when between 81 and 90
+            when first_transactions.first_gift_amount_int between 81 and 90
             then '81-90'
-            when between 91 and 100
+            when first_transactions.first_gift_amount_int between 91 and 100
             then '91-100'
-            when > 100
+            when first_gift_amount_int > 100
             then '100+'
-        end as join_gift_size_string_recur,
+        end
+    ) as join_gift_size_string_recur,
         transactions.recurring as first_gift_recur_status,
         audience.coalesced_audience as first_gift_donor_audience,
         safe_cast(transactions.amount as int64) as first_gift_amount_int
