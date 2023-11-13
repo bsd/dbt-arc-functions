1f9f6336129a50c3cc2435698d8ece191673e1a5
Frydafly <fryda.guedes@gmail.com>
2023-11-13 12:18:59 -0600

    partition by month_diff_str

--- macros/stitch_sfmc_audience_transaction/marts/mart_audience_1x_activation.sql
+++ macros/stitch_sfmc_audience_transaction/marts/mart_audience_1x_activation.sql
@@ -12,7 +12,7 @@ first_gift_donor_audience,
 concat('Act' || month_diff_str ) as activation_str,
 sum(amounts) as total_amount,
 SUM(amounts) OVER (
-    PARTITION BY join_month_year_str, first_gift_join_source, join_gift_size_string, first_gift_donor_audience, activation_str
+    PARTITION BY join_month_year_str, first_gift_join_source, join_gift_size_string, first_gift_donor_audience, month_diff_str
     ORDER BY month_diff_int
   ) AS cumulative_amount
 from {{ref(transactions)}} 
