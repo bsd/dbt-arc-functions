cd8f5d09e2db76d7ae4c22c80d308a1ba0caa40e
Frydafly <fryda.guedes@gmail.com>
2023-11-13 12:27:54 -0600

    added cte for processing

--- macros/stitch_sfmc_audience_transaction/marts/mart_audience_1x_activation.sql
+++ macros/stitch_sfmc_audience_transaction/marts/mart_audience_1x_activation.sql
@@ -11,26 +11,38 @@ join_gift_size_string,
 first_gift_donor_audience,
 concat('Act' || month_diff_str ) as activation_str,
 sum(amounts) as total_amount,
-SUM(amounts) OVER (
-    PARTITION BY join_month_year_str, first_gift_join_source, join_gift_size_string, first_gift_donor_audience, month_diff_str
-    ORDER BY month_diff_int
-  ) AS cumulative_amount
 from {{ref(transactions)}} 
 where first_gift_recur_status = 'one_time'
 group by 1, 2, 3, 4, 5
 
 )
 
+, add_cumulative as (
+select 
+join_month_year_str,
+first_gift_join_source,
+join_gift_size_string, 
+first_gift_donor_audience,
+activation_str,
+total_amount
+SUM(total_amount) OVER (
+    PARTITION BY join_month_year_str, first_gift_join_source, join_gift_size_string, first_gift_donor_audience, activation_str
+    ORDER BY month_diff_int
+  ) AS cumulative_amount
+  from rev_by_cohort
+
+)
+
 , donors_in_cohort as (
 select 
-rev_by_cohort.*,
+add_cumulative.*,
 donors_in_cohort.donors_in_cohort,
-from rev_by_cohort
+from add_cumulative
 left join {{ref(donors_in_cohort) }} donors_in_cohort
-on rev_by_cohort.join_month_year_str = donors_in_cohort.join_month_year_str
-and rev_by_cohort.first_gift_join_source = donors_in_cohort.first_gift_join_source
-and rev_by_cohort.join_gift_size_string = donors_in_cohort.join_gift_size_string
-and rev_by_cohort.first_gift_donor_audience = donors_in_cohort.first_gift_donor_audience
+on add_cumulative.join_month_year_str = donors_in_cohort.join_month_year_str
+and add_cumulativet.first_gift_join_source = donors_in_cohort.first_gift_join_source
+and add_cumulative.join_gift_size_string = donors_in_cohort.join_gift_size_string
+and add_cumulative.first_gift_donor_audience = donors_in_cohort.first_gift_donor_audience
 where donors_in_cohort.first_gift_recur_status = 'one_time'
 
 )
