ff05dcad15eb2a0c6bbbad28820df50e0ab3b389
Frydafly <fryda.guedes@gmail.com>
2023-11-13 12:37:01 -0600

    typo

--- macros/stitch_sfmc_audience_transaction/marts/mart_audience_recur_retention.sql
+++ macros/stitch_sfmc_audience_transaction/marts/mart_audience_recur_retention.sql
@@ -44,7 +44,7 @@ from add_cumulative
 left join {{ref(donors_in_cohort) }} donors_in_cohort
 on add_cumulative.join_month_year_str = donors_in_cohort.join_month_year_str
 and add_cumulative.first_gift_join_source = donors_in_cohort.first_gift_join_source
-and add_cumulative.join_gift_size_string_recur = donors_in_cohort.join_gift_size_strin_recur
+and add_cumulative.join_gift_size_string_recur = donors_in_cohort.join_gift_size_string_recur
 and add_cumulative.first_gift_donor_audience = donors_in_cohort.first_gift_donor_audience
 
 
