d94aeb5aead7dd33cf5c38e68c4c134b7c3becd1
Frydafly <fryda.guedes@gmail.com>
2023-11-15 14:50:01 -0600

    do not truncate strings

--- macros/utils/util_mart_audience_retention_activation.sql
+++ macros/utils/util_mart_audience_retention_activation.sql
@@ -64,7 +64,7 @@ CROSS JOIN month_diff_sequence
         coalesce(rev_by_cohort.first_gift_join_source, first_gift_rollup.first_gift_join_source) as join_source,
         coalesce(rev_by_cohort.join_gift_size_string{{recur_suffix}}, first_gift_rollup.join_gift_size_string{{recur_suffix}}) as join_gift_size,
         coalesce(rev_by_cohort.first_gift_donor_audience, first_gift_rollup.first_gift_donor_audience) as join_donor_audience,
-        '{{ ret_or_act }}' || LPAD(CAST(coalesce(rev_by_cohort.month_diff_int, first_gift_rollup.month_diff_int) AS STRING), 2, '0') as {{retention_or_activation}}_str,
+        '{{ ret_or_act }}' || LPAD(CAST(coalesce(rev_by_cohort.month_diff_int, first_gift_rollup.month_diff_int) AS STRING), 3, '0') as {{retention_or_activation}}_str,
         coalesce(rev_by_cohort.month_diff_int, first_gift_rollup.month_diff_int) as month_diff_int,
         rev_by_cohort.total_amount as total_amount,
         first_gift_rollup.donors_in_cohort
