5a84c28e72c32c4899ac428277f2c3e4077eb9d6
Frydafly <fryda.guedes@gmail.com>
2023-11-15 14:37:32 -0600

    big overhaul

--- macros/utils/util_mart_audience_retention_activation.sql
+++ macros/utils/util_mart_audience_retention_activation.sql
@@ -12,20 +12,16 @@
 {% set donors_in_cohort = 'stg_stitch_sfmc_audience_transaction_first_gift_recur_rollup' if recur_status == 'recurring' else 'stg_stitch_sfmc_audience_transaction_first_gift_1x_rollup' %}
 
 
-with first_gift_rollup as (
+with first_gift_by_cohort as (
 select
 join_month_year_str,
 first_gift_join_source,
 join_gift_size_string{{recur_suffix}},
 first_gift_donor_audience,
-CAST(NULL as string) as {{retention_or_activation}}_str,
-cast(NULL as int64) as month_diff_int,
-cast(NULL as float64) as total_amount,
-cast(NULL as float64) as cumulative_amount,
 count(distinct person_id) as donors_in_cohort
 from {{ ref(first_gift_table)}}
 where first_gift_recur_status = {{boolean_status}}
-group by 1, 2, 3, 4, 5, 6, 7, 8
+group by 1, 2, 3, 4
 
 )
 
@@ -35,46 +31,62 @@ group by 1, 2, 3, 4, 5, 6, 7, 8
         first_gift_join_source,
         join_gift_size_string{{recur_suffix}},
         first_gift_donor_audience,
-        concat('{{ ret_or_act }}' || month_diff_str ) as {{retention_or_activation}}_str,
         month_diff_int,
         sum(amounts) as total_amount
     from {{ ref(transactions_table) }} 
     where first_gift_recur_status = {{boolean_status}}
-    group by 1, 2, 3, 4, 5, 6
+    group by 1, 2, 3, 4, 5
 ),
 
-add_cumulative as (
+month_diff_sequence AS (
+    SELECT number AS month_diff_int
+    FROM UNNEST(GENERATE_ARRAY(0, 100)) AS number
+)
+
+, first_gift_rollup as (
+SELECT 
+    first_gift_by_cohort.join_month_year_str,
+    first_gift_by_cohort.first_gift_join_source,
+    first_gift_by_cohort.join_gift_size_string{{recur_suffix}},
+    first_gift_by_cohort.first_gift_donor_audience,
+    month_diff_sequence.month_diff_int,
+    first_gift_by_cohort.donors_in_cohort
+FROM first_gift_by_cohort
+CROSS JOIN month_diff_sequence
+
+
+)
+
+
+, join_cohorts as (
     select 
-        join_month_year_str,
-        first_gift_join_source,
-        join_gift_size_string{{recur_suffix}},
-        first_gift_donor_audience,
-        {{retention_or_activation}}_str,
-        month_diff_int,
-        total_amount,
+        coalesce(rev_by_cohort.join_month_year_str, first_gift_rollup.join_month_year_str) as join_month_year_str,
+        coalesce(rev_by_cohort.first_gift_join_source, first_gift_rollup.first_gift_join_source) as join_source,
+        coalesce(rev_by_cohort.join_gift_size_string{{recur_suffix}}, first_gift_rollup.join_gift_size_string{{recur_suffix}}) as join_gift_size,
+        coalesce(rev_by_cohort.first_gift_donor_audience, first_gift_rollup.first_gift_donor_audience) as join_donor_audience,
+        '{{ ret_or_act }}' || LPAD(CAST(coalesce(rev_by_cohort.month_diff_int, month_diff_sequence.month_diff_int) AS STRING), 2, '0') as {{activation_or_retention}}_str,
+        coalesce(rev_by_cohort.month_diff_int, month_diff_sequence.month_diff_int) as month_diff_int,
+        rev_by_cohort.total_amount as total_amount,
+        first_gift_rollup.donors_in_cohort
+    from rev_by_cohort
+    full outer join first_gift_rollup
+    on rev_by_cohort.join_month_year_str = first_gift_rollup.join_month_year_str
+    and rev_by_cohort.first_gift_join_source = first_gift_rollup.first_gift_join_source
+    and rev_by_cohort.join_gift_size_string{{recur_suffix}} = first_gift_rollup.join_gift_size_string{{recur_suffix}}
+    and rev_by_cohort.first_gift_donor_audience = first_gift_rollup.first_gift_donor_audience
+    and rev_by_cohort.month_diff_int = first_gift_rollup.month_diff_int
+        )
+
+        select *,
         SUM(total_amount) OVER (
             PARTITION BY join_month_year_str, first_gift_join_source, join_gift_size_string{{recur_suffix}}, first_gift_donor_audience
             ORDER BY month_diff_int asc
         ) AS cumulative_amount
-    from rev_by_cohort
-)
+        from join_cohorts
 
-    select 
-        coalesce(add_cumulative.join_month_year_str, first_gift_rollup.join_month_year_str) as join_month_year_str,
-        coalesce(add_cumulative.first_gift_join_source, first_gift_rollup.first_gift_join_source) as join_source,
-        coalesce(add_cumulative.join_gift_size_string{{recur_suffix}}, first_gift_rollup.join_gift_size_string{{recur_suffix}}) as join_gift_size,
-        coalesce(add_cumulative.first_gift_donor_audience, first_gift_rollup.first_gift_donor_audience) as join_donor_audience,
-        coalesce(add_cumulative.{{retention_or_activation}}_str, first_gift_rollup.{{retention_or_activation}}_str) as {{retention_or_activation}}_str,
-        coalesce(add_cumulative.month_diff_int, first_gift_rollup.month_diff_int) as {{retention_or_activation}}_int,
-        coalesce(add_cumulative.total_amount, first_gift_rollup.total_amount) as total_amount,
-        coalesce(add_cumulative.cumulative_amount, first_gift_rollup.cumulative_amount) as cumulative_amount,
-        first_gift_rollup.donors_in_cohort
-    from add_cumulative
-    full outer join first_gift_rollup
-    on add_cumulative.join_month_year_str = first_gift_rollup.join_month_year_str
-    and add_cumulative.first_gift_join_source = first_gift_rollup.first_gift_join_source
-    and add_cumulative.join_gift_size_string{{recur_suffix}} = first_gift_rollup.join_gift_size_string{{recur_suffix}}
-    and add_cumulative.first_gift_donor_audience = first_gift_rollup.first_gift_donor_audience
+
+
+            
 
 
 {% endmacro %}
