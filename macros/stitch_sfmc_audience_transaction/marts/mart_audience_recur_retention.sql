0fc43a020376cfe61bfd21c68c1d077844b042fa
Frydafly <fryda.guedes@gmail.com>
2023-11-16 14:17:04 -0600

    reworking of recur retention modeling

--- macros/stitch_sfmc_audience_transaction/marts/mart_audience_recur_retention.sql
+++ macros/stitch_sfmc_audience_transaction/marts/mart_audience_recur_retention.sql
@@ -1,7 +1,9 @@
 {% macro create_mart_audience_recur_retention(
     rev_by_cohort='stg_stitch_sfmc_audience_transaction_recur_rev_by_cohort',
-    first_gift_explode='stg_stitch_sfmc_audience_transaction_recur_first_gift_by_cohort'
+    first_gift_explode='stg_stitch_sfmc_audience_transaction_recur_first_gift_by_cohort',
+    donors_by_cohort='stg_stitch_sfmc_audience_transaction_recur_donors_by_cohort'
 ) %}
+
 
 
   with 
@@ -9,28 +11,45 @@
         select
             coalesce(
                 rev_by_cohort.join_month_year_str,
-                first_gift_explode.join_month_year_str
+                first_gift_explode.join_month_year_str,
+                donors_by_cohort.join_month_year_str
             ) as join_month_year_str,
             coalesce(
                 rev_by_cohort.first_gift_join_source,
-                first_gift_explode.first_gift_join_source
+                first_gift_explode.first_gift_join_source,
+                donors_by_cohort.first_gift_join_source
             ) as join_source,
             coalesce(
                 rev_by_cohort.join_gift_size_string,
-                first_gift_explode.join_gift_size_string
+                first_gift_explode.join_gift_size_string,
+                donors_by_cohort.join_gift_size_string
             ) as join_gift_size,
             coalesce(
                 rev_by_cohort.first_gift_donor_audience,
-                first_gift_explode.first_gift_donor_audience
+                first_gift_explode.first_gift_donor_audience,
+                donors_by_cohort.first_gift_donor_audience
             ) as join_donor_audience,
             coalesce(
                 rev_by_cohort.month_diff_int,
-                first_gift_explode.month_diff_int
-            ) retention_int,
+                first_gift_explode.month_diff_int,
+                donors_by_cohort.month_diff_int
+            ) activation_int,
             rev_by_cohort.total_amount,
-            rev_by_cohort.unique_donations,
-            first_gift_explode.donors_in_cohort
+            first_gift_explode.donors_in_cohort,
+            donors_by_cohort.unique_donors
         from {{ref(rev_by_cohort)}} rev_by_cohort
+        left join
+            {{ref(donors_by_cohort)}} donors_by_cohort
+            on rev_by_cohort.join_month_year_str
+            = donors_by_cohort.join_month_year_str
+            and rev_by_cohort.first_gift_join_source
+            = donors_by_cohort.first_gift_join_source
+            and rev_by_cohort.join_gift_size_string
+            = donors_by_cohort.join_gift_size_string
+            and rev_by_cohort.first_gift_donor_audience
+            = donors_by_cohort.first_gift_donor_audience
+            and rev_by_cohort.month_diff_int
+            = donors_by_cohort.month_diff_int
         full outer join
             {{ref(first_gift_explode)}} first_gift_explode
             on rev_by_cohort.join_month_year_str = first_gift_explode.join_month_year_str
