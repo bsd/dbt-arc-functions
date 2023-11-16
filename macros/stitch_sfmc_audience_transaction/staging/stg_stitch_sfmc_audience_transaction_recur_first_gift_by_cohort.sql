8df43d62008862d95b42d4ebd507617a8eadb657
Frydafly <fryda.guedes@gmail.com>
2023-11-16 13:09:14 -0600

    change to recur

--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_recur_first_gift_by_cohort.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_recur_first_gift_by_cohort.sql
@@ -1,4 +1,4 @@
-{% macro create_stg_sfmc_audience_transaction_onetime_first_gift_by_cohort() %}
+{% macro create_stg_sfmc_audience_transaction_recur_first_gift_by_cohort() %}
 
 {{dbt_arc_functions.util_stg_stitch_audience_first_gift_by_cohort(recur_status='recurring')}}
 
