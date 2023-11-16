94c3aad69849c049f05e83feb53b3f1a203bd525
Frydafly <fryda.guedes@gmail.com>
2023-11-16 15:13:35 -0600

    formatting

--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_parameterized_audience_transactions_enriched.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_parameterized_audience_transactions_enriched.sql
@@ -35,32 +35,33 @@ select
             then '501-1000'
             when amount between 1001 and 10000.99
             then '1001-10000'
+            when amount > 10000
             else '10000+'
         end
     ) as gift_size_string,
     (
         case
-            when transactions.amount between 0 and 10.99
+            when amount between 0 and 10.99
             then '0-10'
-            when transactions.amount between 11 and 20.99
+            when amount between 11 and 20.99
             then '11-20'
-            when transactions.amount between 21 and 30.99
+            when amount between 21 and 30.99
             then '21-30'
-            when transactions.amount between 31 and 40.99
+            when amount between 31 and 40.99
             then '31-40'
-            when transactions.amount between 41 and 50.99
+            when amount between 41 and 50.99
             then '41-50'
-            when transactions.amount between 51 and 60.99
+            when amount between 51 and 60.99
             then '51-60'
-            when transactions.amount between 61 and 70.99
+            when amount between 61 and 70.99
             then '61-70'
-            when transactions.amount between 71 and 80.99
+            when amount between 71 and 80.99
             then '71-80'
-            when transactions.amount between 81 and 90.99
+            when amount between 81 and 90.99
             then '81-90'
-            when transactions.amount between 91 and 100.99
+            when amount between 91 and 100.99
             then '91-100'
-            when transactions.amount > 100
+            when amount > 100
             then '100+'
         end
     ) as gift_size_string_recur
