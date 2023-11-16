52d9e76c764ae9c6f3fe1918351f0aca50a5fbc5
Timothy Ryan <tryan@bluestate.co>
2023-11-16 12:40:37 -0500

    ARC-1628 pull all info from transactions

--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transactions_enriched_rollup_join_person_and_transaction.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transactions_enriched_rollup_join_person_and_transaction.sql
@@ -1,13 +1,12 @@
 {% macro create_stg_stitch_sfmc_audience_transactions_enriched_rollup_join_person_and_transaction(
-    audience="stg_stitch_sfmc_arc_audience_unioned",
     first_gift="stg_stitch_sfmc_audience_transaction_first_gift",
     transactions="stg_stitch_sfmc_arc_audience_union_transaction_joined_enriched",
-    donor_engagement="stg_stitch_sfmc_audience_transaction_person_engagement_with_start_and_end_dates"
+    donor_engagement="stg_stitch_sfmc_donor_engagement_by_date_day"
 ) %}
     select
-        audience.date_day as date_day,
-        audience.person_id as person_id,
-        audience.donor_audience as donor_audience,
+        donor_engagement.date_day as date_day,
+        donor_engagement.person_id as person_id,
+        transactions.coalesced_audience as donor_audience,
         transactions.donor_loyalty as donor_loyalty,
         transactions.nth_transaction_this_fiscal_year
         as nth_transaction_this_fiscal_year,
@@ -19,9 +18,26 @@
         first_gift.join_gift_size_string_recur as join_amount_str_recur,
         first_gift.join_month_year_date as join_month_year_str,
     from
+           (
+            select date_day, person_id, donor_engagement
+            from {{ ref(donor_engagement) }}
+        ) as donor_engagement
+    left join
         (
-            select date_day, person_id, donor_audience from {{ ref(audience) }}
-        ) as audience
+            select
+                person_id,
+                transaction_date_day,
+                donor_loyalty,
+                recurring,
+                gift_size_string,
+                coalesced_audience,
+                row_number() over (
+                    partition by person_id, fiscal_year order by transaction_date_day
+                ) as nth_transaction_this_fiscal_year
+            from {{ ref(transactions) }}
+        ) as transactions
+    on donor_engagement.person_id = transactions.person_id
+    and donor_engagement.date_day = transactions.transaction_date_day
     left join
         (
             select
@@ -32,29 +48,5 @@
                 join_month_year_date
             from {{ ref(first_gift) }}
         ) as first_gift
-        on audience.person_id = first_gift.person_id
-    left join
-        (
-            select
-                person_id,
-                transaction_date_day,
-                donor_loyalty,
-                recurring,
-                gift_size_string,
-                row_number() over (
-                    partition by person_id, fiscal_year order by transaction_date_day
-                ) as nth_transaction_this_fiscal_year
-            from {{ ref(transactions) }}
-        ) as transactions
-        on audience.date_day = transactions.transaction_date_day
-        and audience.person_id = transactions.person_id
-    left join
-        (
-            select person_id, donor_engagement, start_date, end_date
-            from {{ ref(donor_engagement) }}
-        ) as donor_engagement
-        on audience.date_day >= donor_engagement.start_date
-        and (audience.date_day <= donor_engagement.end_date or donor_engagement.end_date is null)
-        and audience.person_id = donor_engagement.person_id
-
+        on donor_engagement.person_id = first_gift.person_id
 {% endmacro %}
