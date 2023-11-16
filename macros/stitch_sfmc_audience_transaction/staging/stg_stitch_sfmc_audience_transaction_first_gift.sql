396200d64ee96b82b4e67ff3dc11c7be640a5e85
Frydafly <fryda.guedes@gmail.com>
2023-11-16 15:04:59 -0600

    udpated logic for gift size recur and one time

--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_first_gift.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_first_gift.sql
@@ -37,42 +37,43 @@ with first_transactions as (
         transactions.inbound_channel as first_gift_join_source,
 (
         case
-            when transactions.amount between 0 and 25
+            when transactions.amount between 0 and 25.99
             then '0-25'
-            when transactions.amount between 26 and 100
+            when transactions.amount between 26 and 100.99
             then '26-100'
-            when transactions.amount between 101 and 250
+            when transactions.amount between 101 and 250.99
             then '101-250'
-            when transactions.amount between 251 and 500
+            when transactions.amount between 251 and 500.99
             then '251-500'
-            when transactions.amount between 501 and 1000
+            when transactions.amount between 501 and 1000.99
             then '501-1000'
-            when transactions.amount between 1001 and 10000
+            when transactions.amount between 1001 and 10000.99
             then '1001-10000'
-            else '10000+'
+            when transactions.amount > 10000
+            then '10000+'
         end
     ) as join_gift_size_string,
     (
         case
-            when transactions.amount between 0 and 10
+            when transactions.amount between 0 and 10.99
             then '0-10'
-            when transactions.amount between 11 and 20
+            when transactions.amount between 11 and 20.99
             then '11-20'
-            when transactions.amount between 21 and 30
+            when transactions.amount between 21 and 30.99
             then '21-30'
-            when transactions.amount between 31 and 40
+            when transactions.amount between 31 and 40.99
             then '31-40'
-            when transactions.amount between 41 and 50
+            when transactions.amount between 41 and 50.99
             then '41-50'
-            when transactions.amount between 51 and 60
+            when transactions.amount between 51 and 60.99
             then '51-60'
-            when transactions.amount between 61 and 70
+            when transactions.amount between 61 and 70.99
             then '61-70'
-            when transactions.amount between 71 and 80
+            when transactions.amount between 71 and 80.99
             then '71-80'
-            when transactions.amount between 81 and 90
+            when transactions.amount between 81 and 90.99
             then '81-90'
-            when transactions.amount between 91 and 100
+            when transactions.amount between 91 and 100.99
             then '91-100'
             when transactions.amount > 100
             then '100+'
