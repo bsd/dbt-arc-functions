f34a9772266810e64be0472e8dbc71224d417948
Timothy Ryan <tryan@bluestate.co>
2023-11-22 13:20:52 -0500

    ARC-1718 update obc

--- macros/stitch_facebook_paidmedia/staging/stg_stitch_facebook_paidmedia_clicks_daily_rollup.sql
+++ macros/stitch_facebook_paidmedia/staging/stg_stitch_facebook_paidmedia_clicks_daily_rollup.sql
@@ -5,7 +5,9 @@
     select
         ad_id as message_id,
         safe_cast(date_start as timestamp) as date_timestamp,
-        clicks as total_clicks,
-        unique_clicks as unique_clicks
+        sum(obc.value.value) as total_clicks,
+        NULL as unique_clicks
     from {{ source(source_name, source_table) }} ads_insights
+    cross join unnest(outbound_clicks) obc
+    group by 1, 2
 {% endmacro %}
