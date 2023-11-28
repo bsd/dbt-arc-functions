a60fe91d37255df1bde9b9fb312c0ada2489d959
Timothy Ryan <tryan@bluestate.co>
2023-11-28 15:35:32 -0500

    ARC-1630 entry for every date, audience, platform

--- macros/utils/util_stg_stitch_sfmc_audience_transaction_frequency_donor_counts_interval.sql
+++ macros/utils/util_stg_stitch_sfmc_audience_transaction_frequency_donor_counts_interval.sql
@@ -2,7 +2,7 @@
 {% macro util_stg_stitch_sfmc_audience_transaction_frequency_donor_counts_interval(
     frequency,
     interval,
-    reference_name="stg_stitch_sfmc_audience_transactions_enriched_rollup_join_person_and_transaction"
+    person_and_transaction="stg_stitch_sfmc_audience_transactions_enriched_rollup_join_person_and_transaction"
 ) %}
     {% if frequency not in ['recurring', 'onetime'] %}
         {{ exceptions.raise_compiler_error("'frequency' argument to util_stg_stitch_sfmc_audience_transaction_frequency_donor_counts_interval must be 'recurring' or 'onetime', got " ~ frequency) }}
@@ -11,21 +11,60 @@
         {{ exceptions.raise_compiler_error("'interval' argument to util_stg_stitch_sfmc_audience_transaction_frequency_donor_counts_interval must be 'day', 'week', 'month', or 'year', got " ~ interval) }}
     {% endif %}
     with
+        date_spine as (
+            {% set min_date_query %}
+        SELECT min(start_date) FROM {{ ref(person_and_transaction) }}
+            {% endset %}
+            {% set min_date_results = run_query(min_date_query) %}
+            {% if execute %}
+                {% set min_date %}'{{min_date_results.columns[0].values()[0]}}'{% endset %}
+            {% else %} {% set min_date = "2020-01-01" %}
+            {% endif %}
+
+            {% set max_date_query %}
+        SELECT max(end_date) FROM {{ ref(person_and_transaction) }}
+            {% endset %}
+            {% set max_date_results = run_query(max_date_query) %}
+            {% if execute %}
+                {% set max_date %}'{{max_date_results.columns[0].values()[0]}}'{% endset %}
+            {% else %} {% set max_date = "2020-01-01" %}
+            {% endif %}
+
+            {{
+                dbt_utils.date_spine(
+                    datepart="day", start_date=min_date, end_date=max_date
+                )
+            }}
+
+        ),
+        date_spine_with_audience_and_platform as (
+            select
+                date_day,
+                donor_audience,
+                channel as platform -- from best_guess_inbound_channel
+            from date_spine
+            cross join (
+                select distinct donor_audience from {{ ref(person_and_transaction) }}
+            )
+            cross join (
+                select distinct channel from {{ ref(person_and_transaction) }}
+            )
+        ),
         intermediate_rollup as (
             select
-                {% if interval == 'day' %} date_day,
-                {% else %} last_day(date_day, {{ interval }}) as date_day,
+                {% if interval == 'day' %} date_spine_with_audience_and_platform.date_day as date_day,
+                {% else %} last_day(date_spine_with_audience_and_platform.date_day, {{ interval }}) as date_day,
                 {% endif %}
                 {% if interval == 'day' %} 'daily' as interval_type,
                 {% elif interval == 'week' %} 'weekly' as interval_type,
                 {% elif interval == 'month' %} 'monthly' as interval_type,
                 {% elif interval == 'year' %} 'yearly' as interval_type,
                 {% endif %}
-                donor_audience,
-                channel as platform, -- from best_guess_inbound_channel
+                date_spine_with_audience_and_platform.donor_audience,
+                date_spine_with_audience_and_platform.platform, -- from best_guess_inbound_channel
                 {{
                     dbt_arc_functions.get_fiscal_year(
-                        "date_day", var("fiscal_year_start")
+                        "date_spine_with_audience_and_platform.date_day", var("fiscal_year_start")
                     )
                 }} as fiscal_year,
                 count(
@@ -134,8 +173,11 @@
                         then person_id
                     end
                 ) as unique_reinstated{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts,
-
-            from {{ ref(reference_name) }}
+            from date_spine_with_audience_and_platform
+            left join {{ ref(person_and_transaction) }} person_and_transaction
+            on date_spine_with_audience_and_platform.date_day = person_and_transaction.date_day
+            and date_spine_with_audience_and_platform.donor_audience = person_and_transaction.donor_audience
+            and date_spine_with_audience_and_platform.platform = person_and_transaction.channel
             group by 1, 2, 3, 4, 5
         )
     select
