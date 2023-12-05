a23d1d93344a4572a6f6bc195b3c6bbf9da81697
Derrick Mondowney <109629735+dmbluestate@users.noreply.github.com>
2023-12-05 18:26:30 -0500

    changed sources

---
+++ macros/stitch_googleads_paidmedia/staging/stg_stitch_google_paidmedia_subscribes_daily_rollup.sql
@@ -0,0 +1,10 @@
+{% macro create_stg_stitch_google_paidmedia_subscribes_daily_rollup(
+    source_name="src_stitch_googleads_paidmedia", source_table="ad_performance_report"
+) %}
+
+Select
+id as message_id,
+date as date,
+cast(null as string) as subscribes
+from {{ source(source_name, source_table ) }}
+{% endmacro %}
