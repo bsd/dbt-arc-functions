6303c85b5e915303c179a800b3c183ee8c2284ec
Frydafly <fryda.guedes@gmail.com>
2023-12-05 11:52:57 -0600

    changing mart audience transaction from platform to channel -- fixin the lifetime gift

--- macros/stitch_sfmc_arc/staging/stg_stitch_sfmc_arc_revenue_and_donor_count_by_lifetime_gifts.sql
+++ macros/stitch_sfmc_arc/staging/stg_stitch_sfmc_arc_revenue_and_donor_count_by_lifetime_gifts.sql
@@ -1,26 +1,41 @@
 {% macro create_stg_stitch_sfmc_arc_revenue_and_donor_count_by_lifetime_gifts(
     reference_name="stg_stitch_sfmc_arc_audience_union_transaction_joined_enriched"
 ) %}
+
+with base as (
     select
         extract(year from transaction_date_day) as transaction_date_year,
         extract(month from transaction_date_day) as transaction_date_month,
         transaction_date_day as transaction_date_day,
-        case
-            when gift_count < 6
-            then "less than 6"
-            when gift_count between 6 and 12
-            then "6-12"
-            when gift_count between 13 and 24
-            then "13-24"
-            when gift_count between 25 and 36
-            then "25-36"
-            else "37+"
-        end as recurring_gift_cumulative_str,
+        SUM(gift_count) OVER (ORDER BY transaction_date_day) AS cumulative_gift_count
         count(distinct person_id) as donors,
         sum(amount) as summed_amount,
     from {{ ref(reference_name) }}
     where recurring = true
     group by 1, 2, 3, 4
     order by 1, 2, 3, 4
+
+)
+
+select 
+transaction_date_year,
+transaction_date_month,
+transaction_date_day,
+cumulative_gift_count,
+donors,
+summed_amount,
+-- create cumulative value for gift count
+case
+    when cumulative_gift_count < 6
+    then "less than 6"
+    when cumulative_gift_count between 6 and 12
+    then "6-12"
+    when cumulative_gift_count between 13 and 24
+    then "13-24"
+    when cumulatiove_gift_count between 25 and 36
+    then "25-36"
+    else "37+"
+end as recurring_gift_cumulative_str
+from base
 
 {% endmacro %}
