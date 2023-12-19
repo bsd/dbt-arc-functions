970a145a3a15cc2927a68c84ba434d305dc6c13e
Frydafly <fryda.guedes@gmail.com>
2023-12-19 15:19:52 -0600

    namign edit

--- macros/audience_budget/staging/stg_audience_budget_onetime_donor_counts_with_interval_combined.sql
+++ macros/audience_budget/staging/stg_audience_budget_onetime_donor_counts_with_interval_combined.sql
@@ -1,4 +1,4 @@
-{% macro create_stg_audience_budget_onetime_donor_count_with_interval_combined(
+{% macro create_stg_audience_budget_onetime_donor_counts_with_interval_combined(
     daily='stg_audience_budget_onetime_donor_count_daily',
     monthly='stg_audience_budget_onetime_donor_count_monthly',
     yearly='stg_audience_budget_onetime_donor_count_yearly'
