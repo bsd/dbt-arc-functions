cb94e9385622c6fcf806546c14d478f3a589394f
Frydafly <fryda.guedes@gmail.com>
2023-12-19 15:17:02 -0600

    day month year values

--- macros/audience_budget/staging/stg_audience_budget_recur_donor_count_monthly.sql
+++ macros/audience_budget/staging/stg_audience_budget_recur_donor_count_monthly.sql
@@ -1,7 +1,7 @@
 {% macro create_stg_audience_budget_recur_donor_count_monthly() %}
     
     {{ dbt_arc_functions.util_stg_audience_budget_frequency_donor_counts_interval_type(
-        interval_type='monthly',
+        interval_type='month',
         frequency='recurring'
    )}}
 
--- macros/audience_budget/staging/stg_audience_budget_recur_donor_count_yearly.sql
+++ macros/audience_budget/staging/stg_audience_budget_recur_donor_count_yearly.sql
@@ -1,7 +1,7 @@
 {% macro create_stg_audience_budget_recur_donor_count_yearly() %}
     
     {{ dbt_arc_functions.util_stg_audience_budget_frequency_donor_counts_interval_type(
-        interval_type='yearly',
+        interval_type='year',
         frequency='recurring'
    )}}
 
