dd5c4b09e2fa57e09c971a5cedd1b24bc349fe70
Frydafly <Frydafly@users.noreply.github.com>
2023-12-19 22:52:11 +0000

    [create-pull-request] automated change

--- macros/arc_revenue/marts/mart_arc_revenue_1x_actuals_by_day.sql
+++ macros/arc_revenue/marts/mart_arc_revenue_1x_actuals_by_day.sql
@@ -1,5 +1,5 @@
 {% macro create_mart_arc_revenue_1x_actuals_by_day() %}
 
-{{ dbt_arc_functions.util_mart_arc_revenue_actuals_by_day(recur_status='onetime') }}
+    {{ dbt_arc_functions.util_mart_arc_revenue_actuals_by_day(recur_status="onetime") }}
 
 {% endmacro %}
