fcde9e95b72f557e065c2899bb3572e0a4c8b5dc
Frydafly <fryda.guedes@gmail.com>
2023-12-19 14:33:45 -0600

    prefix dbt arc functions

--- macros/audience_budget_with_audience_transaction/marts/mart_audience_budget_with_audience_transaction.sql
+++ macros/audience_budget_with_audience_transaction/marts/mart_audience_budget_with_audience_transaction.sql
@@ -1,5 +1,5 @@
 {% macro create_mart_audience_budget_with_audience_transaction() %}
     
-    {{ util_mart_audience_budget_with_audience_transaction(recur_status="onetime") }}
+    {{ dbt_arc_functions.util_mart_audience_budget_with_audience_transaction(recur_status="onetime") }}
 
 {% endmacro %}
--- macros/audience_budget_with_audience_transaction/marts/mart_audience_budget_with_audience_transaction_recur.sql
+++ macros/audience_budget_with_audience_transaction/marts/mart_audience_budget_with_audience_transaction_recur.sql
@@ -1,5 +1,5 @@
 {% macro create_mart_audience_budget_with_audience_transaction_recur() %}
 
-{{ util_mart_audience_budget_with_audience_transaction(recur_status="recurring") }}
+{{ dbt_arc_functions.util_mart_audience_budget_with_audience_transaction(recur_status="recurring") }}
 
 {% endmacro %}
