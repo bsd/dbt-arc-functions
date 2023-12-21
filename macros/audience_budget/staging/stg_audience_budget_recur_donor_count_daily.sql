6cdcfb5f0800d24ae748eb1fc964fc8fb0327cbf
Frydafly <fryda.guedes@gmail.com>
2023-12-21 11:09:54 -0500

    corrected macro name

--- macros/audience_budget/staging/stg_audience_budget_recur_donor_count_daily.sql
+++ macros/audience_budget/staging/stg_audience_budget_recur_donor_count_daily.sql
@@ -1,4 +1,4 @@
-{% macro create_stg_audience_budget_recurring_donor_count_daily() %}
+{% macro create_stg_audience_budget_recur_donor_count_daily() %}
 
     {{
         dbt_arc_functions.util_stg_audience_budget_frequency_donor_counts_interval_type(
