445e5bbed675dbcddcdb4e292bc6080b553ce1d3
ryantimjohn <ryantimjohn@users.noreply.github.com>
2023-12-29 22:21:02 +0000

    [create-pull-request] automated change

--- macros/stitch_sfmc_audience_transaction/staging/stg_audience_donors_by_day.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_audience_donors_by_day.sql
@@ -13,8 +13,14 @@
     }}
 
     select
-        coalesce(donor_engagement.date_day, donor_audience.date_day, donor_loyalty.date_day) as date_day,
-        coalesce(donor_engagement.person_id, donor_audience.person_id, donor_loyalty.person_id) as person_id,
+        coalesce(
+            donor_engagement.date_day, donor_audience.date_day, donor_loyalty.date_day
+        ) as date_day,
+        coalesce(
+            donor_engagement.person_id,
+            donor_audience.person_id,
+            donor_loyalty.person_id
+        ) as person_id,
         donor_audience.donor_audience,
         donor_loyalty.donor_loyalty,
         donor_engagement.donor_engagement,
@@ -29,14 +35,18 @@
         first_gift.join_month_year_str,
         first_gift.first_gift_recur_status as join_recur_status,
         first_gift.first_gift_donor_audience as join_donor_audience
-    from {{ ref(donor_audience)}} as donor_audience
-    left join {{ ref(donor_engagement) }} as donor_engagement
-        on donor_audience.person_id = donor_engagement.person_id 
+    from {{ ref(donor_audience) }} as donor_audience
+    left join
+        {{ ref(donor_engagement) }} as donor_engagement
+        on donor_audience.person_id = donor_engagement.person_id
         and donor_engagement.date_day = donor_audience.date_day
-    left join {{ref(donor_loyalty)}} as donor_loyalty
-        on donor_audience.person_id = donor_loyalty.person_id 
-        and donor_audience.date_day between donor_loyalty.start_date and donor_loyalty.end_date 
-    left join {{ ref(transactions) }} as transactions
+    left join
+        {{ ref(donor_loyalty) }} as donor_loyalty
+        on donor_audience.person_id = donor_loyalty.person_id
+        and donor_audience.date_day
+        between donor_loyalty.start_date and donor_loyalty.end_date
+    left join
+        {{ ref(transactions) }} as transactions
         on donor_audience.person_id = transactions.person_id
         and donor_audience.date_day = transactions.transaction_date_day
     left join
