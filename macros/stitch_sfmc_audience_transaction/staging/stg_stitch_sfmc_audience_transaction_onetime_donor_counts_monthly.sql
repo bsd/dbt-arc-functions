b0ff1a0e48ab0bfe904bf80f0b0f63ecc947c97e
ryantimjohn <ryantimjohn@users.noreply.github.com>
2023-12-29 22:36:53 +0000

    [create-pull-request] automated change

--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_onetime_donor_counts_monthly.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_onetime_donor_counts_monthly.sql
@@ -1,5 +1,4 @@
-{% macro create_stg_stitch_sfmc_audience_transaction_onetime_donor_counts_monthly(
-) %}
+{% macro create_stg_stitch_sfmc_audience_transaction_onetime_donor_counts_monthly() %}
     {{
         dbt_arc_functions.util_stg_stitch_sfmc_audience_transaction_frequency_donor_counts_interval(
             "onetime", "month"
