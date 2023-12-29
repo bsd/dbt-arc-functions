0771f811b8ab9094834227571bafd59049bdcd2b
Frydafly <fryda.guedes@gmail.com>
2023-12-29 12:17:33 -0600

    revert addition of today date calculations

--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transactions_enriched_rollup_join_person_and_transaction.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transactions_enriched_rollup_join_person_and_transaction.sql
@@ -10,36 +10,20 @@
         )
     }}
     select
-        coalesce(
-            donor_engagement.date_day, today_transactions.transaction_date_day
-        ) as date_day,
-        coalesce(donor_engagement.person_id, today_transactions.person_id) as person_id,
-        last_value(
-            coalesce(
-                transactions.coalesced_audience, today_transactions.coalesced_audience
-            ) ignore nulls
-        ) over (
-            partition by
-                coalesce(donor_engagement.person_id, today_transactions.person_id)
-            order by
-                coalesce(
-                    donor_engagement.date_day, today_transactions.transaction_date_day
-                )
+        donor_engagement.date_day as date_day,
+        donor_engagement.person_id as person_id,
+        last_value(transactions.coalesced_audience ignore nulls) over (
+            partition by donor_engagement.person_id
+            order by donor_engagement.date_day
             rows between unbounded preceding and current row
         ) as donor_audience,
-        coalesce(
-            transactions.donor_loyalty, today_transactions.donor_loyalty
-        ) as donor_loyalty,
-        coalesce(
-            transactions.nth_transaction_this_fiscal_year,
-            today_transactions.nth_transaction_this_fiscal_year
-        ) as nth_transaction_this_fiscal_year,
-        coalesce(transactions.recurring, today_transactions.recurring) as recurring,
+        transactions.donor_loyalty as donor_loyalty,
+        transactions.nth_transaction_this_fiscal_year
+        as nth_transaction_this_fiscal_year,
+        transactions.recurring as recurring,
         donor_engagement.donor_engagement as donor_engagement,
-        coalesce(
-            transactions.gift_size_string, today_transactions.gift_size_string
-        ) as gift_size_str,
-        coalesce(transactions.channel, today_transactions.channel) as channel,  -- from best_guess_inbound_channel
+        transactions.gift_size_string as gift_size_str,
+        transactions.channel as channel,  -- from best_guess_inbound_channel
         first_gift.first_gift_join_source as join_source,
         first_gift.join_gift_size_string as join_amount_str,
         first_gift.join_gift_size_string_recur as join_amount_str_recur,
@@ -49,24 +33,6 @@
             select date_day, person_id, donor_engagement
             from {{ ref(donor_engagement) }}
         ) as donor_engagement
-    full outer join
-        (
-            select
-                person_id,
-                transaction_date_day,
-                donor_loyalty,
-                recurring,
-                gift_size_string,
-                coalesced_audience,
-                channel,
-                row_number() over (
-                    partition by person_id, fiscal_year order by transaction_date_day
-                ) as nth_transaction_this_fiscal_year
-            from {{ ref(transactions) }}
-            where transaction_date_day = current_date()
-        ) today_transactions
-        on donor_engagement.person_id = today_transactions.person_id
-        and donor_engagement.date_day = today_transactions.transaction_date_day
     left join
         (
             select
