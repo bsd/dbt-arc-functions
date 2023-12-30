8f383a285f95916b4d97552d9342012e7b32c900
Frydafly <fryda.guedes@gmail.com>
2023-12-30 14:48:55 -0600

    refactoring to remove superfluous scd

--- macros/stitch_sfmc_arc/staging/stg_stitch_sfmc_arc_calculated_audience_by_date_day.sql
+++ macros/stitch_sfmc_arc/staging/stg_stitch_sfmc_arc_calculated_audience_by_date_day.sql
@@ -1,10 +1,9 @@
 {% macro create_stg_stitch_sfmc_arc_calculated_audience_by_date_day(
     calculated_audience="stg_stitch_sfmc_parameterized_audience_transaction_jobs_append",
-    audience_snapshot="snp_stitch_sfmc_arc_audience",
-    calculated_audience_scd="stg_stitch_sfmc_donor_audience_calculated_scd"
+    audience_snapshot="snp_stitch_sfmc_arc_audience"
 ) %}
 
-    with
+with
         date_spine as (
             select date
             from
@@ -38,6 +37,51 @@
                 ) as date
 
         ),
+
+        calculated_with_date_spine as (
+
+            select transaction_date_day, person_id, donor_audience
+            from {{ ref(calculated_audience) }} calc_audience
+            join date_spine on date_spine.date = calc_audience.transaction_date_day
+            where
+                calc_audience.transaction_date_day < (select max(date) from date_spine)
+                and donor_audience is not null
+
+        ),
+        change as (
+            select
+                person_id,
+                transaction_date_day,
+                donor_audience,
+                lag(donor_audience) over (
+                    partition by person_id order by transaction_date_day
+                ) as prev_donor_audience
+            from calculated_with_date_spine
+        ),
+        filtered_changes as (
+            select
+                person_id,
+                transaction_date_day,
+                donor_audience,
+                lead(transaction_date_day) over (
+                    partition by person_id order by transaction_date_day
+                ) as next_date
+            from change
+            where prev_donor_audience is null or donor_audience != prev_donor_audience
+
+        ),
+
+calculated_audience_scd as (
+    select
+        person_id,
+        min(transaction_date_day) as start_date,
+        ifnull(max(next_date) - 1, (select max(date) from date_spine)) as end_date,
+        donor_audience
+    from filtered_changes
+    group by person_id, donor_audience, next_date
+    order by person_id, start_date
+),
+
         audience_by_date_day as (
             select
                 date_spine.date as date_day,
--- macros/stitch_sfmc_arc/staging/stg_stitch_sfmc_donor_engagement_by_date_day.sql
+++ macros/stitch_sfmc_arc/staging/stg_stitch_sfmc_donor_engagement_by_date_day.sql
@@ -1,11 +1,64 @@
 {% macro create_stg_stitch_sfmc_donor_engagement_by_date_day(
-    donor_engagement_scd_table="stg_stitch_sfmc_donor_engagement_scd"
+    donor_engagement="stg_stitch_sfmc_audience_transaction_person_engagement_with_start_and_end_dates"
 ) %}
 
-    with
+
+with
+        change as (
+            select
+                person_id,
+                start_date as transaction_date_day,
+                donor_engagement,
+                lag(donor_engagement) over (
+                    partition by person_id order by start_date
+                ) as prev_donor_engagement
+            from {{ ref(donor_engagement) }}
+        ),
+        date_spine as (
+            select date
+            from
+                unnest(
+                    generate_date_array(
+                        (select min(start_date), from {{ ref(donor_engagement) }}),
+                        ifnull(
+                            (select max(start_date) from {{ ref(donor_engagement) }}),
+                            current_date()
+                        )
+                    )
+                ) as date
+
+        ),
+
+    filtered_changes as (
+            select
+                person_id,
+                transaction_date_day,
+                donor_engagement,
+                lead(transaction_date_day) over (
+                    partition by person_id order by transaction_date_day
+                ) as next_date
+            from change
+            where
+                prev_donor_engagement is null
+                or donor_engagement != prev_donor_engagement
+
+    ),
+
+    donor_engagement_scd as (
+    select
+        person_id,
+        min(transaction_date_day) as start_date,
+        ifnull(max(next_date) - 1, (select max(date) from date_spine)) as end_date,
+        donor_engagement
+    from filtered_changes
+    group by person_id, donor_engagement, next_date
+    order by person_id, start_date
+
+    ),
+
         -- Creates a date spine spanning the range of dates present in the donor
         -- engagement SCD table
-        date_spine as (
+        scd_date_spine as (
             select date_day
             from
                 unnest(
@@ -32,12 +85,12 @@
                 date_spine.date_day,
                 donor_engagement_scd.person_id,
                 donor_engagement_scd.donor_engagement
-            from date_spine
+            from scd_date_spine
             inner join
-                {{ ref(donor_engagement_scd_table) }} as donor_engagement_scd
-                on date_spine.date_day >= date(donor_engagement_scd.start_date)
+                donor_engagement_scd
+                on scd_date_spine.date_day >= date(donor_engagement_scd.start_date)
                 and (
-                    date_spine.date_day <= date(donor_engagement_scd.end_date)
+                    scd_date_spine.date_day <= date(donor_engagement_scd.end_date)
                     or date(donor_engagement_scd.start_date) is null
                 )
         ),
