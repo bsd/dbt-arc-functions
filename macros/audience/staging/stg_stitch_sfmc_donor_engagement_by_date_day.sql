2f9357cfad4317c93fad84e9c508decf55266f4e
Frydafly <fryda.guedes@gmail.com>
2023-12-30 16:25:48 -0600

    alter engagement by date day to suit correct references

--- macros/audience/staging/stg_stitch_sfmc_donor_engagement_by_date_day.sql
+++ macros/audience/staging/stg_stitch_sfmc_donor_engagement_by_date_day.sql
@@ -12,106 +12,106 @@ person_with_all_transaction_dates as (
         person_id, date(transaction_date) as transaction_date
     from
         {{ref(audience_transactions_summary_unioned)}}
-                ),
+),
 
-        start_of_active_and_lapsed as (
-            -- Identifies the start of active and lapsed donor engagements based on
-            -- transaction dates.
-            select
-                person_id,
-                transaction_date,
-                -- Calculates the start of the active engagement.
-                case
-                    -- If there's no previous transaction, consider the current
-                    -- transaction date as the start of active engagement
-                    when
-                        lag(transaction_date) over (
-                            partition by person_id order by transaction_date
-                        )
-                        is null
-                    then transaction_date
-                    -- If the gap between the current and previous transaction exceeds
-                    -- 426 days, consider it a new active engagement
-                    when
-                        date_diff(
-                            transaction_date,
-                            lag(transaction_date) over (
-                                partition by person_id order by transaction_date
-                            ),
-                            day
-                        )
-                        > 426
-                    then transaction_date
-                end as start_of_active,
-                -- Calculates the start of the lapsed engagement.
-                case
-                    -- If the gap between the current and next transaction exceeds 426
-                    -- days, consider it the start of lapsed engagement
-                    when
-                        date_diff(
-                            lead(transaction_date) over (
-                                partition by person_id order by transaction_date
-                            ),
-                            transaction_date,
-                            day
-                        )
-                        > 426
-                    then date_add(transaction_date, interval 426 day)
-                    -- If there's no next transaction and the gap between the current
-                    -- transaction and today exceeds 426 days, mark it as lapsed
-                    when
-                        lead(transaction_date) over (
-                            partition by person_id order by transaction_date
-                        )
-                        is null
-                        and date_diff(current_date, transaction_date, day) > 426
-                    then date_add(transaction_date, interval 426 day)
-                end as start_of_lapsed
-            from person_with_all_transaction_dates
-
-        ),
-        donor_engagement_start_dates as (
-            -- Collects the start dates for active and lapsed engagements.
-            select
-                person_id, 'active' as donor_engagement, start_of_active as start_date,
-            from start_of_active_and_lapsed
-            where start_of_active is not null
-            union all
-            select
-                person_id, 'lapsed' as donor_engagement, start_of_lapsed as start_date,
-            from start_of_active_and_lapsed
-            where start_of_lapsed is not null
-        ),
-
-    donor_engagement as (
+start_of_active_and_lapsed as (
+    -- Identifies the start of active and lapsed donor engagements based on
+    -- transaction dates.
     select
         person_id,
+        transaction_date,
+        -- Calculates the start of the active engagement.
+        case
+            -- If there's no previous transaction, consider the current
+            -- transaction date as the start of active engagement
+            when
+                lag(transaction_date) over (
+                    partition by person_id order by transaction_date
+                )
+                is null
+            then transaction_date
+            -- If the gap between the current and previous transaction exceeds
+            -- 426 days, consider it a new active engagement
+            when
+                date_diff(
+                    transaction_date,
+                    lag(transaction_date) over (
+                        partition by person_id order by transaction_date
+                    ),
+                    day
+                )
+                > 426
+            then transaction_date
+        end as start_of_active,
+        -- Calculates the start of the lapsed engagement.
+        case
+            -- If the gap between the current and next transaction exceeds 426
+            -- days, consider it the start of lapsed engagement
+            when
+                date_diff(
+                    lead(transaction_date) over (
+                        partition by person_id order by transaction_date
+                    ),
+                    transaction_date,
+                    day
+                )
+                > 426
+            then date_add(transaction_date, interval 426 day)
+            -- If there's no next transaction and the gap between the current
+            -- transaction and today exceeds 426 days, mark it as lapsed
+            when
+                lead(transaction_date) over (
+                    partition by person_id order by transaction_date
+                )
+                is null
+                and date_diff(current_date, transaction_date, day) > 426
+            then date_add(transaction_date, interval 426 day)
+        end as start_of_lapsed
+    from person_with_all_transaction_dates
+
+),
+donor_engagement_start_dates as (
+    -- Collects the start dates for active and lapsed engagements.
+    select
+        person_id, 'active' as donor_engagement, start_of_active as start_date,
+    from start_of_active_and_lapsed
+    where start_of_active is not null
+    union all
+    select
+        person_id, 'lapsed' as donor_engagement, start_of_lapsed as start_date,
+    from start_of_active_and_lapsed
+    where start_of_lapsed is not null
+),
+
+donor_engagement as (
+select
+    person_id,
+    donor_engagement,
+    start_date,
+    -- Determines the end date as the day before the next start date (or today)
+    -- for each person and engagement type
+    lead(start_date) over (partition by person_id order by start_date)
+    - 1 as end_date
+from donor_engagement_start_dates
+),
+change as (
+    select
+        person_id,
+        start_date as transaction_date_day,
         donor_engagement,
-        start_date,
-        -- Determines the end date as the day before the next start date (or today)
-        -- for each person and engagement type
-        lead(start_date) over (partition by person_id order by start_date)
-        - 1 as end_date
-    from donor_engagement_start_dates
-    ),
-        change as (
-            select
-                person_id,
-                start_date as transaction_date_day,
-                donor_engagement,
-                lag(donor_engagement) over (
-                    partition by person_id order by start_date
-                ) as prev_donor_engagement
-            from {{ ref(donor_engagement) }}
-        ),
+        lag(donor_engagement) over (
+            partition by person_id order by start_date
+        ) as prev_donor_engagement
+    from donor_engagement
+),
         date_spine as (
             select date
             from
                 unnest(
                     generate_date_array(
-                        (select min(start_date), from {{ ref(donor_engagement) }}),
+                        (select min(start_date), from donor_engagement,
                         ifnull(
-                            (select max(start_date) from {{ ref(donor_engagement) }}),
+                            (select max(start_date) from donor_engagement),
                             current_date()
                         )
                     )
@@ -155,12 +155,12 @@ person_with_all_transaction_dates as (
                     generate_date_array(
                         (
                             select min(start_date),
-                            from {{ ref(donor_engagement_scd_table) }}
+                            from donor_engagement_scd
                         ),
                         ifnull(
                             (
                                 select max(start_date)
-                                from {{ ref(donor_engagement_scd_table) }}
+                                from donor_engagement_scd
                             ),
                             current_date()
                         )
