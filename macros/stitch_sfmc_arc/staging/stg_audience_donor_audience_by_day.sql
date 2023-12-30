d142396fdae6b3a58b9a621e2653330b6563236c
Frydafly <fryda.guedes@gmail.com>
2023-12-30 13:28:21 -0600

    incremental for donor audience by day

--- macros/stitch_sfmc_arc/staging/stg_audience_donor_audience_by_day.sql
+++ macros/stitch_sfmc_arc/staging/stg_audience_donor_audience_by_day.sql
@@ -3,6 +3,13 @@
     calculated_audience="stg_stitch_sfmc_parameterized_audience_transaction_jobs_append"
 ) %}
 
+{{config(
+    materialized='incremental',
+    unique_key='id'
+)}}
+
+with base as (
+
     select
         coalesce(
             audience_unioned.date_day, calculated_audience.transaction_date_day
@@ -22,5 +29,18 @@
     left join
         {{ ref(calculated_audience) }} calculated_audience
         on audience_unioned.date_day = calculated_audience.transaction_date_day
+),
+
+
+    select 
+    {{dbt_utils.generate_surrogate_key(['date_day', 'person_id'])}} as id,
+    * 
+    from base 
+    
+    {% if is_incremental() %}
+    where date_day >= (select date_sub(max(date_day), 7 day) from {{ this }})
+
+    {% endif %}
+
 
 {% endmacro %}
