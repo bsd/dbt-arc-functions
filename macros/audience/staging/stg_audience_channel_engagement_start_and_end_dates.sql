315ea5c2af7a0761ef698bf81a556f2d5e4d31fd
Frydafly <fryda.guedes@gmail.com>
2023-12-30 21:24:26 -0600

    restore channel

--- macros/audience/staging/stg_audience_channel_engagement_start_and_end_dates.sql
+++ macros/audience/staging/stg_audience_channel_engagement_start_and_end_dates.sql
@@ -2,76 +2,96 @@
     transactions_summary="stg_audience_parameterized_transactions_summary_unioned"
 ) %}
 
+
     with
         start_of_active_and_lapsed as (
             select
                 person_id,
-                transaction_date,
+                channel,
+                transaction_date_day,
                 case
                     when
-                        lag(transaction_date) over (
-                            partition by person_id order by transaction_date
+                        lag(transaction_date_day) over (
+                            partition by person_id, channel
+                            order by transaction_date_day
                         )
                         is null
-                    then transaction_date
+                    then transaction_date_day
                     when
                         date_diff(
-                            transaction_date,
-                            lag(transaction_date) over (
-                                partition by person_id order by transaction_date
+                            transaction_date_day,
+                            lag(transaction_date_day) over (
+                                partition by person_id, channel
+                                order by transaction_date_day
                             ),
                             day
                         )
                         > 426
-                    then transaction_date
+                    then transaction_date_day
                 end as start_of_active,
                 case
                     when
                         date_diff(
-                            lead(transaction_date) over (
-                                partition by person_id order by transaction_date
+                            lead(transaction_date_day) over (
+                                partition by person_id, channel
+                                order by transaction_date_day
                             ),
-                            transaction_date,
+                            transaction_date_day,
                             day
                         )
                         > 426
-                    then date_add(transaction_date, interval 426 day)
+                    then date_add(transaction_date_day, interval 426 day)
                     when
-                        lead(transaction_date) over (
-                            partition by person_id order by transaction_date
+                        lead(transaction_date_day) over (
+                            partition by person_id, channel
+                            order by transaction_date_day
                         )
                         is null
-                        and date_diff(current_date, transaction_date, day) > 426
-                    then date_add(transaction_date, interval 426 day)
+                        and date_diff(current_date, transaction_date_day, day) > 426
+                    then date_add(transaction_date_day, interval 426 day)
                 end as start_of_lapsed
             from
                 (
                     select distinct
-                        person_id, date(transaction_date) as transaction_date
-                    from {{ ref(transactions_summary) }}
-                ) as person_with_all_transaction_dates
+                        person_id,
+                        channel,
+                        date(transaction_date_day) as transaction_date_day
+                    from
+                        {{
+                            ref(
+                                start_and_end
+                            )
+                        }}
+                ) as person_with_all_transaction_date_days
 
             order by 1, 2
         ),
         donor_engagement_start_dates as (
             select
-                person_id, 'active' as donor_engagement, start_of_active as start_date,
+                person_id,
+                channel,
+                'active' as donor_engagement,
+                start_of_active as start_date,
             from start_of_active_and_lapsed
             where start_of_active is not null
             union all
             select
-                person_id, 'lapsed' as donor_engagement, start_of_lapsed as start_date,
+                person_id,
+                channel,
+                'lapsed' as donor_engagement,
+                start_of_lapsed as start_date,
             from start_of_active_and_lapsed
             where start_of_lapsed is not null
             order by 1, 3
         )
     select
         person_id,
+        channel,
         donor_engagement,
         start_date,
-        lead(start_date) over (partition by person_id order by start_date)
+        lead(start_date) over (partition by person_id, channel order by start_date)
         - 1 as end_date
     from donor_engagement_start_dates
-    order by 1, 3
+    order by 1, 2, 4
 
 {% endmacro %}
