6f92b067cd70ff9ca3d0e05beb7519a4e4e3444d
Frydafly <fryda.guedes@gmail.com>
2023-12-30 20:35:11 -0600

    missed util

--- macros/utils/util_stg_audience_rev_by_cohort.sql
+++ macros/utils/util_stg_audience_rev_by_cohort.sql
@@ -1,7 +1,7 @@
 -- fmt: off
-{% macro util_stg_stitch_sfmc_audience_transaction_rev_by_cohort(
+{% macro util_stg_audience_transaction_rev_by_cohort(
     recur_status,
-    transactions_table="stg_stitch_sfmc_audience_transaction_with_first_gift_cohort"
+    transactions_table="stg_audience_transaction_with_first_gift_cohort"
 ) %}
 
     {% if recur_status not in ["recurring", "onetime"] %}
