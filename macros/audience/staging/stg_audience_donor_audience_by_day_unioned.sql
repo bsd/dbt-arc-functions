f4914b97b3cece86e35768aa03c5eeab996c95a5
Frydafly <fryda.guedes@gmail.com>
2023-12-30 19:36:14 -0600

    better regex

--- macros/audience/staging/stg_audience_donor_audience_by_day_unioned.sql
+++ macros/audience/staging/stg_audience_donor_audience_by_day_unioned.sql
@@ -1,6 +1,6 @@
 {% macro create_stg_audience_donor_audience_by_day_unioned() %}
     {% set relations = dbt_arc_functions.relations_that_match_regex(
-        "^stg_\w+_audience_donor_audience_by_day$"
+        "^stg_*_audience_donor_audience_by_day$"
     ) %}
     {{ dbt_utils.union_relations(relations) }}
 
