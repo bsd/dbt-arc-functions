4d642bffda0365d06e3762920641b97a6266b4ea
Frydafly <fryda.guedes@gmail.com>
2024-01-03 11:51:14 -0600

    has unique combinations by day test

---
+++ tests/generic/has_unique_combinations_by_date.sql
@@ -0,0 +1,10 @@
+{% test has_unique_combinations_by_date(model, date_column) %}
+
+ {{ config(severity="warn") }}
+
+    select {{date_column}}, count(*) as num_combinations
+    from {{model}}
+    group by 1
+    having count(*) > 1
+
+{% endtest %}
