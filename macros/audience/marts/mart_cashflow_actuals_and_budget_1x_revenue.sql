48407e4c63ea710d7d8fcdf755ebb3c21c4f257a
Frydafly <fryda.guedes@gmail.com>
2024-01-09 12:45:56 -0600

    removal of 1x revenue for cashflow as per stephen

--- macros/audience/marts/mart_cashflow_actuals_and_budget_1x_revenue.sql
+++
@@ -1,9 +0,0 @@
-{% macro create_mart_cashflow_actuals_and_budget_1x_revenue() %}
-
-    {{
-        dbt_arc_functions.util_mart_cashflow_actuals_and_budget(
-            combined_or_onetime="onetime"
-        )
-    }}
-
-{% endmacro %}
