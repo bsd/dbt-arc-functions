29b7a9869ce9c15fa88ab243cc5d8eb8c7f805f6
Derrick Mondowney <109629735+dmbluestate@users.noreply.github.com>
2024-01-12 17:39:44 -0500

    add parameters to mart_paidmedia_monthly_goals_and_actuals.sql

---
+++ macros/paidmedia_goals/staging/stg_paidmedia_parameterized_actuals_goals_unioned_transform.sql
@@ -0,0 +1,70 @@
+{% macro create_stg_paidmedia_actuals_goals_unioned_transform(
+    channel ='null',objective = 'null',platform='null',
+    reference_name="stg_paidmedia_actuals_goals_unioned"
+) %}
+    select
+        month_year,
+        parse_date('%B %Y', month_year) as month_year_date,
+        --case # may apply to fracture based clients
+            --when regexp_contains(channel, 'soc') = true
+            --then 'Social'
+            --when regexp_contains(platform, 'soc') = true
+            --then 'Social'
+            --when regexp_contains(channel, 'sear') = true
+            --then 'Search'
+            --when regexp_contains(platform, 'sear') = true
+            --then 'Search'
+            --when regexp_contains(channel, 'disp') = true
+            --then 'Display'
+            --when regexp_contains(platform, 'disp') = true
+            --then 'Display'
+            --when regexp_contains(channel, 'vid') = true
+            --then 'Display'
+            --when regexp_contains(platform, 'vid') = true
+            --then 'Display'
+            --when regexp_contains(channel, 'youtube') = true
+            --then 'Display'
+            --when regexp_contains(platform, 'youtube') = true
+            --then 'Display'
+            --else channel end
+        {{ channel }} as channel,
+        --case # may apply to fracture based clients
+            --when regexp_contains(objective, 'awar') = true
+            --then 'Awareness'
+            --when regexp_contains(objective, 'acq') = true
+            --then 'Acquisition'
+            --when regexp_contains(objective, 'fun') = true
+            --then 'Fundraising'
+            --else objective end
+        {{ objective }} as objective,
+        --case # may apply to fracture based clients
+            --when regexp_contains(platform, 'goog') = true
+            --then 'Google'
+            --when regexp_contains(platform, 'bing') = true
+            --then 'Bing'
+            --when regexp_contains(platform, 'facebook') = true
+            --then 'Facebook'
+            --when regexp_contains(platform, 'fb') = true
+            --then 'Facebook'
+            --when regexp_contains(platform, 'instagram') = true
+            --then 'Facebook'
+            --when regexp_contains(platform, 'youtube') = true
+            --then 'Google'
+            --when regexp_contains(platform, 'yt') = true
+            --then 'Google'
+            --when regexp_contains(platform, 'linked') = true
+            --then 'LinkedIn'
+            --when regexp_contains(platform, 'yahoo') = true
+            --then 'Yahoo'
+            --when platform = 'ad'
+            --then 'Bing'
+            --else platform end
+        {{ platform }} as platform,
+        sum(actual_spend) as actual_spend,
+        sum(actual_revenue) as actual_revenue,
+        sum(actual_donations) as actual_donations,
+        sum(projected_revenue) as projected_revenue,
+        sum(projected_spend) as projected_spend
+    from {{ ref(reference_name) }}
+    group by 1, 2, 3, 4, 5
+{% endmacro %}
