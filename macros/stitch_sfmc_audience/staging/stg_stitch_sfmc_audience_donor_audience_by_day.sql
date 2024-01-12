231d2b0ec075b217c1cdb6412aeb6ee448d83041
Frydafly <fryda.guedes@gmail.com>
2024-01-12 12:40:38 -0600

    iterate

--- macros/stitch_sfmc_audience/staging/stg_stitch_sfmc_audience_donor_audience_by_day.sql
+++ macros/stitch_sfmc_audience/staging/stg_stitch_sfmc_audience_donor_audience_by_day.sql
@@ -1,31 +1,18 @@
 {% macro create_stg_stitch_sfmc_audience_donor_audience_by_day(
-    audience_snapshot="snp_stitch_sfmc_arc_audience",
+    audience_unioned="stg_stitch_sfmc_calculated_and_arc_unioned",
     calculated_audience_by_day="stg_stitch_sfmc_audience_calculated_audience_by_day"
 ) %}
 
-    /*
-
+ /*  
 ## Final Output
 The final output is a unified table containing the merged audience data from both sources. 
 It includes date, person ID, donor audience status, and a column
 indicating the source of the audience data ('unioned_donor_audience' or 'calculated_donor_audience').
-
 */
-    with
 
-        audience_unioned as (
-            select *
-            from {{ ref(audience_snapshot) }}
-            union all
-            select *
-            from {{ ref(calculated_audience_by_day) }}
-
-        )
-
-    /* rejoin calculated audience into the final audience, filling in blanks */
     select
-        audience_unioned.date_day,
-        audience_unioned.person_id,
+        coalesce(audience_unioned.date_day, calculated_audience_by_day.date_day) as date_day,
+        coalesce(audience_unioned.person_id, calculated_audience_by_day.person_id) as person_id,
         coalesce(
             audience_unioned.donor_audience, calculated_audience.donor_audience
         ) as donor_audience,
@@ -34,7 +21,7 @@ indicating the source of the audience data ('unioned_donor_audience' or 'calcula
             then 'unioned_donor_audience'
             else 'calculated_donor_audience'
         end as source_column
-    from audience_unioned
+    from {{ref(audience_unioned)}}
     left join
         {{ ref(calculated_audience_by_day) }} calculated_audience
         on audience_unioned.date_day = calculated_audience.date_day
