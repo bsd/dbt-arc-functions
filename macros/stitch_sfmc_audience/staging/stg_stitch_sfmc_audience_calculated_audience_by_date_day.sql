1726b162c61997b2884d98c7d0c017f7a1b54ee7
Frydafly <Frydafly@users.noreply.github.com>
2024-01-12 21:04:55 +0000

    [create-pull-request] automated change

--- macros/stitch_sfmc_audience/staging/stg_stitch_sfmc_audience_calculated_audience_by_date_day.sql
+++ macros/stitch_sfmc_audience/staging/stg_stitch_sfmc_audience_calculated_audience_by_date_day.sql
@@ -3,114 +3,131 @@
     calculated_audience="stg_stitch_sfmc_audience_parameterized_calculated_audience"
 ) %}
 
+    -- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_calculated_date_spine.sql
+    with
+        stg_stitch_sfmc_audience_transaction_calculated_date_spine as (
 
--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_audience_transaction_calculated_date_spine.sql
+            select date
+            from
+                unnest(
+                    generate_date_array(
+                        (
+                            select min(transaction_date_day),
+                            from {{ ref(calculated_audience) }}
+                        ),
+                        ifnull(
+                            (
+                                select
+                                    min(
+                                        date(
+                                            cast(
+                                                concat(
+                                                    substr(dbt_valid_from, 0, 22),
+                                                    " America/New_York"
+                                                ) as timestamp
+                                            ),
+                                            "America/New_York"
+                                        )
+                                        - 1
+                                    )
 
-with stg_stitch_sfmc_audience_transaction_calculated_date_spine as (
+                                from {{ ref(audience_snapshot) }}
+                            ),
+                            current_date()
+                        )
+                    )
+                ) as date
 
-select date
-    from
-        unnest(
-            generate_date_array(
-                (
-                    select min(transaction_date_day),
-                    from {{ ref(calculated_audience) }}
-                ),
+        ),
+
+        -- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_donor_audience_calculated_with_date_spine.sql
+        stg_stitch_sfmc_donor_audience_calculated_with_date_spine as (
+
+            select transaction_date_day, person_id, donor_audience
+            from {{ ref(calculated_audience) }} calc_audience
+            join
+                stg_stitch_sfmc_audience_transaction_calculated_date_spine date_spine
+                on date_spine.date = calc_audience.transaction_date_day
+            where
+                calc_audience.transaction_date_day < (
+                    select max(date)
+                    from stg_stitch_sfmc_audience_transaction_calculated_date_spine
+                )
+
+        ),
+
+        -- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_donor_audience_calculated_scd.sql
+        changes as (
+            select
+                person_id,
+                transaction_date_day,
+                donor_audience,
+                lag(donor_audience) over (
+                    partition by person_id order by transaction_date_day
+                ) as prev_donor_audience
+            from stg_stitch_sfmc_donor_audience_calculated_with_date_spine
+        ),
+
+        stg_stitch_sfmc_donor_audience_calculated_scd as (
+            select
+                person_id,
+                min(transaction_date_day) as start_date,
                 ifnull(
+                    max(next_date) - 1,
                     (
-                        select min(date(cast(concat(
-                                            substr(dbt_valid_from, 0, 22),
-                                            " America/New_York"
-                                        ) as timestamp
-                                    ),
-                                    "America/New_York"
-                                ) - 1
-                            )
+                        select max(date)
+                        from stg_stitch_sfmc_audience_transaction_calculated_date_spine
+                    )
+                ) as end_date,
+                donor_audience
+            from
+                (
+                    select
+                        person_id,
+                        transaction_date_day,
+                        donor_audience,
+                        lead(transaction_date_day) over (
+                            partition by person_id order by transaction_date_day
+                        ) as next_date
+                    from changes
+                    where
+                        prev_donor_audience is null
+                        or donor_audience != prev_donor_audience
+                ) filtered_changes
+            group by person_id, donor_audience, next_date
+            order by person_id, start_date
+        ),
 
-                        from {{ ref(audience_snapshot) }}),current_date()))) as date
+        -- macros/stitch_sfmc_arc/staging/stg_stitch_sfmc_arc_calculated_audience_by_date_day.sql
+        audience_by_date_day as (
+            select
+                date_spine.date as date_day,
+                calc_audience_scd.person_id,
+                calc_audience_scd.donor_audience
+            from
+                stg_stitch_sfmc_audience_transaction_calculated_date_spine as date_spine
+            inner join
+                stg_stitch_sfmc_donor_audience_calculated_scd as calc_audience_scd
+                on date_spine.date >= date(calc_audience_scd.start_date)
+                and (
+                    date_spine.date <= date(calc_audience_scd.end_date)
+                    or date(calc_audience_scd.start_date) is null
+                )
+        ),
+        deduplicated_table_scd as (
+            select
+                date_day,
+                person_id,
+                donor_audience,
+                row_number() over (
+                    partition by date_day, person_id order by donor_audience
+                ) as row_num
+            from audience_by_date_day
 
-),
-
---macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_donor_audience_calculated_with_date_spine.sql
-
-
-
-stg_stitch_sfmc_donor_audience_calculated_with_date_spine as (
-
-SELECT
-    transaction_date_day,
-    person_id,
-    donor_audience
-FROM {{ ref(calculated_audience) }} calc_audience
-JOIN stg_stitch_sfmc_audience_transaction_calculated_date_spine date_spine ON date_spine.date = calc_audience.transaction_date_day
-WHERE calc_audience.transaction_date_day < (SELECT MAX(date) FROM stg_stitch_sfmc_audience_transaction_calculated_date_spine)
-
-),
-
-
--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_donor_audience_calculated_scd.sql
-
-changes AS (
-  SELECT
-    person_id,
-    transaction_date_day,
-    donor_audience,
-    LAG(donor_audience) OVER (PARTITION BY person_id ORDER BY transaction_date_day) AS prev_donor_audience
-  FROM stg_stitch_sfmc_donor_audience_calculated_with_date_spine
-),
-
-stg_stitch_sfmc_donor_audience_calculated_scd as (
-SELECT
-  person_id,
-  MIN(transaction_date_day) AS start_date,
-  IFNULL(MAX(next_date) -  1, (SELECT MAX(date) FROM stg_stitch_sfmc_audience_transaction_calculated_date_spine)) AS end_date,
-  donor_audience
-FROM (
-  SELECT
-    person_id,
-    transaction_date_day,
-    donor_audience,
-    LEAD(transaction_date_day) OVER (PARTITION BY person_id ORDER BY transaction_date_day) AS next_date
-  FROM changes
-  WHERE prev_donor_audience IS NULL OR donor_audience != prev_donor_audience
-) filtered_changes
-GROUP BY person_id, donor_audience, next_date
-ORDER BY person_id, start_date
-),
-
-
--- macros/stitch_sfmc_arc/staging/stg_stitch_sfmc_arc_calculated_audience_by_date_day.sql
-
-audience_by_date_day as (
-    select
-        date_spine.date as date_day,
-        calc_audience_scd.person_id,
-        calc_audience_scd.donor_audience
-    from stg_stitch_sfmc_audience_transaction_calculated_date_spine as date_spine
-    inner join
-        stg_stitch_sfmc_donor_audience_calculated_scd as calc_audience_scd
-        on date_spine.date >= date(calc_audience_scd.start_date)
-        and (
-            date_spine.date <= date(calc_audience_scd.end_date)
-            or date(calc_audience_scd.start_date) is null
         )
-),
-deduplicated_table_scd as (
-    select
-        date_day,
-        person_id,
-        donor_audience,
-        row_number() over (
-            partition by date_day, person_id order by donor_audience
-        ) as row_num
-    from audience_by_date_day
 
-)
-
-select date_day, person_id, donor_audience
-from deduplicated_table_scd
-where row_num = 1
-
-
+    select date_day, person_id, donor_audience
+    from deduplicated_table_scd
+    where row_num = 1
 
 {% endmacro %}
--- macros/stitch_sfmc_audience/staging/stg_stitch_sfmc_audience_donor_audience_by_day.sql
+++ macros/stitch_sfmc_audience/staging/stg_stitch_sfmc_audience_donor_audience_by_day.sql
@@ -3,16 +3,17 @@
     calculated_audience_by_day="stg_stitch_sfmc_audience_calculated_audience_by_date_day"
 ) %}
 
- /*  
+    /*  
 ## Final Output
 The final output is a unified table containing the merged audience data from both sources. 
 It includes date, person ID, donor audience status, and a column
 indicating the source of the audience data ('unioned_donor_audience' or 'calculated_donor_audience').
 */
-
     select
         coalesce(audience_unioned.date_day, calculated_audience.date_day) as date_day,
-        coalesce(audience_unioned.person_id, calculated_audience.person_id) as person_id,
+        coalesce(
+            audience_unioned.person_id, calculated_audience.person_id
+        ) as person_id,
         coalesce(
             audience_unioned.donor_audience, calculated_audience.donor_audience
         ) as donor_audience,
@@ -21,7 +22,7 @@ indicating the source of the audience data ('unioned_donor_audience' or 'calcula
             then 'unioned_donor_audience'
             else 'calculated_donor_audience'
         end as source_column
-    from {{ref(audience_unioned)}} audience_unioned
+    from {{ ref(audience_unioned) }} audience_unioned
     left join
         {{ ref(calculated_audience_by_day) }} calculated_audience
         on audience_unioned.date_day = calculated_audience.date_day
