6ba416e8286daf4e164af4b3d7095761b11e6d76
Frydafly <fryda.guedes@gmail.com>
2024-01-23 12:16:42 -0600

    added cte for more readability

--- tests/audience/test_stg_audience_channel_by_day_cross_join.sql
+++ tests/audience/test_stg_audience_channel_by_day_cross_join.sql
@@ -1,16 +1,19 @@
 {{ config(severity="error", warn_if=">1", error_if=">10") }}
 
-SELECT
-  COUNT(*) AS total_rows,
-  COUNT(DISTINCT donor_audience) AS distinct_audiences,
-  COUNT(DISTINCT channel) AS distinct_channels
-FROM (
-  SELECT
+
+with combined_data as (
+    SELECT
     date_day,
     donor_audience,
     channel
-  FROM {{ ref('stg_audience_channel_by_day_cross_join') }}
-) AS combined_data
+  from {{ ref('stg_audience_channel_by_day_cross_join') }}
+)
+
+select
+  count(*) as total_rows,
+  count(distinct donor_audience) as distinct_audiences,
+  count(distinct channel) as distinct_channels
+from combined_data
 -- Check for significant difference between total rows and unique audience/channel counts
-HAVING
-  COUNT(*) > 10 * (COUNT(DISTINCT donor_audience) + COUNT(DISTINCT channel))
+having
+  count(*) > 10 * (count(distinct donor_audience) + count(distinct channel))
