262900014142057b107fd9b4ed8073f298f95c56
Frydafly <fryda.guedes@gmail.com>
2024-01-23 14:49:55 -0600

    sqlfmt .

--- macros/stitch_facebook_paidmedia/staging/stg_stitch_facebook_paidmedia_campaigns.sql
+++ macros/stitch_facebook_paidmedia/staging/stg_stitch_facebook_paidmedia_campaigns.sql
@@ -1,18 +1,16 @@
 {% macro create_stg_stitch_facebook_paidmedia_campaigns(
     source_name="src_stitch_facebook_paidmedia", source_table="ads"
 ) %}
-    select distinct
-        ads.campaign_id as campaign_id,
-        ads.ad_id as message_id,
-        'social' as channel_category,
-        'facebook_ad' as channel,
-        cast(null as string) as channel_type,
-        campaigns.name as campaign_name,
-        cast(null as string) as crm_entity,
-        cast(null as string) as source_code_entity,
-        cast(null as string) as preview_url
-    from {{ source(source_name, source_table) }} ads
-    join
-        {{ source(source_name, "campaigns") }} campaigns
-        on ads.campaign_id = campaigns.id
+select distinct
+    ads.campaign_id as campaign_id,
+    ads.ad_id as message_id,
+    'social' as channel_category,
+    'facebook_ad' as channel,
+    cast(null as string) as channel_type,
+    campaigns.name as campaign_name,
+    cast(null as string) as crm_entity,
+    cast(null as string) as source_code_entity,
+    cast(null as string) as preview_url
+from {{ source(source_name, source_table) }} ads
+join {{ source(source_name, "campaigns") }} campaigns on ads.campaign_id = campaigns.id
 {% endmacro %}
