30d044e52b692a2f89364d9fb746dc487f6f20b9
Frydafly <fryda.guedes@gmail.com>
2024-01-23 13:52:55 -0600

    try

--- tests/audience/test_stg_audience_channel_by_day_cross_join.sql
+++ tests/audience/test_stg_audience_channel_by_day_cross_join.sql
@@ -6,34 +6,18 @@ with combined_data as (
   from {{ ref('stg_audience_channel_by_day_cross_join') }}
 ),
 
-distinct_data as (
-select
-  count(*) as valid_combinations
-from (
-  select distinct donor_audience, channel from combined_data
-)
-),
-
-actual_data as (
-  select
-  date_day,
-  count(donor_audience) as donor_audiences,
-  count(channel) as channels,
-  from combined_data
-  group by 1
-),
-
-
-
 potential_explosions as (
   SELECT
-    actual_data.date_day,
-    actual_data.donor_audiences,
-    actual_data.channels,
-    valid_combinations
-  FROM actual_data
-  CROSS JOIN distinct_data
-  WHERE (donor_audiences * channels) > valid_combinations
+    date_day,
+    count(concat(donor_audience, channel)) as actual_combinations
+  FROM combined_data 
+  group by 1 
+  having count(concat(donor_audience, channel)) > 5 * (select
+  count(*)
+from (
+  select distinct donor_audience, channel from combined_data
+)) as valid_combinations
 )
+
 
 select date_day, count(*) from potential_explosions group by 1 having count(*) > 0
