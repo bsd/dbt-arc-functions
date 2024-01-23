5a8483e8c7fb50d06b9baf2b9d5178fd849c2d70
Frydafly <fryda.guedes@gmail.com>
2024-01-23 13:30:21 -0600

    alter test

--- tests/audience/test_stg_audience_channel_by_day_cross_join.sql
+++ tests/audience/test_stg_audience_channel_by_day_cross_join.sql
@@ -4,13 +4,42 @@ with combined_data as (
     donor_audience,
     channel
   from {{ ref('stg_audience_channel_by_day_cross_join') }}
+),
+
+distinct_data as (
+select
+  count(distinct date_day) as distinct_days,
+  count(distinct donor_audience) as distinct_audiences,
+  count(distinct channel) as distinct_channels,
+from combined_data
+),
+
+actual_data as (
+  select
+  date_day,
+  count(donor_audience) as donor_audiences,
+  count(channel) as channels,
+  from combined_data
+  group by 1
+),
+
+mult_distinct_data as (
+  select 
+  distinct_audiences,
+  distinct_channels,
+  (distinct_audiences * distinct_channels) as valid_combinations
+  from distinct_data
+),
+
+potential_explosions as (
+  SELECT
+    actual_data.date_day,
+    actual_data.donor_audiences,
+    actual_data.channels,
+    valid_combinations
+  FROM actual_data
+  CROSS JOIN mult_distinct_data
+  WHERE (donor_audiences * channels) > valid_combinations
 )
 
-select
-  count(*) as total_rows,
-  count(distinct donor_audience) as distinct_audiences,
-  count(distinct channel) as distinct_channels
-from combined_data
--- Check for significant difference between total rows and unique audience/channel counts
-having
-  count(*) > 10 * (count(distinct donor_audience) + count(distinct channel))
+select count(*) from potential_explosions having count(*) > 0
