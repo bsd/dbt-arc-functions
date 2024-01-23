8235ca7f97745882b9f8475075ea0e285d9c5fee
Frydafly <fryda.guedes@gmail.com>
2024-01-23 14:06:38 -0600

    simplify test

--- tests/generic/assert_no_exploding_joins_by_day.sql
+++ tests/generic/assert_no_exploding_joins_by_day.sql
@@ -1,17 +1,15 @@
 {% test assert_no_exploding_joins_by_day (model, date_column='date_day')%}
 
-with base_table_grouped as (
+select *
+from (
   select
     {{date_column}},
-    count(*) as row_count
+    count(*) as row_count,
+    lag(row_count) over (order by {{date_column}}) as previous_row_count
   from {{model}}
   group by 1
-  order by 1
-)
+) as base_table_grouped
+where row_count > previous_row_count
 
-select *
-from base_table_grouped
-where row_count > lag(row_count) over (order by {{date_column}})
-having count(*) > 0
 
 {% endtest %}
