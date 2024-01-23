ccda0ec2c0bd9c01dfa56fb4ce5573c861ec3b4d
Frydafly <fryda.guedes@gmail.com>
2024-01-23 14:17:39 -0600

    fix test

--- tests/generic/assert_no_exploding_joins_by_day.sql
+++ tests/generic/assert_no_exploding_joins_by_day.sql
@@ -1,16 +1,25 @@
 {% test assert_no_exploding_joins_by_day (model, date_column='date_day')%}
 
-select *
-from (
-  select
+with base_table_grouped as (
+    select
     {{date_column}},
-    count(*) as row_count,
-    lag(row_count) over (order by {{date_column}}) as previous_row_count
-  from {{model}}
+    count(*) as row_count
+    from {{model}}
   group by 1
-  order by date_day asc
-) as base_table_grouped
+),
+
+possible_explosions as (
+
+select 
+{{date_column}},
+row_count,
+lag(row_count) over (order by {{date_column}}) as previous_row_count
 where row_count > previous_row_count
+order by date_day asc
+)
+
+select count(*) from possible_explosions 
+having count(*) > 0
 
 
 {% endtest %}
