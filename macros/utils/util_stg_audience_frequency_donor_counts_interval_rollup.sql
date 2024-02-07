db53658046091a898584b9682f8ced9ac25f879b
Frydafly <fryda.guedes@gmail.com>
2024-02-07 13:01:58 -0600

    same system to budget as well as actuals

--- macros/utils/util_stg_audience_frequency_donor_counts_interval_rollup.sql
+++ macros/utils/util_stg_audience_frequency_donor_counts_interval_rollup.sql
@@ -28,7 +28,10 @@ with base as (
     select
             /* dimensions: date_day, interval_type, donor_audience, channel */
         {% if interval == 'day' %} date_spine_with_audience_and_channel.date_day,
-        {% else %} last_day(date_spine_with_audience_and_channel.date_day, {{ interval }}) as date_day,
+        {% elif interval == 'month' %}
+        date(year(date_spine_with_audience_and_channel.date_day), month(date_spine_with_audience_and_channel.date_day), 1) as date_day,
+        {% elif interval == 'year' %}
+        date(year(date_spine_with_audience_and_channel.date_day), 1, 1) as date_day,
         {% endif %}
         {% if interval == 'day' %} 'daily' as interval_type,
         {% elif interval == 'week' %} 'weekly' as interval_type,
