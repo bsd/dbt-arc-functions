733ec4dd6f16cc6fe61ac2770bcedba44209e1d7
Frydafly <fryda.guedes@gmail.com>
2024-02-08 12:52:50 -0600

    f

--- tests/audience/assert_audience_revenue_by_day_equal_across_marts.sql
+++ tests/audience/assert_audience_revenue_by_day_equal_across_marts.sql
@@ -1,3 +1,18 @@
+{% macro revenue_CTE(revenue_column='total_revenue',
+                    mart_name,
+                    frequency,
+                    day_column='transaction_date_day',
+                    ) %}
+
+
+select 
+ {{day_column}} as date_day,
+ sum({{revenue_column}}) as {{frequency}}_revenue
+from {{ref(mart_name)}}
+group by 1
+
+{% endmacro %}
+
 with a as (
  {{revenue_CTE(mart_name='mart_arc_revenue_1x_actuals_by_day',
               frequency='onetime')}}
