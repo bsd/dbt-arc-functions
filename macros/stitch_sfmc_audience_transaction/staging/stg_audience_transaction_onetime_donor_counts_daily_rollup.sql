6fc80083377ac00cb3e9cfa96b3f79547f23d18c
Frydafly <fryda.guedes@gmail.com>
2024-02-09 14:31:29 -0600

    comma and new srouces

--- macros/stitch_sfmc_audience_transaction/staging/stg_audience_transaction_onetime_donor_counts_daily_rollup.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_audience_transaction_onetime_donor_counts_daily_rollup.sql
@@ -1,5 +1,5 @@
 {% macro create_stg_audience_transaction_onetime_donor_counts_daily_rollup(
-    person_and_transaction="stg_stitch_sfmc_arc_audience_union_transaction_joined_enriched",
+    person_and_transaction="stg_stitch_sfmc_arc_audience_union_transaction_joined_enriched"
 ) %}
 {{
     dbt_arc_functions.util_stg_audience_transaction_frequency_donor_counts_interval_rollup(
