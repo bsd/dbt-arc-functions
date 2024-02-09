a2fe86906c75afe6a6d4061e207d9691f8921007
Frydafly <fryda.guedes@gmail.com>
2024-02-09 13:29:12 -0600

    remove references to cross_join table

--- macros/stitch_sfmc_audience_transaction/staging/stg_audience_transaction_recur_donor_counts_yearly_rollup.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_audience_transaction_recur_donor_counts_yearly_rollup.sql
@@ -1,6 +1,5 @@
 {% macro create_stg_audience_transaction_recur_donor_counts_yearly_rollup(
-    person_and_transaction="stg_stitch_sfmc_arc_audience_union_transaction_joined_enriched",
-    cross_join="stg_audience_channel_by_day_cross_join"
+    person_and_transaction="stg_stitch_sfmc_arc_audience_union_transaction_joined_enriched"
 ) %}
 {{
     dbt_arc_functions.util_stg_audience_transaction_frequency_donor_counts_interval_rollup(
