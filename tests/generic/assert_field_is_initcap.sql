e7e31fefb39348f4c76c16671f8cad6a07fcf73b
Frydafly <fryda.guedes@gmail.com>
2024-02-19 13:52:07 -0600

    fix sql

--- tests/generic/assert_field_is_initcap.sql
+++ tests/generic/assert_field_is_initcap.sql
@@ -4,9 +4,9 @@
 
 with actual_values as (select distinct {{column_name}} as actual_value from {{model}}),
 
-expected_values as (select distinct initcap({{column_name}} as expected_value from {{model}}))
+expected_values as (select distinct initcap({{column_name}}) as expected_value from {{model}})
 
 select * from actual_values 
-where actual_value not in expected_values
+where actual_value not in (select * from expected_values)
 
 {% endtest %}
