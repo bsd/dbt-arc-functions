af320cafe4080343a9669d8d4cac0346f6b619b1
Frydafly <fryda.guedes@gmail.com>
2024-02-19 15:23:19 -0600

    fix: try to fix the cross join issue for donor counts

--- macros/utils/util_stg_stitch_sfmc_audience_transaction_frequency_donor_counts_interval.sql
+++ macros/utils/util_stg_stitch_sfmc_audience_transaction_frequency_donor_counts_interval.sql
@@ -59,31 +59,72 @@ from
     {% elif interval == 'year' and frequency == 'recurring'%}
     {{ref("stg_audience_transaction_recur_donor_counts_yearly_rollup")}}
     {% endif %}
+),
+
+distinct_audiences as (
+    select distinct donor_audience from base
+),
+
+distinct_channels as (
+    select distinct channel from base
+),
+
+distinct_days as (
+    select distinct date_day from base
 )
 
-    select
+cross_join as (
+    select 
         date_day,
-        interval_type,
-        fiscal_year,
         donor_audience,
-        channel,
-        total{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts, 
-        new{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts,
-        new_to_fy{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts,
-        retained{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts,
-        retained3{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts,
-        reinstated{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts,
-        active{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts,
-        lapsed{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts,
-        sum(unique_totalFY{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts) over w as total{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts_cumulative,
-        sum(unique_newFY{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts) over w as new_to_fy{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts_cumulative,
-        sum(new{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts) over w as new{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts_cumulative,
-        sum(unique_retainedFY{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts) over w as retained{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts_cumulative,
-        sum(unique_retained3FY{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts) over w as retained3{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts_cumulative,
-        sum(unique_reinstatedFY{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts) over w as reinstated{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts_cumulative
-    from base 
+        channel 
+    from distinct_days 
+    cross join distinct_channels
+    cross join distinct_audiences
+),
+
+true_cumulative as (
+    select 
+        cross_join.date_day,
+        base.fiscal_year,
+        base.interval,
+        cross_join.donor_audience,
+        cross_join.channel,
+        sum(base.unique_totalFY{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts) over w as total{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts_cumulative,
+        sum(base.unique_newFY{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts) over w as new_to_fy{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts_cumulative,
+        sum(base.new{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts) over w as new{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts_cumulative,
+        sum(base.unique_retainedFY{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts) over w as retained{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts_cumulative,
+        sum(base.unique_retained3FY{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts) over w as retained3{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts_cumulative,
+        sum(base.unique_reinstatedFY{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts) over w as reinstated{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts_cumulative
+    from cross_join 
+    full outer join base using (date_day, donor_audience, channel)
     window w as (
         partition by fiscal_year, donor_audience, channel
         order by date_day
     )
+)
+
+    select
+        true_cumulative.date_day,
+        true_cumulative.interval_type,
+        true_cumulative.fiscal_year,
+        true_cumulative.donor_audience,
+        true_cumulative.channel,
+        base.total{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts, 
+        base.new{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts,
+        base.new_to_fy{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts,
+        base.retained{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts,
+        base.retained3{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts,
+        base.reinstated{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts,
+        base.active{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts,
+        base.lapsed{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts,
+        true_cumulative.total{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts_cumulative,
+        true_cumulative.new_to_fy{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts_cumulative,
+        true_cumulative.new{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts_cumulative,
+        true_cumulative.retained{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts_cumulative,
+        true_cumulative.retained3{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts_cumulative,
+        true_cumulative.reinstated{% if frequency == 'recurring' %}_recur_{% else %}_onetime_{% endif %}donor_counts_cumulative
+    from true_cumulative 
+    full outer join base using (date_day, donor_audience, channel)
+    
 {% endmacro %}
