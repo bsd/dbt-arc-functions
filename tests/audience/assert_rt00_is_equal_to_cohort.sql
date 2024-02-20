b65cf8d44b91640b2cdbc3bde3627ad59a7a0f3c
Frydafly <fryda.guedes@gmail.com>
2024-02-20 12:49:45 -0600

    typo

--- tests/audience/assert_rt00_is_equal_to_cohort.sql
+++ tests/audience/assert_rt00_is_equal_to_cohort.sql
@@ -1,11 +1,11 @@
-with rt100 as (
+with rt00 as (
     select 
         *
     from {{ref('mart_audience_recur_retention')}}
     where retention_int = 0
 )
 
-select * from rt100
+select * from rt00
 where donors_in_cohort != donors_retained
 
 
