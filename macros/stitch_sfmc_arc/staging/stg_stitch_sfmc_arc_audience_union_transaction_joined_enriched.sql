9bddc46ec7fa470317001793e74c6cb6d6c1110a
Derrick Mondowney <109629735+dmbluestate@users.noreply.github.com>
2024-02-21 14:01:05 -0500

    Split out donor loyalty and adjust transactions enriched mart

--- macros/stitch_sfmc_arc/staging/stg_stitch_sfmc_arc_audience_union_transaction_joined_enriched.sql
+++ macros/stitch_sfmc_arc/staging/stg_stitch_sfmc_arc_audience_union_transaction_joined_enriched.sql
@@ -3,6 +3,7 @@
     donor_audience_unioned="stg_stitch_sfmc_arc_audience_unioned",
     donor_engagement_by_day="stg_stitch_sfmc_donor_engagement_by_date_day",
     donor_transaction_enriched="stg_stitch_sfmc_parameterized_audience_transactions_enriched",
+    donor_loyalty_scd = "stg_stitch_sfmc_arc_donor_loyalty_scd",
     jobs_append="stg_stitch_sfmc_parameterized_audience_transaction_jobs_append"
 ) %}
 
@@ -52,92 +53,7 @@ with audience_union_transaction_joined as (
         on transaction_enriched.transaction_date_day = donor_engagement.date_day
         and transaction_enriched.person_id = donor_engagement.person_id
 
-)
-
-, donor_loyalty_counts as (
-
-        /*
-
-donor_loyalty_counts calculates donor loyalty-related information. 
-It determines the start and end dates for each fiscal year, 
-assigns a row number to each donor within a fiscal year, 
-and organizes the data for further analysis.
-
-*/
-     select
-        person_id,
-        fiscal_year,
-        min(transaction_date_day) as start_date,
-        date_sub(
-            date(concat(fiscal_year, '-', '{{ var('fiscal_year_start') }}')),
-            interval 1 day
-        ) as end_date,
-
-        row_number() over (partition by person_id order by fiscal_year desc) as row_num
-    from audience_union_transaction_joined
-    group by person_id, fiscal_year
-    order by person_id, fiscal_year        
-    )
-
-    ,    donation_history as (
-        /*
-donation_history computes the donation history for each donor, 
-including the previous fiscal year, fiscal year before previous, 
-and the last donation date.
-*/
-            select
-                person_id,
-                fiscal_year,
-                lag(fiscal_year) over (
-                    partition by person_id order by fiscal_year
-                ) as previous_fiscal_year,
-                lag(fiscal_year, 2) over (
-                    partition by person_id order by fiscal_year
-                ) as fiscal_year_before_previous,
-                max(transaction_date_day) as last_donation_date
-            from audience_union_transaction_joined
-            group by person_id, fiscal_year
-        )
-
-        , arc_donor_loyalty as (
-        /*
-Based on the data from donor_loyalty_counts and donation_history,
-arc_donor_loyalty determines the donor's loyalty status for each fiscal year. 
-It classifies donors as new, retained, retained with three or more years, 
-or reactivated donors.
-*/
-    select
-        donor_loyalty_counts.person_id,
-        donor_loyalty_counts.fiscal_year,
-        donor_loyalty_counts.start_date,
-        donor_loyalty_counts.end_date,
-        case
-            when donation_history.previous_fiscal_year is null
-            then 'new_donor'
-            when
-                donation_history.previous_fiscal_year
-                = donor_loyalty_counts.fiscal_year - 1
-                and donation_history.fiscal_year_before_previous is null
-            then 'retained_donor'
-            when
-                donation_history.previous_fiscal_year
-                = donor_loyalty_counts.fiscal_year - 1
-                and donation_history.fiscal_year_before_previous is not null
-            then 'retained_3+_donor'
-            when
-                donation_history.previous_fiscal_year
-                <> donor_loyalty_counts.fiscal_year - 1
-            then 'reactivated_donor'
-        end as donor_loyalty
-    from donor_loyalty_counts
-    left join
-        donation_history
-        on donor_loyalty_counts.person_id = donation_history.person_id
-        and donor_loyalty_counts.fiscal_year = donation_history.fiscal_year
-    order by donor_loyalty_counts.person_id, donor_loyalty_counts.fiscal_year
-
 ),
-
 dedupe as (
 
         /*
@@ -167,7 +83,7 @@ making sure to finally dedupe on transaction_id.
     from
         audience_union_transaction_joined
     left join
-         arc_donor_loyalty
+        {{ref(donor_loyalty_scd)}} arc_donor_loyalty
         on audience_union_transaction_joined.person_id = arc_donor_loyalty.person_id
         and audience_union_transaction_joined.transaction_date_day
         between arc_donor_loyalty.start_date and arc_donor_loyalty.end_date
