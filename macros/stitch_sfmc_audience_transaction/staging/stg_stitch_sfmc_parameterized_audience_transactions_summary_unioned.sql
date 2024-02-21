b3dc6cca4d239b06a3952e10722d595be04a4350
dmbluestate <109629735+dmbluestate@users.noreply.github.com>
2024-02-21 22:42:14 +0000

    [create-pull-request] automated change

--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_parameterized_audience_transactions_summary_unioned.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_parameterized_audience_transactions_summary_unioned.sql
@@ -1,57 +1,57 @@
 {% macro create_stg_stitch_sfmc_parameterized_audience_transactions_summary_unioned(
-    channel="NULL" , digital_status = 'NULL'
+    channel="NULL", digital_status="NULL"
 ) %}
-{% set relations = dbt_arc_functions.relations_that_match_regex(
-    "^stg_stitch_.*_transactions$"
-) %}
-{{
-    config(
-        materialized="table",
-        partition_by={
-            "field": "transaction_date_day",
-            "data_type": "date",
-            "granularity": "day",
-        },
-    )
-}}
+    {% set relations = dbt_arc_functions.relations_that_match_regex(
+        "^stg_stitch_.*_transactions$"
+    ) %}
+    {{
+        config(
+            materialized="table",
+            partition_by={
+                "field": "transaction_date_day",
+                "data_type": "date",
+                "granularity": "day",
+            },
+        )
+    }}
 
-with
-    base as ({{ dbt_utils.union_relations(relations) }}),
-    dedupe as (
+    with
+        base as ({{ dbt_utils.union_relations(relations) }}),
+        dedupe as (
 
-        select
-            *,
-            cast(
-                timestamp_trunc(transaction_date, day) as date
-            ) as transaction_date_day,
-            -- deduping by transaction_id since there are multiple
-            row_number() over (
-                partition by transaction_id order by transaction_date asc
-            ) as row_number
-        from base
-        where
-            transaction_date is not null
-            and person_id is not null
-            and amount > 0
-            -- and only the last 10 years of transactions because we won't go
-            -- further for
-            -- audience data
-            and transaction_date >= date_sub(current_date(), interval 10 year)
-    )
+            select
+                *,
+                cast(
+                    timestamp_trunc(transaction_date, day) as date
+                ) as transaction_date_day,
+                -- deduping by transaction_id since there are multiple
+                row_number() over (
+                    partition by transaction_id order by transaction_date asc
+                ) as row_number
+            from base
+            where
+                transaction_date is not null
+                and person_id is not null
+                and amount > 0
+                -- and only the last 10 years of transactions because we won't go
+                -- further for
+                -- audience data
+                and transaction_date >= date_sub(current_date(), interval 10 year)
+        )
 
-select
-    _dbt_source_relation,
-    transaction_id,
-    transaction_date,
-    transaction_date_day,
-    person_id,
-    recurring,
-    initcap({{ channel }}) as channel,
-    cast({{ digital_status }} as boolean) as is_digital,
-    appeal_business_unit,
-    appeal,
-    amount
-from dedupe
-where row_number = 1
+    select
+        _dbt_source_relation,
+        transaction_id,
+        transaction_date,
+        transaction_date_day,
+        person_id,
+        recurring,
+        initcap({{ channel }}) as channel,
+        cast({{ digital_status }} as boolean) as is_digital,
+        appeal_business_unit,
+        appeal,
+        amount
+    from dedupe
+    where row_number = 1
 
 {% endmacro %}
