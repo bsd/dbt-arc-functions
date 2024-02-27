398c5ec81b7acc1ca3501e176b1153bd3e9073ae
Frydafly <22733009+Frydafly@users.noreply.github.com>
2024-02-27 21:24:47 +0000

    [create-pull-request] automated change

--- tests/audience/assert_new_onetime_donor_equal_by_join_date_across_marts.sql
+++ tests/audience/assert_new_onetime_donor_equal_by_join_date_across_marts.sql
@@ -2,29 +2,25 @@
 
 with
     activation_mart as (
-        select
-            join_month_year_date as join_date,
-            sum(donors_in_cohort) as new_donors
+        select join_month_year_date as join_date, sum(donors_in_cohort) as new_donors
         from {{ ref("mart_audience_1x_activation") }}
         where activation_int = 0
         group by 1
     ),
 
     onetime_donors as (
-        select 
-            date_day as join_date,
-            sum(new_onetime_donor_counts) as new_donors
+        select date_day as join_date, sum(new_onetime_donor_counts) as new_donors
         from {{ ref("mart_audience_budget_with_audience_transaction") }}
-        where lower(interval_type) = 'monthly' 
+        where lower(interval_type) = 'monthly'
         group by 1
     ),
 
     source_of_truth as (
-        select 
-        date_trunc(first_transaction_date, month) as join_date, 
-        count(distinct person_id) as new_donors
-        from {{ref('stg_stitch_sfmc_parameterized_audience_transaction_first_gift')}}
-        where first_gift_recur_status = False
+        select
+            date_trunc(first_transaction_date, month) as join_date,
+            count(distinct person_id) as new_donors
+        from {{ ref("stg_stitch_sfmc_parameterized_audience_transaction_first_gift") }}
+        where first_gift_recur_status = false
         group by 1
 
     ),
@@ -34,13 +30,14 @@ with
         select
             join_date,
             activation_mart.new_donors as activation_new_donors,
-            onetime_donors.new_donors as onetime_new_donors, 
+            onetime_donors.new_donors as onetime_new_donors,
             source_of_truth.new_donors as actual_new_donors
         from activation_mart
         full join onetime_donors using (join_date)
         full join source_of_truth using (join_date)
-        where activation_mart.new_donors != source_of_truth.new_donors
-        or onetime_donors.new_donors != source_of_truth.new_donors
+        where
+            activation_mart.new_donors != source_of_truth.new_donors
+            or onetime_donors.new_donors != source_of_truth.new_donors
     )
 
 select join_date, count(*)
