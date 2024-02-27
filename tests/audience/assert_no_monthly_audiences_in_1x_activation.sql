be04f053ed3739ac739e01d63e281d25b562683f
Frydafly <fryda.guedes@gmail.com>
2024-02-27 15:07:03 -0600

    corrections

--- tests/audience/assert_no_monthly_audiences_in_1x_activation.sql
+++ tests/audience/assert_no_monthly_audiences_in_1x_activation.sql
@@ -1,4 +1,4 @@
-select distinct donor_audience from {{ref('mart_audience_1x_activation')}}
+select distinct join_donor_audience from {{ref('mart_audience_1x_activation')}}
 /* insert variations of monthly audience names that just shouldn't show up here at all */
-where lower(donor_audience) = 'monthly'
-or lower(donor_audience) = 'recurring'
+where lower(join_donor_audience) = 'monthly'
+or lower(join_donor_audience) = 'recurring'
