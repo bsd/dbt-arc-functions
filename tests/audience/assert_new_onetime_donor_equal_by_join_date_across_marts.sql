14fe0a80ec42f698c966993e13a5ce7e0217edeb
Frydafly <fryda.guedes@gmail.com>
2024-02-27 15:03:43 -0600

    enhance testing

--- tests/audience/assert_new_onetime_donor_equal_by_join_date_across_marts.sql
+++ tests/audience/assert_new_onetime_donor_equal_by_join_date_across_marts.sql
@@ -16,15 +16,27 @@ with
         group by 1
     ),
 
+    source_of_truth as (
+        select 
+        join_month_year_date as join_date, 
+        count(distinct person_id) as new_donors
+        from {{ref('stg_stitch_sfmc_parameterized_audience_transaction_first_gift')}}
+        where first_gift_recur_status = False
+        group by 1
+
+    )
+
     issues as (
 
         select
             join_date,
             activation_mart.new_donors as activation_new_donors,
-            onetime_donors.new_donors as onetime_new_donors
+            onetime_donors.new_donors as onetime_new_donors, 
+            source_of_truth.new_donors as actual_new_donors
         from activation_mart
         full join onetime_donors using (join_date)
-        where activation_mart.new_donors != onetime_donors.new_donors
+        where activation_mart.new_donors != source_of_truth.new_donors
+        or onetime_donors.new_donors != source_of_truth.new_donors
     )
 
 select join_date, count(*)
