cd9cdb67a99ea0cb04695973733465bb3a0fb6c6
Frydafly <fryda.guedes@gmail.com>
2024-02-27 15:12:09 -0600

    another

--- tests/audience/assert_new_onetime_donor_equal_by_join_date_across_marts.sql
+++ tests/audience/assert_new_onetime_donor_equal_by_join_date_across_marts.sql
@@ -2,17 +2,20 @@
 
 with
     activation_mart as (
-        select distinct
-            join_month_year_date as join_date, sum(donors_in_cohort) as new_donors
+        select
+            join_month_year_date as join_date,
+            sum(donors_in_cohort) as new_donors
         from {{ ref("mart_audience_1x_activation") }}
         where activation_int = 0
         group by 1
     ),
 
     onetime_donors as (
-        select date_day as join_date, sum(new_onetime_donor_counts) as new_donors
+        select 
+            date_day as join_date,
+            sum(new_onetime_donor_counts) as new_donors
         from {{ ref("mart_audience_budget_with_audience_transaction") }}
-        where lower(interval_type) = 'monthly' and new_onetime_donor_counts > 0
+        where lower(interval_type) = 'monthly' 
         group by 1
     ),
 
