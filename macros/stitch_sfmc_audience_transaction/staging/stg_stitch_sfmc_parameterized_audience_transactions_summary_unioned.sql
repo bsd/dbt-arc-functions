720fc365c5932b9fdf0609921ba49eae18b4a52d
Derrick Mondowney <109629735+dmbluestate@users.noreply.github.com>
2024-02-28 15:30:35 -0500

    change partition

--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_parameterized_audience_transactions_summary_unioned.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_parameterized_audience_transactions_summary_unioned.sql
@@ -9,7 +9,7 @@
             materialized="table",
             partition_by={
                 "field": "transaction_date_day",
-                "data_type": "date",
+                "data_type": "month",
                 "granularity": "day",
             },
         )
