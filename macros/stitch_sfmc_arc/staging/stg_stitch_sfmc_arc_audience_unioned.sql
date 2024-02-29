82d37547e8865e8e47be031c6b37cb0398402484
Derrick Mondowney <109629735+dmbluestate@users.noreply.github.com>
2024-02-29 12:00:52 -0500

    change partition

--- macros/stitch_sfmc_arc/staging/stg_stitch_sfmc_arc_audience_unioned.sql
+++ macros/stitch_sfmc_arc/staging/stg_stitch_sfmc_arc_audience_unioned.sql
@@ -9,7 +9,7 @@
             partition_by={
                 "field": "date_day",
                 "data_type": "month",
-                "granularity": "day",
+                "granularity": "month",
             },
         )
     }}
