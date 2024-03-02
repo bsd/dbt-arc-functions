84e65ff3fb99545ecaa23f67a53d246f11e306f3
Frydafly <fryda.guedes@gmail.com>
2024-03-02 11:07:53 -0600

    test edits

--- tests/audience/assert_first_gift_donor_audience_is_only_prospects.sql
+++ tests/audience/assert_first_gift_donor_audience_is_only_prospects.sql
@@ -1,2 +1,5 @@
-select distinct first_gift_donor_audience from {{ref("stitch_sfmc_parameterized_audience_transaction_first_gift")}}
-where lower(first_gift_donor_audience) not in ('prospect new', 'prospect existing')
+select 
+distinct first_gift_donor_audience 
+from {{ref("stitch_sfmc_parameterized_audience_transaction_first_gift")}}
+where lower(first_gift_donor_audience) != 'prospect new'
+or lower(first_gift_donor_audience) != 'prospect existing'
