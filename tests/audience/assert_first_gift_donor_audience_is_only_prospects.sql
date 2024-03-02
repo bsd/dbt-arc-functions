66319f03982da84953c77467821e5858d34b157b
Frydafly <fryda.guedes@gmail.com>
2024-03-02 11:16:01 -0600

    get counts

--- tests/audience/assert_first_gift_donor_audience_is_only_prospects.sql
+++ tests/audience/assert_first_gift_donor_audience_is_only_prospects.sql
@@ -1,5 +1,6 @@
 {{ config(severity="warn") }}
 select 
-distinct first_gift_donor_audience 
+first_gift_donor_audience, count(distinct person_id) as people 
 from {{ref("stg_stitch_sfmc_parameterized_audience_transaction_first_gift")}}
 where first_gift_donor_audience not like '%Prospect%'
+group by 1
