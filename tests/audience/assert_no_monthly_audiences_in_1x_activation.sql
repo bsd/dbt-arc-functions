a0b3167a5efe007cd107f6bde7fe2b75497f3828
Frydafly <fryda.guedes@gmail.com>
2024-03-04 11:23:47 -0600

    accepted values for join donor audience

--- tests/audience/assert_no_monthly_audiences_in_1x_activation.sql
+++
@@ -1,5 +0,0 @@
-{{ config(severity="warn") }}
-select distinct join_donor_audience
-from {{ ref("mart_audience_1x_activation") }}
-/* insert variations of monthly audience names that just shouldn't show up here at all */
-where lower(join_donor_audience) = 'monthly' or lower(join_donor_audience) = 'recurring'
