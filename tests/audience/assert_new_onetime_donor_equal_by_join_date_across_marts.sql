d58446e36e8f5a4a6df3e163b0234141380415b3
Frydafly <fryda.guedes@gmail.com>
2024-03-07 14:16:20 -0600

    modify test

--- tests/audience/assert_new_onetime_donor_equal_by_join_date_across_marts.sql
+++ tests/audience/assert_new_onetime_donor_equal_by_join_date_across_marts.sql
@@ -15,7 +15,7 @@ with
         group by 1
     ),
 
-    source_of_truth as (
+    source_of_truth_all as (
         select
             date_trunc(first_transaction_date, month) as join_date,
             count(distinct person_id) as new_donors
@@ -25,19 +25,31 @@ with
 
     ),
 
+    source_of_truth_digital as (
+        select 
+            date_trunc(first_transaction_date, month) as join_date,
+            count(distinct person_id) as new_donors
+        from {{ ref("stg_stitch_sfmc_parameterized_audience_transaction_first_gift") }}
+        where first_gift_recur_status = false
+        and transaction_id in (select distinct transaction_id from {{ref("stg_stitch_sfmc_arc_audience_union_transaction_joined_enriched_digital")}})
+        group by 1
+    ),
+
     issues as (
 
         select
             join_date,
             activation_mart.new_donors as activation_new_donors,
             onetime_donors.new_donors as onetime_new_donors,
-            source_of_truth.new_donors as actual_new_donors
+            source_of_truth.new_donors as actual_new_donors,
+            source_of_truth_digital.new_donors as actual_new_donors_digital
         from activation_mart
         full join onetime_donors using (join_date)
         full join source_of_truth using (join_date)
+        full join source_of_truth_digital using (join_date)
         where
             activation_mart.new_donors != source_of_truth.new_donors
-            or onetime_donors.new_donors != source_of_truth.new_donors
+            or onetime_donors.new_donors != source_of_truth_digital.new_donors
     )
 
 select join_date, count(*)
