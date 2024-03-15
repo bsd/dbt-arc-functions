65b5c5337bd9ee0b067465d7b30e94b3f1d101ea
Frydafly <fryda.guedes@gmail.com>
2024-03-15 12:11:38 -0500

    audience as amount

--- macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_parameterized_audience_transaction_first_gift.sql
+++ macros/stitch_sfmc_audience_transaction/staging/stg_stitch_sfmc_parameterized_audience_transaction_first_gift.sql
@@ -77,45 +77,45 @@ and pulls attributes from that first gift.
                 audience.channel as first_gift_join_source,
                 (
                     case
-                        when transactions.amount between 0 and 25.99
+                        when audience.amount between 0 and 25.99
                         then '0-25'
-                        when transactions.amount between 26 and 100.99
+                        when audience.amount between 26 and 100.99
                         then '26-100'
-                        when transactions.amount between 101 and 250.99
+                        when audience.amount between 101 and 250.99
                         then '101-250'
-                        when transactions.amount between 251 and 500.99
+                        when audience.amount between 251 and 500.99
                         then '251-500'
-                        when transactions.amount between 501 and 1000.99
+                        when audience.amount between 501 and 1000.99
                         then '501-1000'
-                        when transactions.amount between 1001 and 10000.99
+                        when audience.amount between 1001 and 10000.99
                         then '1001-10000'
-                        when transactions.amount > 10000
+                        when audience.amount > 10000
                         then '10000+'
                     end
                 ) as join_gift_size_string,
                 (
                     case
-                        when transactions.amount between 0 and 10.99
+                        when audience.amount between 0 and 10.99
                         then '0-10'
-                        when transactions.amount between 11 and 20.99
+                        when audience.amount between 11 and 20.99
                         then '11-20'
-                        when transactions.amount between 21 and 30.99
+                        when audience.amount between 21 and 30.99
                         then '21-30'
-                        when transactions.amount between 31 and 40.99
+                        when audience.amount between 31 and 40.99
                         then '31-40'
-                        when transactions.amount between 41 and 50.99
+                        when audience.amount between 41 and 50.99
                         then '41-50'
-                        when transactions.amount between 51 and 60.99
+                        when audience.amount between 51 and 60.99
                         then '51-60'
-                        when transactions.amount between 61 and 70.99
+                        when audience.amount between 61 and 70.99
                         then '61-70'
-                        when transactions.amount between 71 and 80.99
+                        when audience.amount between 71 and 80.99
                         then '71-80'
-                        when transactions.amount between 81 and 90.99
+                        when audience.amount between 81 and 90.99
                         then '81-90'
-                        when transactions.amount between 91 and 100.99
+                        when audience.amount between 91 and 100.99
                         then '91-100'
-                        when transactions.amount > 100
+                        when audience.amount > 100
                         then '100+'
                     end
                 ) as join_gift_size_string_recur,
@@ -128,7 +128,7 @@ and pulls attributes from that first gift.
                 ) as first_gift_recur_status,
                 audience.donor_audience as first_gift_donor_audience,
                 transactions.recurring as transaction_recur_status,
-                transactions.amount
+                audience.amount
             from first_transactions
             left join
                 transactions
