1d944cda1b0bfad7270e2825f70cff028f1003d3
Tim Ryan <ryantimjohn@gmail.com>
2024-03-15 17:05:00 -0400

    [create-pull-request] automated change (#535)
    Co-authored-by: Frydafly <22733009+Frydafly@users.noreply.github.com>

--- tests/audience/assert_transaction_ids_preserved.sql
+++ tests/audience/assert_transaction_ids_preserved.sql
@@ -1,12 +1,20 @@
 with
-transactions as (
-    select
-        *
-    from {{ref('stg_stitch_sfmc_parameterized_audience_transactions_summary_unioned')}}
-),
+    transactions as (
+        select *
+        from
+            {{
+                ref(
+                    "stg_stitch_sfmc_parameterized_audience_transactions_summary_unioned"
+                )
+            }}
+    ),
 
-audience as (select * from {{ref('stg_stitch_sfmc_arc_audience_union_transaction_joined_enriched')}})
+    audience as (
+        select *
+        from {{ ref("stg_stitch_sfmc_arc_audience_union_transaction_joined_enriched") }}
+    )
 
-select is_digital, count(*) from transactions
+select is_digital, count(*)
+from transactions
 where transaction_id not in (select distinct transaction_id from audience)
 group by 1
