70c4cd1e920fc71bbc7027a503e5e4d7f98492dc
Frydafly <fryda.guedes@gmail.com>
2024-03-25 13:39:26 -0500

    null purchases

--- macros/ga4_google_analytics_web/staging/stg_ga4_web_daily_purchases_rollup.sql
+++ macros/ga4_google_analytics_web/staging/stg_ga4_web_daily_purchases_rollup.sql
@@ -1,11 +1,11 @@
 {% macro create_stg_ga4_web_daily_purchases_rollup(
-    reference_name="fct_ga4__sessions"
+    reference_name="fct_ga4__sessions_daily"
 ) %}
     select
-        session_start_date as session_date,
+        session_partition_date as session_date,
         session_key,
-        sum(count_purchases) as purchases
-    from {{ ref(reference_name) }}
+        sum(cast(NULL as int64)) as purchases
+    from {{ ref(reference_name) }} --value is now missing from ga4 models
     group by 1, 2
     having purchases > 0
 
